95bafe2a8987e6b780837fb010aa984cda9eccd9
周朋 <hiflow@qq.com>
2016-05-20 17:00:43 +0800

    no commit message

---
+++ CCFlow/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的常规信息通过以下特性集 
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("CCFlow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("济南驰骋信息技术有限公司")]
+[assembly: AssemblyProduct("CCFlow")]
+[assembly: AssemblyCopyright("Copyright ©  2003 - 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型 
+// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型， 
+// 则将该类型上的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("6ec44440-8262-4533-a2a0-2f72dfde7d81")]
+
+// 程序集的版本信息由下列四个值组成:
+//
+//      主版本
+//      次版本 
+//      内部版本号
+//      修订号
+//
+// 您可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值， 
+// 方法是按如下所示使用“*”:
+[assembly: AssemblyFileVersion("5.0.0.0")]

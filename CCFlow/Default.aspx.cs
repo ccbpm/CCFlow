132ca2c6a02d42eac68f78e9d80d5a6b326c2d2b
周朋 <hiflow@qq.com>
2016-05-20 16:59:32 +0800

    no commit message

---
+++ CCFlow/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using BP.DA;
+using BP.En;
+using BP.Sys;
+using BP.WF;
+using BP.Web;
+
+
+namespace CCFlow
+{
+    public partial class Default : WebPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.Response.Redirect("./WF/Admin/CCBPMDesigner/Login.aspx", true);
+            return;
+        }
+    }
+}

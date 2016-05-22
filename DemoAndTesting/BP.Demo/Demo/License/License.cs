a48621e67b053b1f3bfd2510d0ce2d2ca3a5494a
周朋 <hiflow@qq.com>
2016-05-22 12:39:12 +0800

    no commit message

---
+++ DemoAndTesting/BP.Demo/Demo/License/License.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Data;
+using BP.DA;
+using BP.En;
+using BP.Port;
+using BP.GPM;
+namespace BP.Demo.License
+{
+    public class LicenseType
+    {
+        /// <summary>
+        /// 身份证
+        /// </summary>
+        public const string SFZ = "SFZ";
+        /// <summary>
+        /// 驾驶证
+        /// </summary>
+        public const string JSZ = "JSZ";
+        /// <summary>
+        /// 房产证
+        /// </summary>
+        public const string FCZ = "FCZ";
+        /// <summary>
+        /// 结婚证
+        /// </summary>
+        public const string JHZ = "JHZ";
+
+
+        public static string GetNameByID(string id)
+        {
+            switch (id)
+            {
+                case LicenseType.SFZ:
+                    return "身份证";
+                case LicenseType.JSZ:
+                    return "驾驶证";
+                case LicenseType.FCZ:
+                    return "房产证";
+                case LicenseType.JHZ:
+                    return "结婚证";
+                default:
+                    throw new Exception("@没有涉及到的ID" + id);
+            }
+        }
+        public static string GetIDByName(string name)
+        {
+            switch (name)
+            {
+                case "身份证" :
+                    return LicenseType.SFZ;
+                case "驾照":
+                case "驾驶证":
+                    return LicenseType.JSZ;
+                case "房产证" :
+                    return LicenseType.FCZ;
+                case "结婚证":
+                    return LicenseType.JHZ;
+                default:
+                    throw new Exception("@没有涉及到的ID" + name);
+            }
+        }
+    }
+    /// <summary>
+    /// 证照属性
+    /// </summary>
+    public class LicenseAttr : BP.En.EntityNoNameAttr
+    {
+        #region 基本属性
+        /// <summary>
+        /// 身份证号
+        /// </summary>
+        public const string SFZ = "SFZ";
+        /// <summary>
+        /// 类型ID
+        /// </summary>
+        public const string ZJLX = "ZJLX";
+        /// <summary>
+        /// 证件类型名称
+        /// </summary>
+        public const string ZJLXName = "ZJLXName";
+        /// <summary>
+        /// 证件代码
+        /// </summary>
+        public const string ZJCode = "ZJCode";
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public const string XB = "XB";
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public const string FilePath = "FilePath";
+
+        /// <summary>
+        /// 上传日期
+        /// </summary>
+        public const string RDT = "RDT";
+        /// <summary>
+        /// 上传人
+        /// </summary>
+        public const string Rec = "Rec";
+        /// <summary>
+        /// 扩展
+        /// </summary>
+        public const string Ext = "Ext";
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+        public const string FileSize = "FileSize";
+        #endregion
+
+    }
+    /// <summary>
+    /// 证照 的摘要说明。
+    /// </summary>
+    public class License : EntityMyPK
+    {
+        #region 扩展属性
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return LicenseType.GetNameByID(this.ZJLX);
+            }
+        }
+        /// <summary>
+        /// 身份证号
+        /// </summary>
+        public string SFZ
+        {
+            get
+            {
+                return this.GetValStrByKey(LicenseAttr.SFZ);
+            }
+            set
+            {
+                this.SetValByKey(LicenseAttr.SFZ, value);
+            }
+        }
+        public string FilePath
+        {
+            get
+            {
+                return this.GetValStrByKey(LicenseAttr.FilePath);
+            }
+            set
+            {
+                this.SetValByKey(LicenseAttr.FilePath, value);
+            }
+        }
+        /// <summary>
+        /// 证件类型ID
+        /// </summary>
+        public string ZJLX
+        {
+            get
+            {
+                return this.GetValStrByKey(LicenseAttr.ZJLX);
+            }
+            set
+            {
+                this.SetValByKey(LicenseAttr.ZJLX, value);
+            }
+        }
+        /// <summary>
+        /// 上传人
+        /// </summary>
+        public string Rec
+        {
+            get
+            {
+                return this.GetValStrByKey(LicenseAttr.Rec);
+            }
+            set
+            {
+                this.SetValByKey(LicenseAttr.Rec, value);
+            }
+        }
+        /// <summary>
+        /// 上传日期
+        /// </summary>
+        public string RDT
+        {
+            get
+            {
+                return this.GetValStrByKey(LicenseAttr.RDT);
+            }
+            set
+            {
+                this.SetValByKey(LicenseAttr.RDT, value);
+            }
+        }
+        /// <summary>
+        /// 证件代码
+        /// </summary>
+        public string ZJCode
+        {
+            get
+            {
+                string str= this.GetValStrByKey(LicenseAttr.ZJCode);
+                if (string.IsNullOrEmpty(str))
+                    return "无";
+                return str;
+            }
+            set
+            {
+                this.SetValByKey(LicenseAttr.ZJCode, value);
+            }
+        }
+        /// <summary>
+        /// 文件ext
+        /// </summary>
+        public string Ext
+        {
+            get
+            {
+                return this.GetValStrByKey(LicenseAttr.Ext);
+            }
+            set
+            {
+                this.SetValByKey(LicenseAttr.Ext, value);
+            }
+        }
+        public float FileSize
+        {
+            get
+            {
+                return this.GetValFloatByKey(LicenseAttr.FileSize);
+            }
+            set
+            {
+                this.SetValByKey(LicenseAttr.FileSize, value);
+            }
+        }
+        #endregion
+
+        #region 构造函数
+        /// <summary>
+        /// 证照
+        /// </summary>
+        public License()
+        {
+        }
+        /// <summary>
+        /// 证照
+        /// </summary>
+        /// <param name="no">编号</param>
+        public License(string no)
+        {
+            this.MyPK = no.Trim();
+            if (this.MyPK.Length == 0)
+                throw new Exception("@要查询的证照编号为空。");
+            try
+            {
+                this.Retrieve();
+            }
+            catch (Exception ex)
+            {
+                int i = this.RetrieveFromDBSources();
+                if (i == 0)
+                    throw new Exception("@用户或者密码错误：[" + no + "]，或者帐号被停用。@技术信息(从内存中查询出现错误)：ex1=" + ex.Message);
+            }
+        }
+        public override UAC HisUAC
+        {
+            get
+            {
+                UAC uac = new UAC();
+                uac.OpenForAppAdmin();
+                return uac;
+            }
+        }
+        /// <summary>
+        /// 重写基类方法
+        /// </summary>
+        public override Map EnMap
+        {
+            get
+            {
+                if (this._enMap != null)
+                    return this._enMap;
+
+                Map map = new Map();
+
+                #region 基本属性
+                map.PhysicsTable = "Demo_License"; // 要物理表。
+                map.DepositaryOfMap = Depositary.Application;    //实体map的存放位置.
+                map.DepositaryOfEntity = Depositary.None; //实体存放位置
+                map.EnDesc = "用户"; // "用户"; // 实体的描述.
+                map.EnType = EnType.App;   //实体类型。
+                #endregion
+
+                #region 字段
+                /*关于字段属性的增加 */
+                map.AddMyPK();
+                map.AddTBString(LicenseAttr.SFZ, null, "身份证号", true, false, 0, 500, 30);
+
+                map.AddTBString(LicenseAttr.Name, null, "证件名称", true, false, 0, 500, 30);
+                map.AddTBString(LicenseAttr.ZJLX, null, "证件类型", true, false, 0, 500, 30);
+
+                map.AddTBString(LicenseAttr.FilePath, null, "文件路径", false, false, 0, 500, 10);
+                map.AddTBString(LicenseAttr.Ext, null, "扩展名", false, false, 0, 20, 10);
+
+                map.AddTBString(LicenseAttr.ZJCode, null, "证件代码", false, false, 0, 500, 10);
+
+                map.AddTBString(LicenseAttr.RDT, null, "上传日期", false, false, 0, 500, 10);
+                map.AddTBString(LicenseAttr.Rec, null, "上传人", false, false, 0, 500, 10);
+
+                map.AddTBFloat(LicenseAttr.FileSize, 0, "大小", false, false);
+                #endregion 字段
+
+
+                this._enMap = map;
+                return this._enMap;
+            }
+        }
+        /// <summary>
+        /// 获取集合
+        /// </summary>
+        public override Entities GetNewEntities
+        {
+            get { return new Licenses(); }
+        }
+        #endregion 构造函数
+    }
+    /// <summary>
+    /// 证照s
+    // </summary>
+    public class Licenses : EntitiesMyPK
+    {
+        #region 构造方法
+        /// <summary>
+        /// 得到它的 Entity 
+        /// </summary>
+        public override Entity GetNewEntity
+        {
+            get
+            {
+                return new License();
+            }
+        }
+        /// <summary>
+        /// 证照s
+        /// </summary>
+        public Licenses()
+        {
+        }
+        /// <summary>
+        /// 身份证
+        /// </summary>
+        /// <param name="sfz"></param>
+        public Licenses(string sfz)
+        {
+            this.Retrieve(LicenseAttr.SFZ, sfz);
+        }
+        #endregion 构造方法
+    }
+}
---
+++ DemoAndTesting/BP.Demo/Demo/License/People.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Data;
+using BP.DA;
+using BP.En;
+using BP.Port;
+using BP.GPM;
+
+namespace BP.Demo.License
+{
+    /// <summary>
+    /// 居民属性
+    /// </summary>
+    public class PeopleAttr : BP.En.EntityNoNameAttr
+    {
+        #region 基本属性
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public const string Addr = "Addr";
+        /// <summary>
+        /// 电话
+        /// </summary>
+        public const string Tel = "Tel";
+        /// <summary>
+        /// 出生日期
+        /// </summary>
+        public const string BDT = "BDT";
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public const string XB = "XB";
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public const string Age = "Age";
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public const string Email = "Email";
+
+        #endregion
+    }
+    /// <summary>
+    /// 居民 的摘要说明。
+    /// </summary>
+    public class People : EntityNoName
+    {
+        #region 扩展属性
+        public string XB
+        {
+            get
+            {
+                return this.GetValStrByKey(PeopleAttr.XB);
+            }
+            set
+            {
+                this.SetValByKey(PeopleAttr.XB, value);
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return this.GetValStrByKey(PeopleAttr.Email);
+            }
+            set
+            {
+                this.SetValByKey(PeopleAttr.Email, value);
+            }
+        }
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Addr
+        {
+            get
+            {
+                return this.GetValStrByKey(PeopleAttr.Addr);
+            }
+            set
+            {
+                this.SetValByKey(PeopleAttr.Addr, value);
+            }
+        }
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public string Age
+        {
+            get
+            {
+                return this.GetValStrByKey(PeopleAttr.Age);
+            }
+            set
+            {
+                this.SetValByKey(PeopleAttr.Age, value);
+            }
+        }
+        /// <summary>
+        /// 电话
+        /// </summary>
+        public string Tel
+        {
+            get
+            {
+                return this.GetValStrByKey(PeopleAttr.Tel);
+            }
+            set
+            {
+                this.SetValByKey(PeopleAttr.Tel, value);
+            }
+        }
+        /// <summary>
+        /// 出生日期
+        /// </summary>
+        public string BDT
+        {
+            get
+            {
+                return this.GetValStrByKey(PeopleAttr.BDT);
+            }
+            set
+            {
+                this.SetValByKey(PeopleAttr.BDT, value);
+            }
+        }
+        #endregion
+       
+        #region 构造函数
+        /// <summary>
+        /// 居民
+        /// </summary>
+        public People()
+        {
+        }
+        /// <summary>
+        /// 居民
+        /// </summary>
+        /// <param name="no">编号</param>
+        public People(string no)
+        {
+            this.No = no.Trim();
+            if (this.No.Length == 0)
+                throw new Exception("@要查询的居民编号为空。");
+
+            this.Retrieve();
+        }
+
+        public override UAC HisUAC
+        {
+            get
+            {
+                UAC uac = new UAC();
+                uac.OpenForAppAdmin();
+                return uac;
+            }
+        }
+        /// <summary>
+        /// 重写基类方法
+        /// </summary>
+        public override Map EnMap
+        {
+            get
+            {
+                if (this._enMap != null)
+                    return this._enMap;
+
+                Map map = new Map();
+
+                #region 基本属性
+                map.EnDBUrl =
+                    new DBUrl(DBUrlType.AppCenterDSN); //要连接的数据源（表示要连接到的那个系统数据库）。
+                map.PhysicsTable = "Demo_People"; // 要物理表。
+                map.DepositaryOfMap = Depositary.Application;    //实体map的存放位置.
+                map.DepositaryOfEntity = Depositary.None; //实体存放位置
+                map.EnDesc = "用户"; // "用户"; // 实体的描述.
+                map.EnType = EnType.App;   //实体类型。
+                #endregion
+
+                #region 字段
+                /*关于字段属性的增加 */
+                map.AddTBStringPK(PeopleAttr.No, null, "编号", true, false, 1, 20, 30);
+                map.AddTBString(PeopleAttr.Name, null, "名称", true, false, 0, 999, 30);
+
+                map.AddTBString(PeopleAttr.Addr, null, "地址", true, false, 0, 999, 30);
+
+                map.AddTBString(PeopleAttr.Tel, null, "电话", false, false, 0, 999, 10);
+                map.AddTBString(PeopleAttr.BDT, null, "出生日期", false, false, 0, 999, 10);
+                map.AddTBString(PeopleAttr.Email, null, "邮箱", true, false, 0, 999, 132);
+
+                map.AddTBString(PeopleAttr.XB, null, "性别", true, false, 0, 999, 132);
+                map.AddTBString(PeopleAttr.Age, null, "年龄", true, false, 0, 999, 132);
+                #endregion 字段
+
+               
+                RefMethod rm = new RefMethod();
+                rm.Title = "修改密码";
+                rm.ClassMethodName = this.ToString() + ".DoResetpassword";
+                rm.HisAttrs.AddTBString("pass1", null, "输入密码", true, false, 0, 100, 100);
+                rm.HisAttrs.AddTBString("pass2", null, "再次输入", true, false, 0, 100, 100);
+                map.AddRefMethod(rm);
+
+                this._enMap = map;
+                return this._enMap;
+            }
+        }
+        /// <summary>
+        /// 获取集合
+        /// </summary>
+        public override Entities GetNewEntities
+        {
+            get { return new Peoples(); }
+        }
+        #endregion 构造函数
+    }
+    /// <summary>
+    /// 居民s
+    // </summary>
+    public class Peoples : EntitiesNoName
+    {
+        #region 构造方法
+        /// <summary>
+        /// 得到它的 Entity 
+        /// </summary>
+        public override Entity GetNewEntity
+        {
+            get
+            {
+                return new People();
+            }
+        }
+        /// <summary>
+        /// 居民s
+        /// </summary>
+        public Peoples()
+        {
+        }
+        public override int RetrieveAll()
+        {
+            return base.RetrieveAll("Name");
+        }
+        #endregion 构造方法
+    }
+}

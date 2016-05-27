a4eb03faffe6f27ec680abf66cbf18d199e522cc
ccflow代国强 <daiguoqiang@ccflow.org>
2016-05-27 14:07:03 +0800

    no commit message

--- Components/BP.En30/DA/DataType.cs
+++ Components/BP.En30/DA/DataType.cs
@@ -424,6 +424,35 @@ namespace BP.DA
             sr.Write(Doc);
             sr.Close();
         }
+
+        /// <summary>
+        /// Http下载文件
+        /// </summary>
+        public static string HttpDownloadFile(string url, string path)
+        {
+            // 设置参数
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+
+            //发送请求并获取相应回应数据
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            //直到request.GetResponse()程序才开始向目标网页发送Post请求
+            Stream responseStream = response.GetResponseStream();
+
+            //创建本地文件写入流
+            Stream stream = new FileStream(path, FileMode.Create);
+
+            byte[] bArr = new byte[1024];
+            int size = responseStream.Read(bArr, 0, (int)bArr.Length);
+            while (size > 0)
+            {
+                stream.Write(bArr, 0, size);
+                size = responseStream.Read(bArr, 0, (int)bArr.Length);
+            }
+            stream.Close();
+            responseStream.Close();
+            return path;
+        }
+
         /// <summary>
         /// 读取URL内容
         /// </summary>

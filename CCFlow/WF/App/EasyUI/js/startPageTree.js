39912348f809fbffbdd854fe628001a5bcb4e4a1
ccflow代国强 <daiguoqiang@ccflow.org>
2016-06-14 14:58:12 +0800

    no commit message

--- CCFlow/WF/App/EasyUI/js/startPageTree.js
+++ CCFlow/WF/App/EasyUI/js/startPageTree.js
@@ -1,4 +1,11 @@
-﻿var strTimeKey = "";
+﻿/// <reference path="../../../../app/GovService/FlowHelp.aspx" />
+/// <reference path="../../../../app/GovService/FlowHelp.aspx" />
+/// <reference path="../../../../app/AdvFunc.aspx" />
+/// <reference path="../../../../app/GovService/FlowHelp.aspx" />
+/// <reference path="../../../../app/GovService/FlowHelp.aspx" />
+/// <reference path="../../../../app/GovService/FlowHelp.aspx" />
+/// <reference path="../../../../app/GovService/FlowHelp.aspx" />
+var strTimeKey = "";
 //发起流程
 function StartFlow(url) {
 
@@ -11,8 +18,8 @@ function StartListUrl(url) {
 }
 //打开窗体
 function WinOpenIt(tabid, text, url) {
-    if (ccflow.config.IsWinOpenStartWork == 1) {
-        window.open(url, '_blank', 'height=600,width=850,top=50,left=50,toolbar=no,menubar=no,scrollbars=yes, resizable=yes,location=no, status=no');
+    if (ccflow.config.IsWinOpenStartWork == 0) {
+        window.parent.f_addTab(tabid + strTimeKey, text, url);
     } else {
         var winWidth = 850;
         var winHeight = 680;
@@ -25,7 +32,7 @@ function WinOpenIt(tabid, text, url) {
     }
 }
 function WinOpenWindow(url) {
-    var newWindow = window.open(url, '_blank', 'height=600,width=850,top=50,left=50,toolbar=no,menubar=no,scrollbars=yes, resizable=yes,location=no, status=no');
+    var newWindow = window.open(url, '_blank', 'height=600,width=1100,top=50,left=50,toolbar=no,menubar=no,scrollbars=yes, resizable=yes,location=no, status=no');
     newWindow.focus();
 }
 function callBack(jsonData, scope) {
@@ -44,28 +51,24 @@ function callBack(jsonData, scope) {
                             return value;
                         var h = "";
                         if (rec.STARTLISTURL) {
-                            h = rec.STARTLISTURL + "?FK_Flow=" + rec.NO + "&FK_Node=" + rec.NO + "01&T=" + strTimeKey;
+                            h = rec.STARTLISTURL + "?FK_Flow=" + rec.NO + "&FK_Node=" + Number(rec.NO) + "01&T=" + strTimeKey;
+                     
                             return "<a href='javascript:void(0);' onclick=StartListUrl('" + h + "')>" + rec.NAME + "</a>";
                         }
-                        h = "../../MyFlow.aspx?FK_Flow=" + rec.NO + "&FK_Node=" + rec.NO + "01&T=" + strTimeKey;
-                        return "<a href='javascript:void(0);' onclick=ShowEasyUiTitleDiv('" + rec.NO + "','" + rec.NAME + "','" + h + "')>" + rec.NAME + "</a>";
+                        h = "../../MyFlow.aspx?FK_Flow=" + rec.NO + "&FK_Node=" + Number(rec.NO) + "01&T=" + strTimeKey;
+
+                       
+                        return "<a href='javascript:void(0);' onclick=WinOpenWindow('" + h + "')>" + rec.NAME + "</a>";
+
+                        //return "<a href='"+h+"'>"+ rec.NAME +" </a>";
                     }
                     },
-                    { field: 'IsBatchStart', title: '批量发起', formatter: function (value, rec) {
-                        if (rec.FK_FLOWSORT == "")
-                            return value;
-                        var h = "";
-                        if (rec.ISBATCHSTART == "1") {
-                            h = "../../BatchStart.aspx?FK_Flow=" + rec.NO;
-                            h = "<a href='javascript:void(0);' onclick=StartListUrl('" + h + "')>批量发起</a>";
-                        }
-                        return h;
-                    }
-                    },
+                   
                     { field: 'RoleType', title: '流程图', formatter: function (value, rec) {
                         if (rec.FK_FLOWSORT == "")
                             return value;
-                        return "<a href='javascript:void(0);' onclick=OpenEasyUiFlowPicture('" + rec.NO + "','" + rec.NAME + "')>打开</a>";
+                        //return "<a href='javascript:void(0);' onclick=OpenPage('" + rec.NO + "')>打开</a>";
+                        return "<a href='javascript:void(0);' onclick=LayerOpenFlow('/WF/Admin/CCBPMDesigner/truck/Truck.htm?FK_Flow="+rec.NO+"&WorkID=null&FID=null&DoType=Chart&T=123')>打开</a>";
                     }
                     },
                     { field: 'HistoryFlow', title: '历史发起', width: 180, formatter: function (value, rec) {
@@ -74,17 +77,18 @@ function callBack(jsonData, scope) {
                         return "<a href='javascript:void(0);' onclick=ShowEasyUiHistoryData('" + rec.NO + "','" + rec.NAME + "')>查看</a>";
                     }
                     },
-                    { field: 'Note', title: '描述', width: 280, formatter: function (value, rec) {
-                        if (rec.FK_FLOWSORT == "")
-                            return value;
-                        if (rec.NOTE == null || rec.NOTE == "") {
-                            return "无";
-                        }
-                        return rec.Note;
-                    }
-                    }
+                         {
+                             field: 'FK_Flow', title: '事项帮助', width: 180, formatter: function (value, rec) {
+                               
+                                 if (rec.FK_FLOWSORT == "")
+                                     return value;
+                                 //return "<a href='javascript:void(0):'onclick=window.open('/App/GovService/FlowHelp.aspx?FK_Flow=" + rec.NO + "') >查看帮助</a>";
+
+                                 return "<a href='javascript:void(0):'onclick= LayerOpen('/App/GovService/FlowHelp.aspx?FK_Flow=" + rec.NO + "')>查看帮助</a>";
+                             }
+                         }
                 ]],
-            onLoadSuccess: function (row, data) {
+            onLoadSuccess: function (row, data) {   
 
             }
 
@@ -96,6 +100,35 @@ function callBack(jsonData, scope) {
     $("#pageloading").hide();
 }
 
+// 弹窗美化事项帮助
+function LayerOpen(url)
+{
+
+
+    layer.open({
+        type: 2, skin: 'layui-layer-lan',
+        title: '事项帮助列表',
+        shadeClose: true, shade: 0.8, area: ['800px', '70%'],
+        content:url
+    })
+}
+
+//弹窗美化流程图
+function LayerOpenFlow(url) {
+
+
+    layer.open({
+        type: 2, skin: 'layui-layer-lan',
+        title: '事项轨迹图',
+        shadeClose: true, shade: 0.8, area: ['100%', '100%'],
+        content: url
+    })
+}
+function OpenPage(flowno) {
+
+    window.open('/WF/Admin/CCBPMDesigner/truck/Truck.htm?FK_Flow='+flowno+'&WorkID=null&FID=null&DoType=Chart&T=123');
+
+}
 //加载历史发起数据
 function LoadEasyUiHistoryGrid(flowNo) {
     $("#pageloading").show();
@@ -111,13 +144,14 @@ function LoadEasyUiHistoryGrid(flowNo) {
                 collapsible: false,
                 url: '/WF/App/EasyUI/Base/DataService.aspx?method=historystartflow&FK_Flow=' + flowNo,
                 columns: [[
-                        { title: '标题', field: 'Title', width: 320, align: 'left', formatter: function (value, rec) {
+                        {
+                            title: '标题', field: 'TITLE', width: 320, align: 'left', formatter: function (value, rec) {
                             var h = "../../WFRpt.aspx?WorkID=" + rec.OID + "&FK_Flow=" + flowNo + "&FID=" + rec.FID + "&T=" + strTimeKey;
-                            return "<a href='javascript:void(0);' onclick=WinOpenWindow('" + h + "')>" + rec.Title + "</a>";
+                            return "<a href='javascript:void(0);' onclick=WinOpenWindow('" + h + "')>" + rec.TITLE + "</a>";
                         }
                         },
-                        { title: '发起时间', field: 'FlowStartRDT' },
-                        { title: '参与人', field: 'FlowEmps', width: 300 }
+                        { title: '发起时间', field: 'FLOWSTARTRDT' },
+                        { title: '参与人', field: 'FLOWEMPS', width: 300 }
                     ]]
             });
         }
@@ -126,8 +160,11 @@ function LoadEasyUiHistoryGrid(flowNo) {
 }
 //打开流程图
 function OpenEasyUiFlowPicture(flowNo, flowName) {
-    var pictureUrl = "../../../DataUser/FlowDesc/" + flowNo + "." + flowName + "/Flow.png";
 
+        //var pictureUrl = "../../../DataUser/FlowDesc/" + flowNo + "." + flowName + "/Flow.png";
+
+    //var pictureUrl = "/WF/Admin/CCBPMDesigner/truck/Truck.htm?FK_Flow=" + fl.No + "&WorkID=null&FID=null&DoType=Chart&T=" + timeKey + "";
+    var pictureUrl = "/WF/Admin/CCBPMDesigner/truck/Truck.htm?FK_Flow=" + flowNo + "&WorkID=null&FID=null&DoType=Chart&T=" + Date.now + "";
     document.getElementById("FlowPic").src = pictureUrl;
     $("#flowPicDiv").dialog({
         height: 500,
@@ -143,6 +180,8 @@ function OpenEasyUiFlowPicture(flowNo, flowName) {
         }]
     });
 }
+
+
 //显示历史发起
 function ShowEasyUiHistoryData(flowNo, flowName) {
     //打开层
@@ -163,21 +202,17 @@ function ShowEasyUiTitleDiv(tabid, text, url) {
     //执行命令
     Application.data.createEmptyCase(tabid, "", function (js, scope) {
         if (js == "addform") {
-            if (ccflow.config.IsWinOpenStartWork == 0) {
+            if (ccflow.config.IsWinOpenStartWork == 1) {
                 WinOpenIt(tabid, text, url);
             } else {
-                if (window.parent && window.parent.f_addTab) {
-                    window.parent.f_addTab(tabid + strTimeKey, text, url);
-                } else {
-                    WinOpenIt(tabid, text, url);
-                }
+                window.parent.f_addTab(tabid + strTimeKey, text, url);
             }
         } else {
             //打开层
             $('#divTitle').show();
             $("#divTitle").dialog({
                 title: '新建- ' + text + "流程",
-                width: 510,
+                width: 400,
                 height: 180,
                 resizable: true,
                 buttons: [{ text: '确定', handler: function () {

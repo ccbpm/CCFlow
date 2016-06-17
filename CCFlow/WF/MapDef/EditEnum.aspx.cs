7ff18cbf8ea96e84e8cf6950e7e0513bdf99a894
ccflow代国强 <daiguoqiang@ccflow.org>
2016-06-17 11:36:49 +0800

    增加下拉框联动

--- CCFlow/WF/MapDef/EditEnum.aspx.cs
+++ CCFlow/WF/MapDef/EditEnum.aspx.cs
@@ -287,10 +287,8 @@ namespace CCFlow.WF.MapDef
                 isItem = this.Pub1.AddTR(isItem);
                 this.Pub1.AddTDIdx(idx++);
                 string html = "<a href=\"javascript:WinOpen('./MapExt/DDLFullCtrl.aspx?FK_MapData=" + mapAttr.FK_MapData + "&RefNo=" + mapAttr.KeyOfEn + "&MyPK=" + mapAttr.FK_MapData + "_" + MapExtXmlList.DDLFullCtrl + "_" + mapAttr.KeyOfEn + "')\">下拉框自动完成</a>";
-
+                html += " - <a href=\"javascript:WinOpen('./MapExt/ActiveDDL.aspx?FK_MapData=" + mapAttr.FK_MapData + "&RefNo=" + mapAttr.KeyOfEn + "&MyPK=" + MapExtXmlList.ActiveDDL + "_" + mapAttr.MyPK + "')\">设置级联动(如:省份,城市联动)</a>";
                 html += " - <a href=\"javascript:WinOpen('./MapExt/RadioBtns.aspx?FK_MapData=" + mapAttr.FK_MapData + "&KeyOfEn=" + mapAttr.KeyOfEn + "&MyPK=" + mapAttr.FK_MapData + "_" + MapExtXmlList.DDLFullCtrl + "_" + mapAttr.KeyOfEn + "')\">高级JS设置</a>";
-
-
                 this.Pub1.AddTD(html);
                 this.Pub1.AddTD("&nbsp;");
 

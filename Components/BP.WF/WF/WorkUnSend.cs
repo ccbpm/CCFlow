e93e9f6a61f0910a251db7b27a9412ea573f5014
ccflowdgq <daiguoqiang@ccflow.org>
2016-10-11 08:55:40 +0800

    去掉撤销无效判断

--- Components/BP.WF/WF/WorkUnSend.cs
+++ Components/BP.WF/WF/WorkUnSend.cs
@@ -367,14 +367,14 @@ namespace BP.WF
                     throw new Exception("分合流点不允许撤消。");
                 case NodeWorkType.WorkFL:
                     /*判断权限,当前的发送人是谁，如果是自己，就可以撤销.*/
-                    if (gwf.Sender.Contains("(" + WebUser.No + ",") || gwf.Sender == WebUser.No || gwf.Sender=="" )
-                    {
+                    //if (gwf.Sender.Contains("(" + WebUser.No + ",") || gwf.Sender == WebUser.No || gwf.Sender=="" )
+                    //{
 
-                    }
-                    else
-                    {
-                        throw new Exception("@分流点不是您发送的，您不能执行撤销。");
-                    }
+                    //}
+                    //else
+                    //{
+                    //    throw new Exception("@分流点不是您发送的，您不能执行撤销。");
+                    //}
                     //return this.DoUnSendFeiLiu(gwf);
                     break;
                 case NodeWorkType.StartWorkFL:

550897d53e5d6dc5387ffac0c6b9076775fb35a5
周朋 <hiflow@qq.com>
2016-10-12 18:43:53 +0800

    流程设计器 h5 代码重构.

---
+++ CCFlow/WF/Admin/Handler.ashx.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CCFlow.WF.Admin
+{
+    /// <summary>
+    /// Handler 的摘要说明
+    /// </summary>
+    public class Handler : IHttpHandler
+    {
+        #region 属性.
+        /// <summary>
+        /// 执行类型
+        /// </summary>
+        public string DoType
+        {
+            get
+            {
+                string str = context.Request.QueryString["DoType"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        public string MyPK
+        {
+            get
+            {
+                string str = context.Request.QueryString["MyPK"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        /// <summary>
+        /// 字典表
+        /// </summary>
+        public string FK_SFTable
+        {
+            get
+            {
+                string str = context.Request.QueryString["FK_SFTable"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+
+            }
+        }
+        public string EnumKey
+        {
+            get
+            {
+                string str = context.Request.QueryString["EnumKey"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+
+            }
+        }
+        public string KeyOfEn
+        {
+            get
+            {
+                string str = context.Request.QueryString["KeyOfEn"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        public string FK_MapData
+        {
+            get
+            {
+                string str = context.Request.QueryString["FK_MapData"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        /// <summary>
+        ///  节点ID.
+        /// </summary>
+        public int FK_Node
+        {
+            get
+            {
+                string str = context.Request.QueryString["FK_Node"];
+                if (str == null || str == "" || str == "null")
+                    return 0;
+                return int.Parse(str);
+            }
+        }
+        /// <summary>
+        /// 明细表
+        /// </summary>
+        public string FK_MapDtl
+        {
+            get
+            {
+                string str = context.Request.QueryString["FK_MapDtl"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        public HttpContext context = null;
+        /// <summary>
+        /// 获得表单的属性.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetValFromFrmByKey(string key)
+        {
+            string val = context.Request.Form[key];
+            if (val == null)
+                return null;
+            val = val.Replace("'", "~");
+            return val;
+        }
+        public int GetValIntFromFrmByKey(string key)
+        {
+            return int.Parse(this.GetValFromFrmByKey(key));
+        }
+        public bool GetValBoolenFromFrmByKey(string key)
+        {
+            string val = this.GetValFromFrmByKey(key);
+            if (val == null || val == "")
+                return false;
+            return true;
+        }
+        /// <summary>
+        /// 公共方法获取值
+        /// </summary>
+        /// <param name="param">参数名</param>
+        /// <returns></returns>
+        public string GetRequestVal(string param)
+        {
+            return HttpUtility.UrlDecode(context.Request[param], System.Text.Encoding.UTF8);
+        }
+        #endregion 属性.
+
+        public void ProcessRequest(HttpContext mycontext)
+        {
+            context = mycontext;
+            string msg = "";
+            try
+            {
+                switch (this.DoType)
+                {
+                    case "ReLoginSubmit": //重新登录.
+                        msg = this.ReLoginSubmit();
+                        break;
+                    default:
+                        msg = "err@没有判断的执行类型：" + this.DoType;
+                        break;
+                }
+                context.Response.Write(msg);
+            }
+            catch (Exception ex)
+            {
+                context.Response.Write("err@" + ex.Message);
+            }
+            //输出信息.
+        }
+
+        public string ReLoginSubmit()
+        {
+
+            string userNo = this.GetValFromFrmByKey("TB_UserNo");
+            string password = this.GetValFromFrmByKey("TB_Pass"); 
+
+            BP.Port.Emp emp = new BP.Port.Emp();
+            emp.No = userNo;
+            if (emp.RetrieveFromDBSources() == 0)
+                return "err@用户名或密码错误.";
+
+            if (emp.CheckPass(password) == false)
+                return "err@用户名或密码错误.";
+
+            BP.Web.WebUser.SignInOfGener(emp);
+
+            string url = context.Session["LastUrl"] as string;
+            if (string.IsNullOrEmpty(url) == true)
+            {
+                return "url@" + url;
+            }
+
+            url = context.Request["LastUrl"] as string;
+            url = url.Replace("[", "");
+            url = url.Replace("]", "");
+            return "url@" + url;
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}

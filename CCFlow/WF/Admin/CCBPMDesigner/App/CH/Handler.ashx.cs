179fd0b70036c6cbec8f0b66acefef1ad16bc9ec
周朋 <hiflow@qq.com>
2016-11-03 06:24:23 -0400

    no commit message

---
+++ CCFlow/WF/Admin/CCBPMDesigner/App/CH/Handler.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CCFlow.WF.Admin.CCBPMDesigner.App.CH
+{
+    /// <summary>
+    /// Handler 的摘要说明
+    /// </summary>
+    public class Handler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}

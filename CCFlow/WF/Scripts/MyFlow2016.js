3c5daf19fdc152160cd0c3fd95337bd6be04cab2
fly飘 <384082127@qq.com>
2016-11-08 02:13:54 -0500

    no commit message

--- CCFlow/WF/Scripts/MyFlow2016.js
+++ CCFlow/WF/Scripts/MyFlow2016.js
@@ -1047,7 +1047,12 @@ function SetAth(data) {
         $('#athModal').removeClass('in');
     }
     $('#athModal').hide();
-    $(window.frames["track0"]).contents().find(".modal-backdrop").hide();
+    var ifs = $("iframe[id^=track]").contents();
+    if (ifs.length > 0) {
+        for (var i = 0; i < ifs.length; i++) {
+            $(ifs[i]).find(".modal-backdrop").hide();
+        }
+    }
 }
 
 //查看页面的附件展示  查看页面调用

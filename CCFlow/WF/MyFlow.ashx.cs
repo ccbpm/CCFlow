0de20234c3ca7412566e484c4e0d7407ff17dc48
xiaoyang9002 <yyh9002@163.com>
2016-11-14 04:27:24 -0500

    no commit message

--- CCFlow/WF/MyFlow.ashx.cs
+++ CCFlow/WF/MyFlow.ashx.cs
@@ -815,9 +815,15 @@ namespace CCFlow.WF
             DataSet ds = new DataSet();
             if (this.DoType.ToUpper() != "VIEW")
             {
-
                 ds = BP.WF.CCFlowAPI.GenerWorkNode(this.FK_Flow, this.FK_Node, this.WorkID,
                     this.FID, BP.Web.WebUser.No);
+
+                //获取WF_GenerWorkFlow
+                string sql = string.Format("select work1.PFID,work1.PWorkID,work1.PNodeID,work1.PFlowNo,ISNULL(work2.PWorkID,0) PWorkID2,work2.PNodeID PNodeID2,work2.PFlowNo PFlowNo2,work1.FK_Flow,work1.FK_Node,work1.WorkID from WF_GenerWorkFlow work1 left join  WF_GenerWorkFlow work2 on  work1.FID=work2.WorkID where work1.WorkID='{0}'", WorkID);
+
+                DataTable wf_generWorkFlowDt = BP.DA.DBAccess.RunSQLReturnTable(sql);
+                wf_generWorkFlowDt.TableName = "WF_GenerWorkFlow";
+                ds.Tables.Add(wf_generWorkFlowDt);
             }
             DataTable trackDt = BP.WF.Dev2Interface.DB_GenerTrack(this.FK_Flow, this.WorkID, this.FID).Tables["Track"];
             ds.Tables.Add(trackDt.Copy());

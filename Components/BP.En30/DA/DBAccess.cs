30c55c409ec74208af176ff4f338f3f8ee6ab034
ccflowdgq <daiguoqiang@ccflow.org>
2016-11-15 07:07:47 -0500

    修改编码格式

--- Components/BP.En30/DA/DBAccess.cs
+++ Components/BP.En30/DA/DBAccess.cs
@@ -195,8 +195,7 @@ namespace BP.DA
         public static string GetBigTextFromDB(string tableName, string tablePK, string pkVal, string fileSaveField)
         {
             byte[] byteFile = GetByteFromDB(tableName, tablePK, pkVal, fileSaveField);
-            System.Text.UnicodeEncoding converter = new System.Text.UnicodeEncoding();
-            return converter.GetString(byteFile);
+            return System.Text.Encoding.UTF8.GetString(byteFile);
         }
         /// <summary>
         /// 从数据库里提取文件

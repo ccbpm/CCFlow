b426e47b8bd0137f8b13d11db9890906b08174ca
周朋 <hiflow@qq.com>
2016-11-15 10:20:45 +0800

    no commit message

---
+++ CCFlow/WF/Comm/RefFunc/Handler.ashx.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web;
+using BP.En;
+using BP.DA;
+using BP.Sys;
+
+namespace CCFlow.WF.Comm.RefFunc
+{
+    /// <summary>
+    /// Handler 的摘要说明
+    /// </summary>
+    public class Handler : IHttpHandler
+    {
+
+        #region 执行.
+        public HttpContext context = null;
+        /// <summary>
+        /// 执行类型
+        /// </summary>
+        public string DoType
+        {
+            get
+            {
+                string str = context.Request.QueryString["DoType"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public string MyPK
+        {
+            get
+            {
+                string str = context.Request.QueryString["MyPK"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public string No
+        {
+            get
+            {
+                string str = context.Request.QueryString["No"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        /// <summary>
+        /// 枚举值
+        /// </summary>
+        public string EnumKey
+        {
+            get
+            {
+                string str = context.Request.QueryString["EnumKey"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        /// <summary>
+        /// 实体 EnsName
+        /// </summary>
+        public string EnsName
+        {
+            get
+            {
+                string str = context.Request.QueryString["EnsName"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        public string SFTable
+        {
+            get
+            {
+                string str = context.Request.QueryString["SFTable"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        /// <summary>
+        /// 表单外键
+        /// </summary>
+        public string FK_MapData
+        {
+            get
+            {
+                string str = context.Request.QueryString["FK_MapData"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        #endregion 执行.
+
+        public void ProcessRequest(HttpContext mycontext)
+        {
+            context = mycontext;
+            string msg = "";
+            try
+            {
+                switch (this.DoType)
+                {
+                    case "Dot2DotSearch_Init": //初始化界面.
+                        break;
+                    default:
+                        msg = "err@没有判断的标记:" + this.DoType;
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = "err@" + ex.Message;
+            }
+
+            context.Response.ContentType = "text/plain";
+            context.Response.Write(msg);
+        }
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <returns></returns>
+        public string Dot2DotSearch_Init()
+        {
+            return "";
+        }
+
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}

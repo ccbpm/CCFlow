4cbac1ed0d0df9a62c0bf5cfe2aeed519f0e0004
周朋 <hiflow@qq.com>
2017-03-10 10:13:20 +0800

    no commit message

---
+++ Components/BP.WF/WebContral/WF_Admin_CCFormDesigner.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Data;
+using System.Web;
+using BP.WF;
+using BP.Web;
+using BP.Sys;
+using BP.DA;
+using BP.En;
+
+namespace BP.WF.WebContral
+{
+    public class WF_Admin_CCFormDesigner : WebContralBase
+    {
+        /// <summary>
+        /// 初始化数据
+        /// </summary>
+        /// <param name="mycontext"></param>
+        public WF_Admin_CCFormDesigner(HttpContext mycontext)
+        {
+            this.context = mycontext;
+        }
+
+
+        public  string Home_Int()
+        {
+            string no = this.GetRequestVal("No");
+
+            MapData md = new MapData(no);
+
+            // 基础信息.
+            Hashtable ht = new Hashtable();
+            ht.Add("No", no);
+            ht.Add("Name", md.Name);
+            ht.Add("PTable", md.PTable);
+            ht.Add("FrmTypeT", md.HisFrmTypeText);
+            ht.Add("FrmTreeName", md.FK_FormTreeText);
+
+            //统计信息.
+            ht.Add("SumDataNum", DBAccess.RunSQLReturnValInt("SELECT COUNT(*) FROM "+md.PTable )); //数据量.
+            ht.Add("SumAttrNum", DBAccess.RunSQLReturnValInt("SELECT COUNT(*) FROM Sys_MapAttr WHERE FK_MapData='" + no + "'")); //字段数量.
+            ht.Add("SumAttrFK", DBAccess.RunSQLReturnValInt("SELECT COUNT(*) FROM Sys_MapAttr WHERE FK_MapData='" + no + "' AND LGType=2 ")); //外键.
+            ht.Add("SumAttrEnum", DBAccess.RunSQLReturnValInt("SELECT COUNT(*) FROM Sys_MapAttr WHERE FK_MapData='" + no + "' AND LGType=1 ")); //外键.
+
+
+            ht.Add("MapFrmFrees","/WF/Comm/RefFunc/UIEn.aspx?EnsName=BP.WF.Template.MapFrmFrees&PK="+no ) ; //自由表单属性.
+            ht.Add("MapFrmFools", "/WF/Comm/RefFunc/UIEn.aspx?EnsName=BP.WF.Template.MapFrmFools&PK=" + no); //傻瓜表单属性.
+            ht.Add("MapFrmExcels", "/WF/Comm/RefFunc/UIEn.aspx?EnsName=BP.WF.Template.MapFrmExcels&PK=" + no); //傻瓜表单属性.
+            ht.Add("MapFrmFools", "/WF/Comm/RefFunc/UIEn.aspx?EnsName=BP.WF.Template.MapFrmFools&PK=" + no);  //Excel表单属性.
+            ht.Add("MapDataURLs", "/WF/Comm/RefFunc/UIEn.aspx?EnsName=BP.WF.Template.MapDataURLs&PK=" + no);  //嵌入式表单属性.
+
+            return BP.DA.DataType.ToJsonEntityModel(ht);
+        }
+
+        #region 字段列表 的操作
+        /// <summary>
+        /// 初始化字段列表.
+        /// </summary>
+        /// <returns></returns>
+        public string FiledsList_Init()
+        {
+            //DataSet ds = new DataSet();
+            MapAttrs attrs = new MapAttrs();
+            attrs.Retrieve(MapAttrAttr.FK_MapData, this.FK_MapData);
+            foreach (MapAttr item in attrs)
+            {
+                if (item.LGType == FieldTypeS.Enum)
+                {
+                    SysEnumMain se = new SysEnumMain(item.UIBindKey);
+                    item.UIRefKey = se.CfgVal;
+                    continue;
+                }
+
+                if (item.LGType == FieldTypeS.FK)
+                {
+                    item.UIRefKey = item.UIBindKey;
+                    continue;
+                }
+
+                item.UIRefKey = "无";
+            }
+            return attrs.ToJson();
+        }
+
+        /// <summary>
+        /// 删除字段
+        /// </summary>
+        /// <returns></returns>
+        public string FiledsList_Delete()
+        {
+            MapAttr attr = new MapAttr(this.MyPK);
+            if (attr.Delete() == 1)
+            {
+                return "删除成功！";
+            }
+            else
+            {
+                return "err@删除失败！";
+            }
+        }
+        #endregion 字段列表 的操作
+
+         
+    }
+}
---
+++ Components/BP.WF/WebContral/WebContralBase.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Collections.Generic;
+using System.Collections;
+using System.Data;
+using System.Web;
+using BP.WF;
+using BP.Web;
+using BP.Sys;
+using BP.DA;
+using BP.En;
+using System.Reflection;
+
+
+namespace BP.WF.WebContral
+{
+    abstract public class WebContralBase
+    {
+        #region 属性.
+        /// <summary>
+        /// 执行类型
+        /// </summary>
+        public string DoType
+        {
+            get
+            {
+                string str = context.Request.QueryString["DoType"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        public string MyPK
+        {
+            get
+            {
+                string str = context.Request.QueryString["MyPK"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        /// <summary>
+        /// 字典表
+        /// </summary>
+        public string FK_SFTable
+        {
+            get
+            {
+                string str = context.Request.QueryString["FK_SFTable"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+
+            }
+        }
+        public string EnumKey
+        {
+            get
+            {
+                string str = context.Request.QueryString["EnumKey"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+
+            }
+        }
+        public string KeyOfEn
+        {
+            get
+            {
+                string str = context.Request.QueryString["KeyOfEn"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        public string FK_MapData
+        {
+            get
+            {
+                string str = context.Request.QueryString["FK_MapData"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        public int GroupField
+        {
+            get
+            {
+                string str = context.Request.QueryString["GroupField"];
+                if (str == null || str == "" || str == "null")
+                    return 0;
+                return int.Parse(str);
+            }
+        }
+
+        /// <summary>
+        /// 框架ID
+        /// </summary>
+        public string FK_MapFrame
+        {
+            get
+            {
+                string str = context.Request.QueryString["FK_MapFrame"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+        /// <summary>
+        ///  节点ID.
+        /// </summary>
+        public int FK_Node
+        {
+            get
+            {
+                string str = context.Request.QueryString["FK_Node"];
+                if (str == null || str == "" || str == "null")
+                    return 0;
+                return int.Parse(str);
+            }
+        }
+        /// <summary>
+        ///   RefOID
+        /// </summary>
+        public int RefOID
+        {
+            get
+            {
+                string str = context.Request.QueryString["RefOID"];
+                if (str == null || str == "" || str == "null")
+                    return 0;
+                return int.Parse(str);
+            }
+        }
+        /// <summary>
+        /// 明细表
+        /// </summary>
+        public string FK_MapDtl
+        {
+            get
+            {
+                string str = context.Request.QueryString["FK_MapDtl"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+
+        /// <summary>
+        /// 字段属性编号
+        /// </summary>
+        public string Ath
+        {
+            get
+            {
+                string str = context.Request.QueryString["Ath"];
+                if (str == null || str == "" || str == "null")
+                    return null;
+                return str;
+            }
+        }
+
+        public HttpContext context = null;
+        /// <summary>
+        /// 获得表单的属性.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetValFromFrmByKey(string key)
+        {
+            string val = context.Request.Form[key];
+            if (val == null)
+                return null;
+            val = val.Replace("'", "~");
+            return val;
+        }
+        public int GetValIntFromFrmByKey(string key)
+        {
+            string str = this.GetValFromFrmByKey(key);
+            if (str == null || str == "")
+                throw new Exception("@参数:" + key + " 没有取到值.");
+
+            return int.Parse(str);
+        }
+        public bool GetValBoolenFromFrmByKey(string key)
+        {
+            string val = this.GetValFromFrmByKey(key);
+            if (val == null || val == "")
+                return false;
+            return true;
+        }
+        /// <summary>
+        /// 公共方法获取值
+        /// </summary>
+        /// <param name="param">参数名</param>
+        /// <returns></returns>
+        public string GetRequestVal(string param)
+        {
+            return HttpUtility.UrlDecode(context.Request[param], System.Text.Encoding.UTF8);
+        }
+        /// <summary>
+        /// 公共方法获取值
+        /// </summary>
+        /// <param name="param">参数名</param>
+        /// <returns></returns>
+        public int GetRequestValInt(string param)
+        {
+            string str = GetRequestVal(param);
+            if (str == null || str == "")
+                return 0;
+            return int.Parse(str);
+        }
+        #endregion 属性.
+
+
+        public string DoMethod(object myEn,  string methodName)
+        {
+            Type tp = myEn.GetType();
+            MethodInfo mp = tp.GetMethod(methodName);
+            if (mp == null)
+                throw new Exception("@对象实例[" + tp.FullName + "]中没有找到方法[" + methodName + "]！");
+
+            object[] paras = null;
+            try
+            {
+                return mp.Invoke(this, paras) as string;  //调用由此 MethodInfo 实例反射的方法或构造函数。
+            }
+            catch (System.Reflection.TargetException ex)
+            {
+                string strs = "";
+                if (paras == null)
+                {
+                    throw new Exception(ex.Message);
+                }
+                else
+                {
+                    foreach (object obj in paras)
+                    {
+                        strs += "para= " + obj.ToString() + " type=" + obj.GetType().ToString() + "\n<br>";
+                    }
+                }
+                throw new Exception(ex.Message + "  more info:" + strs);
+            }
+        }
+    }
+}

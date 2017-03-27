0affc02a5a81ad048d179d0281829cc5bc97eb3d
yyh9002 <1061400090@qq.com>
2017-03-27 22:15:32 +0800

    no commit message

--- Components/BP.WF/HttpHandler/WF_Admin_FoolFormDesigner_ImpExp.cs
+++ Components/BP.WF/HttpHandler/WF_Admin_FoolFormDesigner_ImpExp.cs
@@ -44,7 +44,7 @@ namespace BP.WF.HttpHandler
             }
 
             //加入表单库目录.
-            sql = "SELECT 'ND'+NO,NAME,PARENTNO FROM Sys_FormTree ORDER BY  PARENTNO IDX ";
+            sql = "SELECT 'ND'+NO,NAME,PARENTNO FROM Sys_FormTree ORDER BY  PARENTNO, IDX ";
             dt = BP.DA.DBAccess.RunSQLReturnTable(sql);
             dt.TableName = "Sys_FormTree";
             ds.Tables.Add(dt);
@@ -57,7 +57,125 @@ namespace BP.WF.HttpHandler
 
             return BP.Tools.Json.ToJson(ds);
         }
+
+        /// <summary>
+        /// 从本机装载表单模版
+        /// </summary>
+        /// <param name="fileByte">文件流</param>
+        /// <param name="fk_mapData">表单模版ID</param>
+        /// <param name="isClear">是否清空？</param>
+        /// <returns>执行结果</returns>
+        public string Imp_LoadFrmTempleteFromLocalFile()
+        {
+            byte[] fileByte=new byte[0];
+            string fk_mapData = this.FK_MapData;
+            //文件路径.
+            string file = "\\Temp\\" + fk_mapData + ".xml";
+            string path = System.Web.HttpContext.Current.Request.PhysicalApplicationPath + file;
+
+            //保存到服务器，指定的位置.
+            BP.DA.DataType.WriteFile(path, fileByte);
+
+            //读取文件.
+            DataSet ds = new DataSet();
+            ds.ReadXml(file);
+
+            //执行装载.
+            MapData.ImpMapData(fk_mapData, ds, true);
+
+            if (fk_mapData.Contains("ND"))
+            {
+                /* 判断是否是节点表单 */
+                int nodeID = 0;
+                try
+                {
+                    nodeID = int.Parse(fk_mapData.Replace("ND", ""));
+                }
+                catch
+                {
+                    return "执行成功.";
+                }
+
+                Node nd = new Node(nodeID);
+                nd.RepareMap();
+            }
+            return "执行成功.";
+        }
+        /// <summary>
+        /// 从节点上Copy
+        /// </summary>
+        /// <param name="fromMapData">从表单ID</param>
+        /// <param name="fk_mapdata">到表单ID</param>
+        /// <param name="isClear">是否清楚现有的元素？</param>
+        /// <param name="isSetReadonly">是否设置为只读？</param>
+        /// <returns>执行结果</returns>
+        public string Imp_CopyFrm()
+        {
+            string fromMapData = this.FromMapData;
+            string fk_mapdata = this.FK_MapData;
+            bool isClear = this.IsClear;
+            bool isSetReadonly = this.IsSetReadonly;
+
+            MapData md = new MapData(fromMapData);
+
+            MapData.ImpMapData(fk_mapdata, BP.Sys.CCFormAPI.GenerHisDataSet(md.No), isSetReadonly);
+
+            // 如果是节点表单，就要执行一次修复，以免漏掉应该有的系统字段。
+            if (fk_mapdata.Contains("ND") == true)
+            {
+                string fk_node = fk_mapdata.Replace("ND", "");
+                Node nd = new Node(int.Parse(fk_node));
+                nd.RepareMap();
+            }
+            return "执行成功.";
+        }
+
         #endregion
 
+
+        public string FK_MapData
+        {
+            get
+            {
+                return context.Request.QueryString["FK_MapData"];
+            }
+        }
+        public bool IsClear
+        {
+            get
+            {
+                string isClearStr = context.Request.QueryString["FK_MapData"];
+                bool isClear = false;
+                if (!string.IsNullOrEmpty(isClearStr) && isClearStr.ToString().ToLower() == "on")
+                {
+                    isClear = true;
+                }
+                return isClear;
+            }
+        }
+
+        public bool IsSetReadonly
+        {
+            get
+            {
+                string isSetReadonlyStr = context.Request.QueryString["IsSetReadonly"];
+                bool isSetReadonly = false;
+                if (!string.IsNullOrEmpty(isSetReadonlyStr) && isSetReadonlyStr.ToString().ToLower() == "on")
+                {
+                    isSetReadonly = true;
+                }
+                return isSetReadonly;
+            }
+        }
+
+
+        public string FromMapData
+        {
+            get
+            {
+                string fromMapData = context.Request.QueryString["FromMapData"];
+                return fromMapData;
+            }
+        }
     }
 }

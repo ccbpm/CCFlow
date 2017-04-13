76c8557b5c829fe4d29653af83ca807996d01f4c
驰骋BPM <zhoupeng@ccflow.org>
2017-04-13 10:51:52 +0800

    no commit message

--- CCFlow/WF/Admin/AttrFlow/CheckFlow.htm.cs
+++ CCFlow/WF/Admin/AttrFlow/CheckFlow.htm.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace CCFlow.WF.Admin.AttrFlow
 {
-    public partial class CheckFlow : System.Web.UI.Page
+    public partial class CheckFlow1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {

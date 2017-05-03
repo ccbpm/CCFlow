ecce9ae3a9e6a4af3655bf055a7b4e1ced61e829
gzingu <i@gzingu.com>
2017-05-03 17:37:28 +0800

    增加 下载文件时判断文件是否存在 的功能

--- CCFlow/WF/Comm/Do.aspx.cs
+++ CCFlow/WF/Comm/Do.aspx.cs
@@ -19,65 +19,74 @@ namespace CCFlow.Web.Comm
 	/// <summary>
 	/// Do 的摘要说明。
 	/// </summary>
-	public partial class Do : BP.Web.WebPage 
+	public partial class Do : BP.Web.WebPage
 	{
 		public ActionType GetActionType
 		{
 			get
 			{
-				return (ActionType)int.Parse( this.Request.QueryString["ActionType"] ); 
+				return (ActionType)int.Parse(this.Request.QueryString["ActionType"]);
 			}
 		}
 		protected void Page_Load(object sender, System.EventArgs e)
 		{
-            switch (this.DoType)
-            {
-                case "DelGradeEns":
-                    Entity enGrade = BP.En.ClassFactory.GetEns(this.EnsName).GetNewEntity;
-                    enGrade.PKVal = this.RefPK;
-                    enGrade.Delete();
-                    this.WinClose();
-                    return;
-                case "DownFile":
-                    Entity enF = BP.En.ClassFactory.GetEn(this.EnName);
-                    enF.PKVal = this.Request.QueryString["PK"];
-                    enF.Retrieve();
-                    string pPath = enF.GetValStringByKey("MyFilePath") + "\\" + enF.PKVal + "." + enF.GetValStringByKey("MyFileExt");
-                    BP.Sys.PubClass.DownloadFile(pPath,
-                        enF.GetValStringByKey("MyFileName"));
-                    this.WinClose();
-                    return;
-                default:
-                    break;
-            }
-			switch(this.GetActionType)
+			switch (this.DoType)
+			{
+				case "DelGradeEns":
+					Entity enGrade = BP.En.ClassFactory.GetEns(this.EnsName).GetNewEntity;
+					enGrade.PKVal = this.RefPK;
+					enGrade.Delete();
+					this.WinClose();
+					return;
+				case "DownFile":
+					Entity enF = BP.En.ClassFactory.GetEn(this.EnName);
+					enF.PKVal = this.Request.QueryString["PK"];
+					enF.Retrieve();
+					string pPath = enF.GetValStringByKey("MyFilePath") + "\\" + enF.PKVal + "." + enF.GetValStringByKey("MyFileExt");
+
+					//判断文件是否存在
+					if (!System.IO.File.Exists(pPath))
+					{
+						Response.Write("<script>alert('文件不存在！');</script>");
+						this.WinClose();
+						return;
+					}
+
+					BP.Sys.PubClass.DownloadFile(pPath,
+						enF.GetValStringByKey("MyFileName"));
+					this.WinClose();
+					return;
+				default:
+					break;
+			}
+			switch (this.GetActionType)
 			{
 				case ActionType.DeleteFile:
-					SysFileManager sysfile = new SysFileManager( int.Parse(this.Request.QueryString["OID"]) );
+					SysFileManager sysfile = new SysFileManager(int.Parse(this.Request.QueryString["OID"]));
 					sysfile.Delete();
 					break;
 				case ActionType.PrintEnBill:
-					string className=this.Request.QueryString["MainEnsName"];
-					Entity en =ClassFactory.GetEns(className).GetNewEntity;
+					string className = this.Request.QueryString["MainEnsName"];
+					Entity en = ClassFactory.GetEns(className).GetNewEntity;
 					try
 					{
-						en.PKVal=this.Request.QueryString["PK"];
+						en.PKVal = this.Request.QueryString["PK"];
 						en.Retrieve();
 					}
 					catch
 					{
-						en.PKVal=this.Request.QueryString[en.PK];
+						en.PKVal = this.Request.QueryString[en.PK];
 						en.Retrieve();
 					}
 					//this.GenerRptByPeng(en);
 					break;
 				default:
-					throw new Exception("do error"+this.GetActionType);
+					throw new Exception("do error" + this.GetActionType);
 			}
 
 
 			this.WinClose();
-			
+
 		}
 
 		#region Web 窗体设计器生成的代码
@@ -89,13 +98,13 @@ namespace CCFlow.Web.Comm
 			InitializeComponent();
 			base.OnInit(e);
 		}
-		
+
 		/// <summary>
 		/// 设计器支持所需的方法 - 不要使用代码编辑器修改
 		/// 此方法的内容。
 		/// </summary>
 		private void InitializeComponent()
-		{    
+		{
 		}
 		#endregion
 	}

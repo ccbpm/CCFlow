fed4a8eb149a774fc0b79a912041b7745fe0a283
于庆海 <438704315@qq.com>
2017-05-03 17:38:16 +0800

    我的关注

--- Components/BP.WF/HttpHandler/WF_App_ACE.cs
+++ Components/BP.WF/HttpHandler/WF_App_ACE.cs
@@ -202,5 +202,100 @@ namespace BP.WF.HttpHandler
             return BP.Tools.Json.ToJson(dt);
         }
         #endregion 草稿.
+
+        #region 抄送流程.
+        /// <summary>
+        /// 抄送流程
+        /// </summary>
+        /// <returns></returns>
+        public string cc_Init()
+        {
+            string sta = this.GetRequestVal("Sta");
+            if (sta == null || sta == "")
+                sta = "-1";
+
+            int pageSize = 6;// int.Parse(pageSizeStr);
+
+            string pageIdxStr = this.GetRequestVal("PageIdx");
+            if (pageIdxStr == null)
+                pageIdxStr = "1";
+            int pageIdx = int.Parse(pageIdxStr);
+
+            //实体查询.
+            BP.WF.SMSs ss = new BP.WF.SMSs();
+            BP.En.QueryObject qo = new BP.En.QueryObject(ss);
+
+            System.Data.DataTable dt = null;
+            if (sta == "-1")
+                dt = BP.WF.Dev2Interface.DB_CCList(BP.Web.WebUser.No);
+            if (sta == "0")
+                dt = BP.WF.Dev2Interface.DB_CCList_UnRead(BP.Web.WebUser.No);
+            if (sta == "1")
+                dt = BP.WF.Dev2Interface.DB_CCList_Read(BP.Web.WebUser.No);
+            if (sta == "2")
+                dt = BP.WF.Dev2Interface.DB_CCList_Delete(BP.Web.WebUser.No);
+
+            int allNum = qo.GetCount();
+            qo.DoQuery(BP.WF.SMSAttr.MyPK, pageSize, pageIdx);
+
+            //绑定分页
+            // this.Pub1.BindPageIdx(allNum, pageSize, pageIdx, "CC.aspx?Sta=" + sta);
+
+            /*BP.WF.XML.CCMenus tps = new BP.WF.XML.CCMenus();
+            tps.RetrieveAll();
+            string link = ""; // "<a href='?MsgType=All'>全部</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+            foreach (BP.WF.XML.CCMenu tp in tps)
+            {
+                link += "<a href='?Sta=" + tp.No + "'>" + tp.Name + "</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+            }*/
+
+            return BP.Tools.Json.ToJsonUpper(dt);
+        }
+        #endregion 抄送.
+
+        #region 我的关注流程.
+        /// <summary>
+        /// 我的关注流程
+        /// </summary>
+        /// <returns></returns>
+        public string Focus_Init()
+        {
+            string flowNo = this.GetRequestVal("FK_Flow");
+
+            int idx = 0;
+            //获得关注的数据.
+            System.Data.DataTable dt = BP.WF.Dev2Interface.DB_Focus(flowNo, BP.Web.WebUser.No);
+            SysEnums stas = new SysEnums("WFSta");
+            string[] tempArr;
+            foreach (System.Data.DataRow dr in dt.Rows)
+            {
+                int wfsta = int.Parse(dr["WFSta"].ToString());
+                //edit by liuxc,2016-10-22,修复状态显示不正确问题
+                string wfstaT = (stas.GetEntityByKey(SysEnumAttr.IntKey, wfsta) as SysEnum).Lab;
+                string currEmp = string.Empty;
+
+                if (wfsta != (int)BP.WF.WFSta.Complete)
+                {
+                    //edit by liuxc,2016-10-24,未完成时，处理当前处理人，只显示处理人姓名
+                    foreach (string emp in dr["ToDoEmps"].ToString().Split(';'))
+                    {
+                        tempArr = emp.Split(',');
+
+                        currEmp += tempArr.Length > 1 ? tempArr[1] : tempArr[0] + ",";
+                    }
+
+                    currEmp = currEmp.TrimEnd(',');
+
+                    //currEmp = dr["ToDoEmps"].ToString();
+                    //currEmp = currEmp.TrimEnd(';');
+                }
+                dr["ToDoEmps"] = currEmp;
+                dr["FlowNote"] = wfstaT;
+                dr["AtPara"] = (wfsta == (int)BP.WF.WFSta.Complete ? dr["Sender"].ToString().TrimStart('(').TrimEnd(')').Split(',')[1] : "");
+            }
+            return BP.Tools.Json.ToJsonUpper(dt) ;
+        }
+        #endregion 我的关注.
     }
+   
 }

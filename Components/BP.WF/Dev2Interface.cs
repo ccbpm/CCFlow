0eaa8a8844444e7de9e08b8f445ae1a3293c848a
张庆鹏 <zhangqingpeng@ccflow.org>
2017-05-04 18:21:37 +0800

    为烟台纵横添加，可以根据部门与岗位交集，获取流程发起权限

--- Components/BP.WF/Dev2Interface.cs
+++ Components/BP.WF/Dev2Interface.cs
@@ -848,9 +848,16 @@ namespace BP.WF
             if (BP.Sys.SystemConfig.OSDBSrc == OSDBSrc.Database)
             {
                 string sql = "";
-                // 采用新算法.
+                // 采用新算法.   为烟台纵横添加，可以根据部门与岗位交集，获取流程发起权限 by  zqp
                 if (BP.WF.Glo.OSModel == BP.Sys.OSModel.OneOne)
+                {
                     sql = "SELECT FK_Flow FROM V_FlowStarter WHERE FK_Emp='" + userNo + "'";
+                    sql += " UNION ";
+                    sql += "select FK_FLOW from wf_node where nodeid in";
+                    sql += "(SELECT a.fk_node FROM WF_NodeDept A, Port_EmpDept B, WF_NodeStation C, Port_EmpStation D "
+                    + " WHERE A.FK_Dept= B.FK_Dept AND B.FK_Emp='" + userNo + "' AND  A.FK_Node=C.FK_Node AND C.FK_Station=D.FK_Station AND D.FK_Emp='" + userNo + "')";
+                }
+
                 else
                     sql = "SELECT FK_Flow FROM V_FlowStarterBPM WHERE FK_Emp='" + userNo + "'";
 

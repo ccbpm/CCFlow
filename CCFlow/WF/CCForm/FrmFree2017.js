e6633835df2142f56cde69779c1f148b4edd5515
yyh9002 <1061400090@qq.com>
2017-05-16 00:03:50 +0800

    no commit message

--- CCFlow/WF/CCForm/FrmFree2017.js
+++ CCFlow/WF/CCForm/FrmFree2017.js
@@ -2421,6 +2421,11 @@ function figure_MapAttr_Template( mapAttr) {
     eleHtml = $('<div>' + eleHtml + '</div>');
     eleHtml.children(0).css('width', mapAttr.UIWidth).css('height', mapAttr.UIHeight);
     eleHtml.css('position', 'absolute').css('top', mapAttr.Y).css('left', mapAttr.X);
+
+    if (mapAttr.UIIsEnable == "0") {
+        enableAttr = eleHtml.find('[name=TB_' + mapAttr.KeyOfEn + ']').attr('disabled', true);
+        enableAttr = eleHtml.find('[name=DDL_' + mapAttr.KeyOfEn + ']').attr('disabled', true);
+    }
     return eleHtml;
 }
 

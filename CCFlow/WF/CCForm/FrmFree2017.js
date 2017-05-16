e3fbe3fac5eaf08d369e3ea0cc722f383e62c505
驰骋BPM <zhoupeng@ccflow.org>
2017-05-16 22:59:24 +0800

    no commit message

--- CCFlow/WF/CCForm/FrmFree2017.js
+++ CCFlow/WF/CCForm/FrmFree2017.js
@@ -105,19 +105,7 @@ function KindEditerSync() {
     catch (err) {
     }
 }
-
-function Shift() {
-    //  var url = '/WF/WorkOpt/Forward.htm';
-    //window.open(url);
-}
-
-function ReturnWork() {
-    //var url = '/WF/WorkOpt/ReturnWork.htm';
-    //window.open(url);
-}
-
-
-
+ 
 // ccform 为开发者提供的内置函数. 
 // 获取DDL值 
 function ReqDDL(ddlID) {
@@ -259,14 +247,7 @@ function WinOpen(url, winName) {
     return;
 }
 
-
-function DoDelSubFlow(fk_flow, workid) {
-    if (window.confirm('您确定要终止进程吗？') == false)
-        return;
-    var url = 'Do.aspx?DoType=DelSubFlow&FK_Flow=' + fk_flow + '&WorkID=' + workid;
-    WinShowModalDialog(url, '');
-    window.location.href = window.location.href; //aspxPage + '.aspx?WorkID=';
-}
+ 
 function Do(warning, url) {
     if (window.confirm(warning) == false)
         return;
@@ -461,102 +442,7 @@ function pageParamToUrl() {
     }
     return paramUrlStr;
 }
-//初始化按钮
-function initBar() {
-    $.ajax({
-        type: 'post',
-        async: true,
-        data: pageData,
-        url: "../MyFlow.ashx?DoType=InitToolBar&m=" + Math.random(),
-        dataType: 'html',
-        success: function (data) {
-            var barHtml = data;
-            $('.Bar').html(barHtml);
-            if ($('[name=Return]').length > 0) {
-                $('[name=Return]').attr('onclick', '');
-                $('[name=Return]').unbind('click');
-                $('[name=Return]').bind('click', function () { initModal("returnBack"); $('#returnWorkModal').modal().show(); });
-            }
-            if ($('[name=Shift]').length > 0) {
-
-                $('[name=Shift]').attr('onclick', '');
-                $('[name=Shift]').unbind('click');
-                $('[name=Shift]').bind('click', function () { initModal("shift"); $('#returnWorkModal').modal().show(); });
-            }
-            if ($('[name=Askfor]').length > 0) {
-                $('[name=Askfor]').attr('onclick', '');
-                $('[name=Askfor]').unbind('click');
-                $('[name=Askfor]').bind('click', function () { initModal("askfor"); $('#returnWorkModal').modal().show(); });
-            }
-            if ($('[name=SelectAccepter]').length > 0) {
-                $('[name=SelectAccepter]').attr('onclick', '');
-                $('[name=SelectAccepter]').unbind('click');
-                $('[name=SelectAccepter]').bind('click', function () { initModal("accepter"); $('#returnWorkModal').modal().show(); });
-            }
-            if ($('[name=Delete]').length > 0) {
-                var onclickFun = $('[name=Delete]').attr('onclick');
-                if (onclickFun != undefined) {
-                    $('[name=Delete]').attr('onclick', onclickFun.replace('MyFlowInfo.htm', 'MyFlowInfo.aspx'));
-                }
-            }
-        }
-    });
-}
-
-//初始化退回、移交、加签窗口
-function initModal(modalType, toNode) {
-    //初始化退回窗口的SRC
-    var returnWorkModalHtml = '<div class="modal fade" id="returnWorkModal" data-backdrop="static">' +
-       '<div class="modal-dialog">'
-           + '<div class="modal-content" style="border-radius:0px;width:700px;text-align:left;">'
-              + '<div class="modal-header">'
-                  + '<button type="button" style="color:white;float: right;background: transparent;border: none;" data-dismiss="modal" aria-hidden="true">&times;</button>'
-                   + '<h4 class="modal-title" id="modalHeader">工作退回</h4>'
-               + '</div>'
-               + '<div class="modal-body">'
-                   + '<iframe style="width:100%;border:0px;height:400px;" id="iframeReturnWorkForm" name="iframeReturnWorkForm"></iframe>'
-               + '</div>'
-           + '</div><!-- /.modal-content -->'
-       + '</div><!-- /.modal-dialog -->'
-   + '</div>';
-
-    $('body').append($(returnWorkModalHtml));
-
-    var modalIframeSrc = '';
-    if (modalType != undefined) {
-        switch (modalType) {
-            case "returnBack":
-                $('#modalHeader').text("工作退回");
-                modalIframeSrc = "../WF/WorkOpt/ReturnWork.htm?FK_Node=" + pageData.FK_Node + "&FID=" + pageData.FID + "&WorkID=" + pageData.WorkID + "&FK_Flow=" + pageData.FK_Flow + "&s=" + Math.random()
-                break;
-            case "shift":
-                $('#modalHeader').text("工作移交");
-                modalIframeSrc = "../WF/WorkOpt/forward.htm?FK_Node=" + pageData.FK_Node + "&FID=" + pageData.FID + "&WorkID=" + pageData.WorkID + "&FK_Flow=" + pageData.FK_Flow + "&Info=&s=" + Math.random()
-                break;
-            case "askfor":
-                $('#modalHeader').text("加签");
-                modalIframeSrc = "../WF/WorkOpt/Askfor.htm?FK_Node=" + pageData.FK_Node + "&FID=" + pageData.FID + "&WorkID=" + pageData.WorkID + "&FK_Flow=" + pageData.FK_Flow + "&Info=&s=" + Math.random()
-                break;
-            case "accepter":
-                $('#modalHeader').text("选择下一个节点及下一个节点接受人");
-                modalIframeSrc = "../WF/WorkOpt/Accepter.htm?FK_Node=" + pageData.FK_Node + "&FID=" + pageData.FID + "&WorkID=" + pageData.WorkID + "&FK_Flow=" + pageData.FK_Flow + "&s=" + Math.random()
-                break;
-                //发送选择接收节点和接收人
-            case "sendAccepter":
-                $('#modalHeader').text("发送到节点：" + toNode.Name);
-                modalIframeSrc = "../WF/WorkOpt/Accepter.htm?FK_Node=" + pageData.FK_Node + "&FID=" + pageData.FID + "&WorkID=" + pageData.WorkID + "&FK_Flow=" + pageData.FK_Flow + "&ToNode=" + toNode.No + "&s=" + Math.random()
-                break;
-            default:
-                break;
-        }
-    }
-    $('#iframeReturnWorkForm').attr('src', modalIframeSrc);
-}
-
-//退回操作  显示退回窗口
-function showReturnWorkModel() {
-    $('#returnWorkModal').modal().show();
-}
+  
 //设置附件为只读
 function setAttachDisabled() {
     //附件设置
@@ -567,6 +453,7 @@ function setAttachDisabled() {
         }
     })
 }
+
 //隐藏下方的功能按钮
 function setToobarUnVisible() {
     //隐藏下方的功能按钮
@@ -592,7 +479,6 @@ function setFormEleDisabled() {
     $('#divCCForm select').attr('disabled', 'disabled');
     $('#divCCForm input[type!=button]').attr('disabled', 'disabled');
 }
-
 
 //保存
 function Save() {
@@ -1661,77 +1547,7 @@ function getFormData(isCotainTextArea, isCotainUrlParam) {
         dataArr.push(formss);
     var formData = dataArr.join('&');
     return formData;
-}
-//发送
-function Send() {
-    //比填写检查
-    //必填项和正则表达式检查
-    var formCheckResult = true;
-    if (!checkBlanks()) {
-        formCheckResult = false;
-    }
-    if (!checkReg()) {
-        formCheckResult = false;
-    }
-    if (!formCheckResult) {
-        //alert("表单填写不正常，请检查！！！");
-        return;
-    }
-    var toNode = 0;
-    //含有发送节点 且接收
-    if ($('#DDL_ToNode').length > 0) {
-        var selectToNode = $('#DDL_ToNode  option:selected').data();
-        if (selectToNode.IsSelectEmps == "1") {//跳到选择接收人窗口
-            initModal("sendAccepter", selectToNode);
-            $('#returnWorkModal').modal().show();
-            return false;
-        } else {
-            toNode = selectToNode.No;
-        }
-    }
-    execSend(toNode);
-}
-
-function execSend(toNode) {
-    //先设置按钮等不可用
-    setToobarDisiable();
-
-    $.ajax({
-        type: 'post',
-        async: true,
-        data: getFormData(true, true) + "&ToNode=" + toNode,
-        url: "../MyFlow.ashx?DoType=Send",
-        dataType: 'html',
-        success: function (data) {
-            if (data.indexOf('err@') == 0) {//发送时发生错误
-                $('#Message').html(data.substring(4, data.length));
-                $('.Message').show();
-                setToobarEnable();
-            }
-            else if (data.indexOf('url@') == 0) {//发送成功时转到指定的URL 
-                var url = data;
-                url = url.replace('url@', '');
-                window.location.href = url;
-                // WinOpen(url, 'ss');
-                // $('#Message').html("<a href=" + data.substring(4, data.length) + ">待处理</a>");
-                // $('.Message').show();
-            }
-            else {
-                OptSuc(data);
-                if (opener != null && opener.window != null && opener.window.parent != null && opener.window.parent.refSubSubFlowIframe != null && typeof (opener.window.parent.refSubSubFlowIframe) == "function") {
-                    opener.window.parent.refSubSubFlowIframe();
-                }
-                //if (window.opener != null && window.opener != undefined && window.opener)
-                //    $('#Message').html(data);
-                //$('.Message').show();
-                ////发送成功时
-                //setAttachDisabled();
-                //setToobarUnVisible();
-                //setFormEleDisabled();
-            }
-        }
-    });
-}
+} 
 
 $(function () {
     $('#btnMsgModalOK').bind('click', function () {
@@ -2067,15 +1883,18 @@ function SaveDtlAll() {
 }
 
 //将v1版本表单元素转换为v2 杨玉慧  silverlight 自由表单转化为H5表单
-function GenerWorkNode() {
+function GenerFreeFrm() {
+
     var href = window.location.href;
     var urlParam = href.substring(href.indexOf('?') + 1, href.length);
+    urlParam = urlParam.replace('&DoType=', '&DoTypeDel=xx');
+
     $.ajax({
         type: 'post',
         async: true,
         data: pageData,
         //url: "../MyFlow.ashx?DoType=GenerWorkNode&DoType=" + pageData.DoType + "&m=" + Math.random(),
-        url: "Handler.ashx?DoType=FrmFree_Init" + "&m=" + Math.random() + "&" + urlParam,
+        url: Handler + "?DoType=FrmFree_Init&m=" + Math.random() + "&" + urlParam,
        // url:"Handler.ashx?DoType=FrmFree_Init&FK_MapData="+pageData.FK_MapData + "&m=" + Math.random(),
         dataType: 'html',
         success: function (data) {

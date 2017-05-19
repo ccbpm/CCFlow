ba55246c9f96df81dea9facc0d143d0ecff9835a
ccflowdgq <daiguoqiang@ccflow.org>
2017-05-19 15:52:36 +0800

    打印在线用WebOffice打开

--- CCFlow/WF/WebOffice/PrintOffice.aspx.designer.cs
+++ CCFlow/WF/WebOffice/PrintOffice.aspx.designer.cs
@@ -10,15 +10,6 @@
 namespace CCFlow.WF.WebOffice {
     
     
-    public partial class Default {
-        
-        /// <summary>
-        /// form1 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    public partial class PrintOffice {
     }
 }

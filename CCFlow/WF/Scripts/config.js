576688da1b13ec1b9e2dd05155ddf5e69edc3672
yyh9002 <1061400090@qq.com>
2017-05-29 09:21:38 +0800

    no commit message

--- CCFlow/WF/Scripts/config.js
+++ CCFlow/WF/Scripts/config.js
@@ -38,4 +38,46 @@ function Handler_AjaxPostData(param, callback, scope) {
 // var plant = 'JFlow'; //运行平台.
 // var Handler = "Handler";
 
-  
+ 
+var load = {};
+
+/*JS 动态加载js 文件*/
+load.LoadJs = function (jsPath, chartset, callbackfun) {
+    try {
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.src = jsPath + "?Version=" + load.Version;
+        script.type = 'text/javascript';
+        if (chartset != undefined) {
+            script.chartset = chartset;
+        }
+
+        if (script.readyState) {
+            script.onreadystatechange = function () {
+                if (script.readyState == "loaded" || script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                    if (callbackfun != undefined && typeof (callbackfun) == 'function') {
+                        callbackfun();
+                    }
+                }
+            }
+        } else {
+            script.onload = function () {
+                if (callbackfun != undefined && typeof (callbackfun) == 'function') {
+                    callbackfun();
+                }
+            }
+        }
+        head.appendChild(script);
+    }
+    catch (err) {
+        alert(err)
+    }
+
+    //$.getScript(jsPath, callbackfun);
+}
+
+var jsPathArrVersion = {};
+/*js 和CSS 的版本号*/
+load.Version = '0412145201';
+

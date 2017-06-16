d7423afb0ec0d4f88a66adacec508cdcc4c5ae89
gzingu <i@gzingu.com>
2017-06-16 15:10:49 +0800

    no commit message

--- CCFlow/WF/CCForm/CCFormAPI.asmx.cs
+++ CCFlow/WF/CCForm/CCFormAPI.asmx.cs
@@ -397,7 +397,7 @@ namespace CCFlow.WF.CCForm
 		public String GetVstoExtensionVersion()
 		{
 			//return BP.Sys.SystemConfig.AppSettings["VstoExtensionVersion"];//2017-05-02 14:53:02：不再在web.config中配置VSTO版本号
-			return "1.0.0.9";
+			return "1.0.0.10";
 		}
 	}
 }

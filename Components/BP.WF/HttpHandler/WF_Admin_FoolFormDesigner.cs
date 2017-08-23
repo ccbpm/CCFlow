c4d97f8ac19e7f96628ff8056f88548fc9b9ec07
wzzwust <wzzwust@163.com>
2017-08-23 17:30:12 +0800

    no commit message

--- Components/BP.WF/HttpHandler/WF_Admin_FoolFormDesigner.cs
+++ Components/BP.WF/HttpHandler/WF_Admin_FoolFormDesigner.cs
@@ -1961,6 +1961,100 @@ namespace BP.WF.HttpHandler
 
             return mtds;
         }
+
+        #region 数据源管理
+        public string SFDBSrcNewGuide_GetList()
+        {
+            //SysEnums enums = new SysEnums(SFDBSrcAttr.DBSrcType);
+            SFDBSrcs srcs = new SFDBSrcs();
+            srcs.RetrieveAll();
+
+            return srcs.ToJson();
+        }
+
+        public string SFDBSrcNewGuide_LoadSrc()
+        {
+            DataSet ds = new DataSet();
+
+            SFDBSrc src = new SFDBSrc();
+            if (!string.IsNullOrWhiteSpace(this.GetRequestVal("No")))
+                src = new SFDBSrc(No);
+            ds.Tables.Add(src.ToDataTableField("SFDBSrc"));
+
+            SysEnums enums = new SysEnums();
+            enums.Retrieve(SysEnumAttr.EnumKey, SFDBSrcAttr.DBSrcType, SysEnumAttr.IntKey);
+            ds.Tables.Add(enums.ToDataTableField("DBSrcType"));
+
+            return BP.Tools.Json.ToJson(ds);
+        }
+
+        public string SFDBSrcNewGuide_SaveSrc()
+        {
+            SFDBSrc src = new SFDBSrc();
+            src.No = this.GetRequestVal("TB_No");
+            if (src.RetrieveFromDBSources() > 0 && this.GetRequestVal("NewOrEdit") == "New")
+            {
+                return("已经存在数据源编号为“" + src.No + "”的数据源，编号不能重复！");
+            }
+            src.Name = this.GetRequestVal("TB_Name");
+            src.DBSrcType = (DBSrcType)this.GetRequestValInt("DDL_DBSrcType");
+            switch (src.DBSrcType)
+            {
+                case DBSrcType.SQLServer:
+                case DBSrcType.Oracle:
+                case DBSrcType.MySQL:
+                case DBSrcType.Informix:
+                    if (src.DBSrcType != DBSrcType.Oracle)
+                        src.DBName = this.GetRequestVal("TB_DBName");
+                    else
+                        src.DBName = string.Empty;                    
+                        src.IP = this.GetRequestVal("TB_IP");
+                        src.UserID = this.GetRequestVal("TB_UserID");
+                        src.Password = this.GetRequestVal("TB_Password");
+                    break;
+                case DBSrcType.WebServices:
+                    src.DBName = string.Empty;
+                    src.IP = this.GetRequestVal("TB_IP");
+                    src.UserID = string.Empty;
+                    src.Password = string.Empty;
+                    break;
+                default:
+                    break;
+            }
+            //测试是否连接成功，如果连接不成功，则不允许保存。
+            string testResult = src.DoConn();
+
+            if (testResult.IndexOf("连接配置成功") == -1)
+            {
+                return (testResult + ".保存失败！");
+            }
+
+            src.Save();
+
+            return "保存成功..";
+        }
+
+        public string SFDBSrcNewGuide_DelSrc()
+        {
+            string no = this.GetRequestVal("No");
+
+            //检验要删除的数据源是否有引用
+            SFTables sfs = new SFTables();
+            sfs.Retrieve(SFTableAttr.FK_SFDBSrc, no);
+
+            if (sfs.Count > 0)
+            {
+                //Alert("当前数据源已经使用，不能删除！");
+                return "当前数据源已经使用，不能删除！";
+                //return;
+            }
+            SFDBSrc src = new SFDBSrc(no);
+            src.Delete();
+            return "删除成功..";
+        }
+        #endregion
+
+
     }
 
     public class WSMethod

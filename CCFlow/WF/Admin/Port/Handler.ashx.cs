078a93d3432f1ec38a8e09b9dd1376dbd7da0d29
周朋 <hiflow@qq.com>
2017-08-29 14:42:13 +0800

    no commit message

---
+++ CCFlow/WF/Admin/Port/Handler.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CCFlow.WF.Admin.Port
+{
+    /// <summary>
+    /// Handler 的摘要说明
+    /// </summary>
+    public class Handler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}

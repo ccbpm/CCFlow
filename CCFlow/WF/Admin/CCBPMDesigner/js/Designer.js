eb232ea5b8cddbd2bf53c881adf90545a339a666
liangjm <liangjm@cnpc.com.cn>
2017-09-23 22:05:23 +0800

    升级傻瓜表单.

--- CCFlow/WF/Admin/CCBPMDesigner/js/Designer.js
+++ CCFlow/WF/Admin/CCBPMDesigner/js/Designer.js
@@ -437,6 +437,19 @@ function NodeProperty_Funs(item) {
                 WinOpen(url);
             }
             break;
+        case "DesignerNodeFormFixNew": //设计傻瓜表单.
+            if (plant == "JFlow") {
+                alert("请选择\"设计节点表单(H5测试版) jflow 暂不支持\"");
+                break;
+            }
+
+            url = "../FoolFormDesigner/Designer.htm?IsFirst=1&FK_MapData=ND" + FK_Node + "&FK_Flow=" + CCBPM_Data_FK_Flow + "&FK_Node=" + FK_Node;
+            if (window.parent && 1 == 3) {
+                window.parent.addTab(CCBPM_Data_FK_Flow + FK_Node + "ND", "傻瓜表单" + FK_Node, url, item.iconCls);
+            } else {
+                WinOpen(url);
+            }
+            break;
         case "DesignerNodeFormFix": //设计傻瓜表单.
             if (plant == "JFlow") {
                 alert("请选择\"设计节点表单(H5测试版)\"");

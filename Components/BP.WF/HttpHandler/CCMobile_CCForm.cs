5cd05e16826ebd8cbd7c7199baceeffdfc9f50f3
周朋 <hiflow@qq.com>
2017-12-06 18:29:26 +0800

    no commit message

--- Components/BP.WF/HttpHandler/CCMobile_CCForm.cs
+++ Components/BP.WF/HttpHandler/CCMobile_CCForm.cs
@@ -27,26 +27,6 @@ namespace BP.WF.HttpHandler
             this.context = mycontext;
         }
 
-        #region 执行父类的重写方法.
-        /// <summary>
-        /// 默认执行的方法
-        /// </summary>
-        /// <returns></returns>
-        protected override string DoDefaultMethod()
-        {
-            switch (this.DoType)
-            {
-                case "DtlFieldUp": //字段上移
-                    return "执行成功.";
-                default:
-                    break;
-            }
-
-            //找不不到标记就抛出异常.
-            throw new Exception("@标记[" + this.DoType + "]，没有找到. @RowURL:" + context.Request.RawUrl);
-        }
-        #endregion 执行父类的重写方法.
-
         public string HandlerMapExt()
         {
             WF_CCForm en = new WF_CCForm(this.context);
--- Components/BP.WF/HttpHandler/CCMobile_MyFlow.cs
+++ Components/BP.WF/HttpHandler/CCMobile_MyFlow.cs
@@ -65,30 +65,6 @@ namespace BP.WF.HttpHandler
             WF_MyFlow en = new WF_MyFlow(this.context);
             return en.Send();
         }
-
-        #region 执行父类的重写方法.
-        /// <summary>
-        /// 默认执行的方法
-        /// </summary>
-        /// <returns></returns>
-        protected override string DoDefaultMethod()
-        {
-            switch (this.DoType)
-            {
-                case "DtlFieldUp": //字段上移
-                    return "执行成功.";
-                default:
-                    break;
-            }
-
-            //找不不到标记就抛出异常.
-            throw new Exception("@标记[" + this.DoType + "]，没有找到. @RowURL:" + context.Request.RawUrl);
-        }
-        #endregion 执行父类的重写方法.
-
       
-        #region xxx 界面 .
-        #endregion xxx 界面方法.
-
     }
 }

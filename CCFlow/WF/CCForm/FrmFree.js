bdd4e9ca167069ed9560725c6ec0b7a58f145f04
周朋 <hiflow@qq.com>
2017-12-08 10:13:01 +0800

    no commit message

--- CCFlow/WF/CCForm/FrmFree.js
+++ CCFlow/WF/CCForm/FrmFree.js
@@ -1,115 +1,4 @@
 ﻿
-function testExpression(exp) {
-	if (exp == null || typeof exp == "undefined" || typeof exp != "string") {
-		return false;
-	}
-	exp = exp.replace(/\s/g, "");
-	if (exp == "" || exp.length == 0) {
-		return false;
-	}
-	if (/[\+\-\*\/]{2,}/.test(exp)) {
-		return false;
-	}
-	if (/\(\)/.test(exp)) {
-		return false;
-	}
-	var stack = [];
-	for (var i = 0; i < exp.length; i++) {
-		var c = exp.charAt(i);
-		if (c == "(") {
-			stack.push("(");
-		} else if (c == ")") {
-			if (stack.length > 0) {
-				stack.pop();
-			} else {
-				return false;
-			}
-		}
-	}
-	if (stack.length != 0) {
-		return false;
-	}
-	if (/^[\+\-\*\/]|[\+\-\*\/]$/.test(exp)) {
-		return false;
-	}
-	if (/\([\+\-\*\/]|[\+\-\*\/]\)/.test(exp)) {
-		return false;
-	}
-	return true;
-}
-
-/**
- * 表单计算(包括普通表单以及从表弹出页表单)
- */
-function calculator(Sys_MapExt) {
-	$.each(Sys_MapExt, function (i, o) {
-		if (o.ExtType == "AutoFull") {
-			if (!testExpression(o.Doc)) {
-				console.log("MyPk: " + o.MyPK + ", 表达式: '" + o.Doc + "'格式错误");
-				return false;
-			}
-			var targets = [];
-			var index = -1;
-			for (var i = 0; i < o.Doc.length; i++) {	// 对于复杂表达式需要重点测试
-				var c = o.Doc.charAt(i);
-				if (c == "(") {
-					index++;
-				} else if (c == ")") {
-					targets.push(o.Doc.substring(index + 1, i));
-					i++;
-					index = i;
-				} else if (/[\+\-|*\/]/.test(c)) {
-					targets.push(o.Doc.substring(index + 1, i));
-					index = i;
-				}
-			}
-			if (index + 1 < o.Doc.length) {
-				targets.push(o.Doc.substring(index + 1, o.Doc.length));
-			}
-			//
-			var expression = {
-				"judgement" : [],
-				"execute_judgement" : [],
-				"calculate" : o.Doc
-			};
-			$.each(targets, function (i, o) {
-				var target = o.replace("@", "");
-				var element = "$(':input[name=TB_" + target + "]')";
-				expression.judgement.push(element + ".length == 0");
-				expression.execute_judgement.push("!isNaN(parseFloat(" + element + ".val()))");
-				expression.calculate = expression.calculate.replace(o, "parseFloat(" + element + ".val())");
-			});
-			(function (targets, expression, resultTarget, pk, expDefined) {
-				$.each(targets, function (i, o) {
-					var target = o.replace("@", "");
-					$(":input[name=TB_" + target + "]").bind("change", function () {
-						var evalExpression = " var result = ''; ";
-						if (expression.judgement.length > 0) {
-							evalExpression += " if ( " + expression.judgement.join(" || ") + " ) { ";
-							evalExpression += " 	console.log(\"MyPk: " + pk + ", 表达式: '" + expDefined + "' " + "中有对象在当前页面不存在\");"
-							evalExpression += " } ";
-						}
-						if (expression.execute_judgement.length > 0) {
-							evalExpression += " else if ( " + expression.execute_judgement.join(" && ") + " ) { ";
-						}
-						if (expression.calculate.length > 0) {
-							evalExpression += " 	result = " + expression.calculate + "; ";
-						}
-						if (expression.execute_judgement.length > 0) {
-							evalExpression += " } ";
-						}
-						eval(evalExpression);
-						$(":input[name=TB_" + resultTarget + "]").val(typeof result == "undefined" ? "" : result);
-					});
-					if (i == 0) {
-						$(":input[name=TB_" + target + "]").trigger("change");
-					}
-				});
-			})(targets, expression, o.AttrOfOper, o.MyPK, o.Doc);
-			$(":input[name=TB_" + o.AttrOfOper + "]").attr("disabled", true);
-		}
-	});
-}
 
 function GenerFreeFrm(mapData, frmData) {
 
@@ -404,13 +293,13 @@ function figure_Template_Dtl(frmDtl, ext) {
     }
     var src = "";
 
-    if (frmDtl.RowShowModel == "0" ) {
+ //   if (frmDtl.RowShowModel == "0" ) {
         if (pageData.IsReadOnly) {
             src = "Dtl.htm?EnsName=" + frmDtl.No + "&RefPKVal=" + this.pageData.OID + "&IsReadonly=1" + strs;
         } else {
             src = "Dtl.htm?EnsName=" + frmDtl.No + "&RefPKVal=" + this.pageData.OID + "&IsReadonly=0" + strs;
         }
-    }
+   // }
 
     var eleIframe = '<iframe></iframe>';
     eleIframe = $("<iframe ID='F" + frmDtl.No + "' src='" + src +

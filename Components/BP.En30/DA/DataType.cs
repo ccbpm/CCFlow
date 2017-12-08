9897aa81c21db122e39cfcd794ea138726ccd566
tinyblade <tinyblade@21cn.com>
2017-12-08 10:51:44 +0800

    文件名排除+

--- Components/BP.En30/DA/DataType.cs
+++ Components/BP.En30/DA/DataType.cs
@@ -417,7 +417,7 @@ namespace BP.DA
         /// <returns>返回合法的文件名</returns>
         public static string PraseStringToFileName(string fileNameFormat)
         {
-            char[] strs = "#?*\"<>/;.,-:".ToCharArray();
+            char[] strs = "+#?*\"<>/;.,-:".ToCharArray();
             foreach (char c in strs)
                 fileNameFormat = fileNameFormat.Replace(c.ToString(), "_");
 

f6185c7305e515cfa269896fb5fd363d240e0a5b
周朋 <hiflow@qq.com>
2017-12-18 13:02:07 +0800

    no commit message

--- CCFlow/WF/WorkOpt/WorkCheck.js
+++ CCFlow/WF/WorkOpt/WorkCheck.js
@@ -339,7 +339,7 @@
 
         function GetUserSiganture(userNo, userName) {
             var func = " oncontextmenu='return false;' ondragstart='return false;'  onselectstart='return false;' onselect='document.selection.empty();'";
-            return "<img src='../../DataUser/Siganture/" + userNo + ".jpg' title='" + userName + "' " + func + " style='height:40px;' border=0 onerror=\"src='../../DataUser/Siganture/UnName.JPG'\" />";
+            return "<img src='../../DataUser/Siganture/" + userNo + ".jpg?m="+Math.random()+"' title='" + userName + "' " + func + " style='height:40px;' border=0 onerror=\"src='../../DataUser/Siganture/UnName.JPG'\" />";
         }
 
         function GetUserSmallIcon(userNo, userName) {

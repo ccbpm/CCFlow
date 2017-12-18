3a5e0fe354da1258f52a7f583c3deb32a7991fb3
ccflowdgq <daiguoqiang@ccflow.org>
2017-12-18 16:02:51 +0800

    方向条件路径修改

--- DesignerForSilverlight/CCFlowDesignerSingle/Glo.cs
+++ DesignerForSilverlight/CCFlowDesignerSingle/Glo.cs
@@ -446,7 +446,7 @@ namespace BP
                     Glo.OpenDialog(Glo.BPMHost + url, "节点表单设计");
                     return;
                 case UrlFlag.Dir: // 方向条件。
-                    url = "/WF/Admin/ConditionLine.htm?FK_Flow=" + fk_flow + "&FK_MainNode=" + node1 + "&FK_Node=" + node1 + "&ToNodeID=" + node2 + "&CondType=2&Lang=CH";
+                    url = "/WF/Admin/Cond/ConditionLine.htm?FK_Flow=" + fk_flow + "&FK_MainNode=" + node1 + "&FK_Node=" + node1 + "&ToNodeID=" + node2 + "&CondType=2&Lang=CH";
                     Glo.OpenDialog(Glo.BPMHost + url, "方向条件", 550, 500);
                     break;
                 case UrlFlag.RunFlow: // 运行流程。

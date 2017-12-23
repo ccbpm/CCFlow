4c7412fc922078551eec60b68d53d2fa1dd62211
周朋 <hiflow@qq.com>
2017-12-23 20:04:44 +0800

    no commit message

--- CCFlow/DataUser/SFTableHandler.ashx.cs
+++ CCFlow/DataUser/SFTableHandler.ashx.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace CCFlow.DataUser
+namespace CCBPM.DataUser
 {
     /// <summary>
     /// Handler 的摘要说明
     /// </summary>
     public class Handler : IHttpHandler
     {
-
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/plain";
@@ -25,11 +24,17 @@ namespace CCFlow.DataUser
             }
         }
 
-        public string TestSFTable()
+        public string Demo_HandlerEmps()
         {
             BP.Port.Emps emps = new BP.Port.Emps();
             emps.RetrieveAll();
             return emps.ToJson();
+        }
+        public string Demo_HandlerDepts()
+        {
+            BP.Port.Depts depts = new BP.Port.Depts();
+            depts.RetrieveAll();
+            return depts.ToJson();
         }
     }
 }

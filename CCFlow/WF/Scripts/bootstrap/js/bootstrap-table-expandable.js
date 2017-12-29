012eee21f7aa140652d45336d4a2b4d5b470ce52
ccflowdgq <daiguoqiang@ccflow.org>
2017-12-29 16:05:23 +0800

    添加合计行

--- CCFlow/WF/Scripts/bootstrap/js/bootstrap-table-expandable.js
+++ CCFlow/WF/Scripts/bootstrap/js/bootstrap-table-expandable.js
@@ -13,7 +13,12 @@
             });
             table.children('tbody').children('tr').filter(':even').each(function () {
                 var element = $(this);
-                element.prepend('<td><div class="table-expandable-arrow"></div></td>');
+                var element_Id = element.attr('id');
+                if (element_Id == "dtl_Sum") {
+                    //element.css('background-color', '#F5F5F5');
+                } else {
+                    element.prepend('<td><div class="table-expandable-arrow"></div></td>');
+                }
             });
         });
     });

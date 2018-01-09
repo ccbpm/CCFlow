fd71f51120a605aa0a7e62dd85ecad67f4a2e9cb
项羽 <dunerunner@163.com>
2018-01-09 15:53:50 +0800

    no commit message

--- CCFlow/WF/CCForm/JS/MultipleChoiceSearch.js
+++ CCFlow/WF/CCForm/JS/MultipleChoiceSearch.js
@@ -15,36 +15,45 @@ function MultipleChoiceSearch(mapExt) {
 	container.width(width);
 	container.height(height);
 
-    (function (FK_MapData, AttrOfOper, sql) {
+    (function (FK_MapData, AttrOfOper, oid, sql) {
         var mselector = $("#" + AttrOfOper + "_mselector");
         mselector.mselector({
             "fit": true,
             "filter": false,
             "sql": sql,
             "onSelect": function (record) {
-                $("#TB_" + AttrOfOper).val(mselector.mselector("getValue"));
-				msSaveVal(FK_MapData, AttrOfOper, record.No, record.Name);
+                $("#TB_" + AttrOfOper).val(mselector.mselector("getText"));
+				msSaveVal(FK_MapData, AttrOfOper, oid, record.No, record.Name);
             },
             "onUnselect": function (record) {
-                $("#TB_" + AttrOfOper).val(mselector.mselector("getValue"));
-                msDelete(AttrOfOper, record.No);
+                $("#TB_" + AttrOfOper).val(mselector.mselector("getText"));
+                msDelete(AttrOfOper, oid, record.No);
             }
         });
-		//mselector.mselector("loadData", [{ No : "admin", Name : "admin"}]);
-    })(mapExt.FK_MapData, mapExt.AttrOfOper, mapExt.Tag1);
+		// init
+		var frmEleDBs = new Entities("BP.Sys.FrmEleDBs");
+		frmEleDBs.Retrieve("FK_MapData", FK_MapData, "EleID", AttrOfOper, "RefPKVal", oid);
+		var initJsonData = [];
+		$.each(frmEleDBs, function (i, o) {
+			initJsonData.push({
+				"No" : o.Tag1,
+				"Name" : o.Tag2
+			});
+		});
+		mselector.mselector("loadData", initJsonData);
+		//
+    })(mapExt.FK_MapData, mapExt.AttrOfOper, (pageData.WorkID || pageData.OID || ""), mapExt.Tag1);
 }
 
 //删除数据.
-function msDelete(keyOfEn, val) {
-    var oid = (pageData.WorkID || pageData.OID || "");
+function msDelete(keyOfEn, oid, val) {
     var frmEleDB = new Entity("BP.Sys.FrmEleDB");
     frmEleDB.MyPK = keyOfEn + "_" + oid + "_" + val;
     frmEleDB.Delete();
 }
 
 //设置值.
-function msSaveVal(fk_mapdata, keyOfEn, val1, val2) {
-    var oid = (pageData.WorkID || pageData.OID || "");
+function msSaveVal(fk_mapdata, keyOfEn, oid, val1, val2) {
     var frmEleDB = new Entity("BP.Sys.FrmEleDB");
     frmEleDB.MyPK = keyOfEn + "_" + oid + "_" + val1;
     frmEleDB.FK_MapData = fk_mapdata;

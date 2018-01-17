bbafeafedc5f156fd9f1e6cd747a464ff2e83b04
ccflow—李岩 <2645191897@qq.com>
2018-01-17 09:02:35 +0800

    no commit message

--- CCFlow/DataUser/JSLibData/Demo_JS_Self.js
+++ CCFlow/DataUser/JSLibData/Demo_JS_Self.js
@@ -1,4 +1,29 @@
-﻿
+﻿function datedifference(sDate1, sDate2) {    //sDate1和sDate2是2006-12-18格式  
+    var dateSpan,
+            tempDate,
+            iDays;
+    sDate1 = Date.parse(sDate1);
+    sDate2 = Date.parse(sDate2);
+    dateSpan = sDate2 - sDate1;
+    dateSpan = Math.abs(dateSpan);
+    iDays = Math.floor(dateSpan / (24 * 3600 * 1000));
+    return iDays
+}
+
+//计算两个时间点的日期.
+function SetQingJiaTianShu() {
+
+    var dtFrom = $("#TB_QingJiaRiJiCong").val();
+    var dtTo = $("#TB_RiJiDao").val();
+
+    var days = datedifference(dtFrom, dtTo);
+    if (days == undefined || days == "NaN" || days ==NaN )
+        days = 0;
+
+    $("#TB_QingJiaTianShu").val(days);
+}
+
+
 // 处理一些控件是否可用.
 function WhenClickCheckBoxClick(cb) {
 

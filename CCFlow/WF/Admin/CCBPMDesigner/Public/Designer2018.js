5dba2badad98a4a033a380e992f19ef697192318
周朋 <hiflow@qq.com>
2018-03-04 15:05:41 +0800

    no commit message

--- CCFlow/WF/Admin/CCBPMDesigner/Public/Designer2018.js
+++ CCFlow/WF/Admin/CCBPMDesigner/Public/Designer2018.js
@@ -1,11 +1,17 @@
 ﻿//全局变量
 
+function WinOpen(url) {
+    window.open(url);
+}
+
 //流程属性.
 function FlowProperty() {
     url = "../../Comm/En.htm?EnName=BP.WF.Template.FlowExt&PK=" + flowNo + "&Lang=CH";
-    OpenEasyUiDialog(url, "eudlgframe", '流程属性', 1000, 550, "icon-property", true, null, null, null, function () {
-        //window.location.href = window.location.href;
-    });
+    WinOpen(url);
+   
+//    OpenEasyUiDialog(url, "eudlgframe", '流程属性', 1000, 550, "icon-property", true, null, null, null, function () {
+//        //window.location.href = window.location.href;
+//    });
 }
 
 //报表设计.
@@ -16,9 +22,11 @@ function FlowRpt() {
     flowId = String(flowId);
 
     url = "../RptDfine/Default.htm?FK_Flow=" + flowNo + "&FK_MapData=ND" + flowId + "MyRpt";
-    OpenEasyUiDialog(url, "eudlgframe", '流程属性', 990, 500, "icon-property", true, null, null, null, function () {
+    WinOpen(url);
 
-    });
+//    OpenEasyUiDialog(url, "eudlgframe", '流程属性', 990, 500, "icon-property", true, null, null, null, function () {
+
+//    });
 }
 
 
@@ -26,9 +34,11 @@ function FlowRpt() {
 //运行流程
 function FlowRun() {
     var url = "../TestFlow.htm?FK_Flow=" + flowNo + "&Lang=CH";
-    OpenEasyUiDialog(url, "eudlgframe", '流程测试运行', 900, 500, "icon-property", true, null, null, null, function () {
-        //window.location.href = window.location.href;
-    });
+    WinOpen(url);
+
+//    OpenEasyUiDialog(url, "eudlgframe", '流程测试运行', 900, 500, "icon-property", true, null, null, null, function () {
+//        //window.location.href = window.location.href;
+//    });
 }
 
 //旧版本.
@@ -37,27 +47,36 @@ function OldVer() {
     window.location.href = url;
 }
 
-function NewNode
 
 /***********************  节点信息. ****************/
 
 //节点属性
 function NodeAttr(nodeID) {
-    url = "../../Comm/En.htm?EnsName=BP.WF.Template.NodeExts&NodeID=" + nodeID + "&Lang=CH";
+    var url = "../../Comm/En.htm?EnsName=BP.WF.Template.NodeExts&NodeID=" + nodeID + "&Lang=CH";
     WinOpen(url);
 }
-
 
 //节点方案
 function NodeFrmSln(nodeID) {
-
+    //表单方案.
     var url = "../AttrNode/NodeFromWorkModel.htm?FK_Node=" + nodeID;
     WinOpen(url);
-   
-    ajaxModal(url, function () {
-        //  window.location.href = window.location.href;
-    });
 }
 
-  
+function NodeFrmFool(nodeID) {
+
+    //表单方案.
+    var url = "../FoolFormDesigner/Designer.htm?FK_MapData=ND203&FK_Flow="+flowNo+"&FK_Node=" + nodeID;
+    WinOpen(url);
+}
+
+//接受人规则.
+function NodeAccepterRole(nodeID) {
+
+    //表单方案.
+    var url = "FoolFormDesigner/Designer.htm?FK_MapData=ND203&FK_Flow=" + flowNo + "&FK_Node=" + nodeID;
+    WinOpen(url);
+
+}
+
  

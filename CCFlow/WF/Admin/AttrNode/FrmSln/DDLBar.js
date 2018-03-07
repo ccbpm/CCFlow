93bc8f07c25a99c5ee5073e1aa3b4e0a297735af
周朋 <hiflow@qq.com>
2018-03-07 18:49:12 +0800

    no commit message

--- CCFlow/WF/Admin/AttrNode/FrmSln/DDLBar.js
+++ CCFlow/WF/Admin/AttrNode/FrmSln/DDLBar.js
@@ -2,36 +2,20 @@
 function InitBar(optionKey) {
 
     var html = "请选择访问规则:";
+
     html += "<select id='changBar' onchange='changeOption()'>";
-
-    html += "<option value=null  disabled='disabled'>+按组织结构绑定</option>";
-    html += "<option value=0 >&nbsp;&nbsp;&nbsp;&nbsp;按照岗位智能计算</option>";
-    html += "<option value=1 >&nbsp;&nbsp;&nbsp;&nbsp;按节点绑定的部门计算</option>";
-    html += "<option value=3 >&nbsp;&nbsp;&nbsp;&nbsp;按节点绑定的人员计算</option>";
-    html += "<option value=9 >&nbsp;&nbsp;&nbsp;&nbsp;按绑定的岗位与部门交集计算</option>";
-    html += "<option value=10 >&nbsp;&nbsp;&nbsp;&nbsp;按绑定的岗位计算并且以绑定的部门集合为纬度</option>";
-    html += "<option value=11 >&nbsp;&nbsp;&nbsp;&nbsp;按指定节点的人员岗位计算</option>";
-    html += "<option value=14 >&nbsp;&nbsp;&nbsp;&nbsp;仅按绑定的岗位计算</option>";
-    html += "<option value=16 >&nbsp;&nbsp;&nbsp;&nbsp;按绑定部门计算，该部门一人处理标识该工作结束(子线程)</option>";
-
-    html += "<option value=null disabled='disabled' >+按访问规则选项</option>";
-    html += "<option value=7 >&nbsp;&nbsp;&nbsp;&nbsp;与开始节点处理人相同</option>";
-    html += "<option value=6 >&nbsp;&nbsp;&nbsp;&nbsp;与上一节点处理人相同</option>";
-    html += "<option value=8 >&nbsp;&nbsp;&nbsp;&nbsp;与指定节点处理人相同</option>";
-
-    html += "<option value=null disabled='disabled' >+按自定义SQL查询</option>";
-    html += "<option value=2 >&nbsp;&nbsp;&nbsp;&nbsp;按设置的SQL获取接受人计算</option>";
-    html += "<option value=17 >&nbsp;&nbsp;&nbsp;&nbsp;按设置的SQLTempate获取接受人计算</option>";
-    html += "<option value=12 >&nbsp;&nbsp;&nbsp;&nbsp;按SQL确定子线程接受人与数据源</option>";
-
-    html += "<option value=null disabled='disabled' >+其他方式</option>";
-    html += "<option value=4 >&nbsp;&nbsp;&nbsp;&nbsp;由上一节点发送人通过“人员选择器”选择接受人</option>";
-    html += "<option value=5 >&nbsp;&nbsp;&nbsp;&nbsp;按上一节点表单指定的字段值作为本步骤的接受人</option>";
-    html += "<option value=13 >&nbsp;&nbsp;&nbsp;&nbsp;由上一节点的明细表来决定子线程的接受人</option>";
-    html += "<option value=15 >&nbsp;&nbsp;&nbsp;&nbsp;由FEE来决定</option>";
-    html += "<option value=‘RB_ByFromEmpToEmp’ >&nbsp;&nbsp;&nbsp;&nbsp;按照配置的人员路由列表计算</option>";
-    html += "<option value=100 >&nbsp;&nbsp;&nbsp;&nbsp;按ccBPM的BPM模式处理</option>";
-
+    html += "<option value=0 >&nbsp;&nbsp;&nbsp;&nbsp;内置傻瓜表单</option>";
+    html += "<option value=1 >&nbsp;&nbsp;&nbsp;&nbsp;内置自由表单</option>";
+    html += "<option value=2 >&nbsp;&nbsp;&nbsp;&nbsp;嵌入式表单</option>";
+    html += "<option value=3 >&nbsp;&nbsp;&nbsp;&nbsp;SDK表单</option>";
+    html += "<option value=5 >&nbsp;&nbsp;&nbsp;&nbsp;表单树（多表单）</option>";
+    //  html += "<option value=5 >&nbsp;&nbsp;&nbsp;&nbsp;表单树（多表单）</option>";
+    html += "<option value=7 >&nbsp;&nbsp;&nbsp;&nbsp;公文表单</option>";
+  //  html += "<option value=8 >&nbsp;&nbsp;&nbsp;&nbsp;Excel表单</option>";
+   // html += "<option value=9 >&nbsp;&nbsp;&nbsp;&nbsp;Word表单</option>";
+    html += "<option value=10 >&nbsp;&nbsp;&nbsp;&nbsp;软通动力（傻瓜轨迹表单）</option>";
+    html += "<option value=11 >&nbsp;&nbsp;&nbsp;&nbsp;表单库表单</option>";
+  //  html += "<option value=100 >&nbsp;&nbsp;&nbsp;&nbsp;禁用</option>";
     html += "</select >";
 
     html += "<input  id='Btn_Save' type=button onclick='Save()' value='保存' />";

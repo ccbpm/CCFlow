1d62d2ef027f1482de988b5cfaead5c5e8d66422
张庆鹏 <zhangqingpeng@ccflow.org>
2018-04-13 09:48:24 +0800

    no commit message

--- CCFlow/WF/Admin/CCFormDesigner/js/FormDesigner.js
+++ CCFlow/WF/Admin/CCFormDesigner/js/FormDesigner.js
@@ -268,7 +268,7 @@ function showFigurePropertyWin(figure) {
 
     if (shap == 'AthImg') {
 
-        alert(CCForm_FK_MapData);
+        //alert(CCForm_FK_MapData);
 
         var url = '../../Comm/RefFunc/EnOnly.htm?EnName=BP.Sys.FrmUI.FrmImgAth&PKVal=' + CCForm_FK_MapData + '_' + figure.CCForm_MyPK;
         CCForm_ShowDialog(url, '图片附件');
@@ -307,8 +307,6 @@ function showFigurePropertyWin(figure) {
     }
 
     if (shap == 'HyperLink') {
-
-        var en=  new Entity("")
         var url = '../../Comm/EnOnly.htm?EnName=BP.Sys.FrmUI.FrmLink&PKVal=' + figure.CCForm_MyPK;
         CCForm_ShowDialog(url, '超链接属性');
         return;

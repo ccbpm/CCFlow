d7894fa6341dcc6139796a65bdecbad06bb4d769
ccflow..liyan <3301790953@qq.com>
2018-04-23 14:32:11 +0800

    no commit message

--- Components/BP.WF/WF/Flow.cs
+++ Components/BP.WF/WF/Flow.cs
@@ -6001,9 +6001,7 @@ namespace BP.WF
             //  sql += "@GO DELETE WF_M2M WHERE FK_Node IN (SELECT NodeID FROM WF_Node WHERE FK_Flow='" + this.No + "')";
             //// 删除配置.
             //sql += "@ DELETE FROM WF_FAppSet WHERE NodeID IN (SELECT NodeID FROM WF_Node WHERE FK_Flow='" + this.No + "')";
-
-            // 删除配置.
-            sql += "@ DELETE FROM WF_FlowEmp WHERE FK_Flow='" + this.No + "' ";
+ 
 
             //// 外部程序设置
             //sql += "@ DELETE FROM WF_FAppSet WHERE  NodeID in (SELECT NodeID FROM WF_Node WHERE FK_Flow='" + this.No + "')";

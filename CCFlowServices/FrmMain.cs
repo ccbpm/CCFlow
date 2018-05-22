8a318beccc7fb06b4743b8fddccbb8064813b7c9
ccflow..liyan <3301790953@qq.com>
2018-05-22 08:44:29 +0800

    no commit message

--- CCFlowServices/FrmMain.cs
+++ CCFlowServices/FrmMain.cs
@@ -94,104 +94,8 @@ namespace SMSServices
         /// </summary>
         public void DoTask()
         {
-            string sql = "SELECT * FROM WF_Task WHERE TaskSta=0 ORDER BY Starter";
-            DataTable dt = null;
-            try
-            {
-                dt = DBAccess.RunSQLReturnTable(sql);
-            }
-            catch
-            {
-                Task ta = new Task();
-                ta.CheckPhysicsTable();
-                dt = DBAccess.RunSQLReturnTable(sql);
-            }
-
-            if (dt.Rows.Count == 0)
-                return;
-
-            #region 自动启动流程
-            foreach (DataRow dr in dt.Rows)
-            {
-                string mypk = dr["MyPK"].ToString();
-                string taskSta = dr["TaskSta"].ToString();
-                string paras = dr["Paras"].ToString();
-                string starter = dr["Starter"].ToString();
-                string fk_flow = dr["FK_Flow"].ToString();
-
-                string startDT = dr[TaskAttr.StartDT].ToString();
-                if (string.IsNullOrEmpty(startDT) == false)
-                {
-                    /*如果设置了发起时间,就检查当前时间是否与现在的时间匹配.*/
-                    if (DateTime.Now.ToString("yyyy-MM-dd HH:mm").Contains(startDT) == false)
-                        continue;
-                }
-
-                Flow fl = new Flow(fk_flow);
-                this.SetText("开始执行(" + starter + ")发起(" + fl.Name + ")流程.");
-                try
-                {
-                    string fTable = "ND" + int.Parse(fl.No + "01").ToString();
-                    MapData md = new MapData(fTable);
-                    sql = "";
-                    //   sql = "SELECT * FROM " + md.PTable + " WHERE MainPK='" + mypk + "' AND WFState=1";
-                    try
-                    {
-                        if (DBAccess.RunSQLReturnTable(sql).Rows.Count != 0)
-                            continue;
-                    }
-                    catch
-                    {
-                        this.SetText("开始节点表单表:" + fTable + "没有设置的默认字段MainPK. " + sql);
-                        continue;
-                    }
-
-                    if (BP.Web.WebUser.No != starter)
-                    {
-                        BP.Web.WebUser.Exit();
-                        BP.Port.Emp empadmin = new BP.Port.Emp(starter);
-                        BP.Web.WebUser.SignInOfGener(empadmin);
-                    }
-
-                    Work wk = fl.NewWork();
-                    string[] strs = paras.Split('@');
-                    foreach (string str in strs)
-                    {
-                        if (string.IsNullOrEmpty(str))
-                            continue;
-
-                        if (str.Contains("=") == false)
-                            continue;
-
-                        string[] kv = str.Split('=');
-                        wk.SetValByKey(kv[0], kv[1]);
-                    }
-
-                    wk.SetValByKey("MainPK", mypk);
-                    wk.Update();
-
-                    WorkNode wn = new WorkNode(wk, fl.HisStartNode);
-                    string msg = wn.NodeSend().ToMsgOfText();
-                    msg = msg.Replace("'", "~");
-                    DBAccess.RunSQL("UPDATE WF_Task SET TaskSta=1,Msg='" + msg + "' WHERE MyPK='" + mypk + "'");
-                }
-                catch (Exception ex)
-                {
-                    //如果发送错误。
-                    this.SetText(ex.Message);
-                    string msg = ex.Message;
-                    try
-                    {
-                        DBAccess.RunSQL("UPDATE WF_Task SET TaskSta=2,Msg='" + msg + "' WHERE MyPK='" + mypk + "'");
-                    }
-                    catch
-                    {
-                        Task TK = new Task();
-                        TK.CheckPhysicsTable();
-                    }
-                }
-            }
-            #endregion 自动启动流程
+            BP.WF.DTS.AutoRunWF_Task task = new BP.WF.DTS.AutoRunWF_Task();
+            task.Do();
         }
         /// <summary>
         /// 执行线程.
@@ -235,6 +139,8 @@ namespace SMSServices
                 if (tsAuto.Minutes > BP.WF.Glo.AutoNodeDTSTimeSpanMinutes || isFirstRun==true)
                 {
                     isFirstRun = false;
+
+                    System.Threading.Thread.Sleep(BP.WF.Glo.AutoNodeDTSTimeSpanMinutes *60* 1000);
 
                     dtOfAutoNode = DateTime.Now;
                     this.SetText("检索自动节点任务....");

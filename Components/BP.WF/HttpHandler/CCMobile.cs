1d44689a6d1f5f03bee8042a39eff30c32f45fe7
ccflow..liyan <3301790953@qq.com>
2018-07-14 17:41:31 +0800

    no commit message

--- Components/BP.WF/HttpHandler/CCMobile.cs
+++ Components/BP.WF/HttpHandler/CCMobile.cs
@@ -73,6 +73,8 @@ namespace BP.WF.HttpHandler
                 if (DBAccess.IsExitsTableCol("Port_Emp", "NikeName") == true)
                 {
                     /*如果包含昵称列,就检查昵称是否存在.*/
+                    Paras ps = new Paras();
+
                     string sql = "SELECT No FROM Port_Emp WHERE NikeName='" + userNo + "'";
                     string no = DBAccess.RunSQLReturnStringIsNull(sql, null);
                     if (no == null)

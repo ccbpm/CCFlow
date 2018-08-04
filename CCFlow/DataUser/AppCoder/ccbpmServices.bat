9ee7368b482f92da535f93abf898f482f227959b
周朋 <hiflow@qq.com>
2018-08-04 10:18:34 +0800

    no commit message

--- CCFlow/DataUser/AppCoder/ccbpmServices.bat
+++ CCFlow/DataUser/AppCoder/ccbpmServices.bat
@@ -1,3 +1,18 @@
 ﻿
+@echo off 
+echo 正在关闭未关闭的IE进程，请稍等...... 
+taskkill /f /im iexplore.exe 
 
+echo -------------开始运行指定的网页页面---------- 
+::echo.&pause 
 
+start "" "C:\Program Files\Internet Explorer\iexplore.exe" 
+
+http://127.0.0.1:80/DataUser/AppCoder/ccbpmServices.htm
+
+echo IE打开完成！ 
+::echo.&pause 
+
+ping 127.0.0.1 -n 2 
+
+taskkill /f /im iexplore.exe

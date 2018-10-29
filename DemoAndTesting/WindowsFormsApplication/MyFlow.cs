6a992554627ac7aee9e92c9ce64ef5a049b6bfaa
周朋 <hiflow@qq.com>
2018-10-29 10:09:48 +0800

    no commit message

---
+++ DemoAndTesting/WindowsFormsApplication/MyFlow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication
+{
+    public partial class MyFlow : Form
+    {
+        public MyFlow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyFlow_Load(object sender, EventArgs e)
+        {
+            string flowNo = "001";
+            int fk_node = 0;
+            Int64 workid = 0;
+
+            ServiceReference.WindowsFormsApplicationDemoSoapClient clent = new ServiceReference.WindowsFormsApplicationDemoSoapClient();
+            clent.HelloWorld();
+
+
+        }
+    }
+}

323a7d25afdd8b61a6e17046c229b7abd3dbc11e
袁丽娜 <nayuanli@126.com>
2018-12-03 18:55:47 +0800

    URL中文乱码

--- CCFlow/WF/CCForm/MapExt2016.js
+++ CCFlow/WF/CCForm/MapExt2016.js
@@ -905,7 +905,7 @@ function GetQueryString(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
     var r = window.location.search.substr(1).match(reg);
     if (r != null)
-        return unescape(r[2]);
+        return decodeURI(r[2]);
     return null;
 }
 //通过正则表达式检测

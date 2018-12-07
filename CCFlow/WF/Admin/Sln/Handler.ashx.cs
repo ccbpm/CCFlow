ae266a62bc029b2be1003000e29ed0e978bce76f
袁丽娜 <nayuanli@126.com>
2018-12-07 17:49:47 +0800

    no commit message

---
+++ CCFlow/WF/Admin/Sln/Handler.ashx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Text;
+using BP.WF;
+using BP.Sys;
+using BP.En;
+using BP.DA;
+using BP.WF.Template;
+
+namespace CCFlow.WF.Admin.Sln
+{
+    public class Handler : BP.WF.HttpHandler.HttpHandlerBase
+    {
+        /// <summary>
+        /// 获取 “Handler业务处理类”的Type
+        /// </summary>
+        public override Type CtrlType
+        {
+            get
+            {
+                return typeof(BP.WF.HttpHandler.WF_Admin_Sln);
+            }
+        }
+    }
+
+}

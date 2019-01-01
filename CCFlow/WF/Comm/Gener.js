d2ef14cd682ba36f3089fd0a089117fbab335646
袁丽娜 <nayuanli@126.com>
2019-01-01 16:08:11 +0800

    增加entities的具有操作符的查询

--- CCFlow/WF/Comm/Gener.js
+++ CCFlow/WF/Comm/Gener.js
@@ -1442,23 +1442,45 @@ var Entities = (function () {
         }
     };
 
-    function getParameters(args) {
+    function getParameters(args, divisor) {
         var params = "";
         var length;
         var orderBy;
-        if (args.length % 2 == 0) {
-            orderBy = args[args.length - 1];
-            length = args.length - 1;
-        } else {
-            length = args.length;
+        if (divisor == null || divisor == undefined)
+            divisor = 2;
+
+        if (divisor == 2) {
+            if (args.length % 2 == 0) {
+                orderBy = args[args.length - 1];
+                length = args.length - 1;
+            } else {
+                length = args.length;
+            }
+            for (var i = 1; i < length; i += 2) {
+                params += "@" + args[i] + "=" + args[i + 1];
+            }
+            if (typeof orderBy !== "undefined") {
+                params += "@OrderBy=" + orderBy;
+            }
+            return params;
         }
-        for (var i = 1; i < length; i += 2) {
-            params += "@" + args[i] + "=" + args[i + 1];
+
+        if (divisor == 3) {
+            if (args.length % divisor != 0) {
+                orderBy = args[args.length - 1];
+                length = args.length - 1;
+            } else {
+                length = args.length;
+            }
+            for (var i = 1; i < length; i += 3) { //args[i+1]是操作符
+                params += "@" + args[i] + "|"+ args[i + 1] +"|"+ args[i + 2];
+            }
+            if (typeof orderBy !== "undefined") {
+                params += "@OrderBy||" + orderBy;
+            }
+            return params;
         }
-        if (typeof orderBy !== "undefined") {
-            params += "@OrderBy=" + orderBy;
-        }
-        return params;
+
     }
 
     Entities.prototype = {
@@ -1520,17 +1542,7 @@ var Entities = (function () {
                         return;
                     }
 
-                    //                    try {
-                    //                        jsonString = JSON.parse(data);
-                    //                        if ($.isArray(jsonString)) {
-                    //                            self.length = jsonString.length;
-                    //                            $.extend(self, jsonString);
-                    //                        } else {
-                    //                            alert("解析失败, 返回值不是集合");
-                    //                        }
-                    //                    } catch (e) {
-                    //                        alert("json解析错误: " + data);
-                    //                    }
+
                 },
                 error: function (XMLHttpRequest, textStatus, errorThrown) {
                     alert("Entities_Delte err@系统发生异常, status: " + XMLHttpRequest.status + " readyState: " + XMLHttpRequest.readyState);
@@ -1545,6 +1557,49 @@ var Entities = (function () {
             });
             this.Paras = getParameters(args);
             this.loadData();
+        },
+        RetrieveCond: function () {
+            var args = [""];
+            $.each(arguments, function (i, o) {
+                args.push(o);
+            });
+            this.Paras = getParameters(args,3);
+            var self = this;
+
+            if (self.ensName == null || self.ensName == "" || self.ensName == "") {
+                alert("在初始化实体期间EnsName没有赋值");
+                return;
+            }
+
+            $.ajax({
+                type: 'post',
+                async: false,
+                url: dynamicHandler + "?DoType=Entities_RetrieveCond&EnsName=" + self.ensName + "&Paras=" + self.Paras + "&t=" + new Date().getTime(),
+                dataType: 'html',
+                success: function (data) {
+
+                    if (data.indexOf("err@") != -1) {
+                        alert(data);
+                        return;
+                    }
+
+                    try {
+                        jsonString = JSON.parse(data);
+                        if ($.isArray(jsonString)) {
+                            self.length = jsonString.length;
+                            $.extend(self, jsonString);
+                        } else {
+                            alert("解析失败, 返回值不是集合");
+                        }
+                    } catch (e) {
+                        alert("json解析错误: " + data);
+                    }
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    alert("Entities_RetrieveCond err@系统发生异常, status: " + XMLHttpRequest.status + " readyState: " + XMLHttpRequest.readyState);
+                }
+            });
+
         },
         Delete: function () {
             var args = [""];

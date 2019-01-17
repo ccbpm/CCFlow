a9aeec6bc239c8d277e6606776f1d036fc49cdf7
黄果 <huangzhimin@ccflow.org>
2019-01-17 16:19:44 +0800

    no commit message

---
+++ CCFlow/WF/Admin/AttrFlow/Handler.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web;
+using BP.WF;
+using BP.Web;
+using BP.Sys;
+using BP.DA;
+using BP.En;
+using BP.WF.Template;
+using Newtonsoft.Json.Converters;
+
+
+namespace CCFlow.WF.Admin.AttrFlow
+{
+    public class Handler : BP.WF.HttpHandler.HttpHandlerBase
+    {
+        /// <summary>
+        /// 获取 “Handler业务处理类”的Type
+        /// </summary>
+        public override Type CtrlType
+        {
+            get
+            {
+                return typeof(BP.WF.HttpHandler.WF_Admin_AttrFlow);
+            }
+        }
+    }
+
+}

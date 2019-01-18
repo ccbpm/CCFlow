d03a10c4cebe7aeab28639c22fdb02fcd95142f6
黄果 <huangzhimin@ccflow.org>
2019-01-18 18:37:00 +0800

    no commit message

---
+++ CCFlow/WF/WorkOpt/Handler.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Web;
+using System.Data;
+using BP.DA;
+using BP.Sys;
+using BP.En;
+using BP.WF;
+using BP.WF.Template;
+
+namespace CCFlow.WF.WorkOpt
+{
+    /// <summary>
+    /// Handler 的摘要说明
+    /// </summary>
+    public class Handler : BP.WF.HttpHandler.HttpHandlerBase
+    {
+        /// <summary>
+        /// 获取 “Handler业务处理类”的Type
+        /// </summary>
+        public override Type CtrlType
+        {
+            get
+            {
+                return typeof(BP.WF.HttpHandler.WF_WorkOpt);
+            }
+        }
+    }
+}

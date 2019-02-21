5d7eb77ad67a0e041375490aa687aae380580128
周朋 <hiflow@qq.com>
2019-02-21 17:47:45 +0800

    no commit message

---
+++ CCFlow/DataUser/PortalInterface.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="PortalInterface.ashx.cs" Class="CCFlow.DataUser.PortalInterface" %>
---
+++ CCFlow/DataUser/PortalInterface.ashx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CCFlow.DataUser
+{
+    /// <summary>
+    /// PortalInterface 的摘要说明
+    /// </summary>
+    public class PortalInterface : IHttpHandler
+    {
+
+         
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+
+           
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}

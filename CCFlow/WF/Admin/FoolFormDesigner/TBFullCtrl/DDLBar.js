ff8daca8080856dfb7fc5effd6adde9259f830ab
周朋 <hiflow@qq.com>
2019-02-21 14:15:57 +0800

    no commit message

---
+++ CCFlow/WF/Admin/FoolFormDesigner/TBFullCtrl/DDLBar.js
@@ -0,0 +1,85 @@
+﻿
+function InitBar(optionKey) {
+
+    var html = "<b>文本框自动完成</b>:";
+
+    html += "<select id='changBar' onchange='changeOption()'>";
+
+    html += "<option value='0' >无,不设置(默认).</option>";
+    html += "<option value='1' >简介模式</option>";
+    html += "<option value='2' >表格模式</option>";
+    html += "</select >";
+
+    html += "<input  id='Btn_Save' type=button onclick='Save()' value='保存' />";
+    html += "<input  id='Btn_FullData' type=button onclick='FullData()' value='填充设置' />";
+    html += "<input  id='Btn_Help' type=button onclick='HelpOnline()' value='在线帮助' />";
+
+    document.getElementById("bar").innerHTML = html;
+    $("#changBar option[value='" + optionKey + "']").attr("selected", "selected");
+
+}
+
+function FullData() {
+    var optionKey = $("#changBar").val();
+    var myPK = optionKey+"_" + GetQueryString("FK_MapData") + "_" + GetQueryString("KeyOfEn");
+    var url = "../FullData/Default.htm?FK_MapData=" + this.GetQueryString("FK_MapData") + "&RefPK=" + myPK;
+
+    window.location.href = url;
+
+}
+
+
+function HelpOnline() {
+    var url = "http://ccform.mydoc.io";
+    window.open(url);
+}
+
+function changeOption() {
+
+    var fk_MapData = GetQueryString("FK_MapData");
+    var KeyOfEn = GetQueryString("KeyOfEn");
+
+    var obj = document.getElementById("changBar");
+    var sele = obj.options;
+    var index = obj.selectedIndex;
+    var optionKey = optionKey = sele[index].value;
+
+    var url = GetUrl(optionKey);
+
+    window.location.href = url + "?FK_MapData=" + fk_MapData + "&KeyOfEn=" + KeyOfEn;
+}
+
+function GetUrl(popModel) {
+
+
+    switch (popModel) {
+        case "0":
+            url = "0.None.htm";
+            break;
+        case "1":
+            url = "1.Simple.htm";
+            break;
+        case "2":
+            url = "2.Table.htm";
+        default:
+            url = "0.None.htm";
+            break;
+    }
+    return url;
+}
+
+function SaveAndClose() {
+
+    Save();
+    window.close();
+}
+
+//打开窗体.
+function OpenEasyUiDialogExt(url, title, w, h, isReload) {
+
+    OpenEasyUiDialog(url, "eudlgframe", title, w, h, "icon-property", true, null, null, null, function () {
+        if (isReload == true) {
+            window.location.href = window.location.href;
+        }
+    });
+}

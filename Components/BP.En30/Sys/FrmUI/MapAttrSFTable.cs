9f6da08a7ba755af054532a9bc4c0d9017d01af5
袁丽娜 <nayuanli@126.com>
2019-02-21 16:48:21 +0800

    no commit message

--- Components/BP.En30/Sys/FrmUI/MapAttrSFTable.cs
+++ Components/BP.En30/Sys/FrmUI/MapAttrSFTable.cs
@@ -151,6 +151,12 @@ namespace BP.Sys.FrmUI
                 map.AddRefMethod(rm);
 
                 rm = new RefMethod();
+                rm.Title = "填充其他控件v2019";
+                rm.ClassMethodName = this.ToString() + ".DoDDLFullCtrl2019()";
+                rm.RefMethodType = RefMethodType.RightFrameOpen;
+                map.AddRefMethod(rm);
+
+                rm = new RefMethod();
                 rm.Title = "外键表属性";
                 rm.ClassMethodName = this.ToString() + ".DoSFTable()";
                 rm.RefMethodType = RefMethodType.LinkeWinOpen;
@@ -196,6 +202,12 @@ namespace BP.Sys.FrmUI
         {
             return "../../Admin/FoolFormDesigner/MapExt/DDLFullCtrl.htm?FK_MapData=" + this.FK_MapData + "&KeyOfEn=" + this.KeyOfEn + "&MyPK=DDLFullCtrl_" + this.FK_MapData + "_" + this.KeyOfEn;
         }
+
+        public string DoDDLFullCtrl2019()
+        {
+            return "../../Admin/FoolFormDesigner/MapExt/DDLFullCtrl2019.htm?FK_MapData=" + this.FK_MapData + "&ExtType=AutoFull&KeyOfEn=" + HttpUtility.UrlEncode(this.KeyOfEn) + "&RefNo=" + HttpUtility.UrlEncode(this.MyPK);
+        }
+
         /// <summary>
         /// 设置下拉框显示过滤
         /// </summary>

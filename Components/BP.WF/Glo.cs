9206b26373f4d4ddc8bbc5b5a5c63cc3cb74cbbc
周朋 <106651+ccflow@users.noreply.gitee.com>
2019-02-28 08:55:53 +0800

    no commit message

--- Components/BP.WF/Glo.cs
+++ Components/BP.WF/Glo.cs
@@ -134,7 +134,7 @@ namespace BP.WF
         /// <summary>
         /// 当前版本号-为了升级使用.
         /// </summary>
-        public static int Ver = 20190226;
+        public static int Ver = 20190225;
         /// <summary>
         /// 执行升级
         /// </summary>

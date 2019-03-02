bb8739ddd79a80cc20cf3c2a66cc9d636a4bdafb
周朋 <106651+ccflow@users.noreply.gitee.com>
2019-03-02 16:16:51 +0800

    单据开发。 for 用友。

---
+++ CCFlow/WF/Bill/MyBill.js
@@ -0,0 +1,36 @@
+﻿//初始化函数
+$(function () {
+
+ //   webUser = new WebUser();
+
+});
+
+
+function New() {
+    alert('尚未完成.');
+}
+
+function DeleteIt () {
+    if (window.confirm('您确定要删除吗？') == false)
+        return;
+}
+
+function Search() {
+    var url = "Search.htm?FrmID=" + GetQueryString("FrmID");
+    window.location.href = url;
+}
+
+function Group() {
+    var url = "Group.htm?FrmID=" + GetQueryString("FrmID");
+    window.location.href = url;
+}
+
+
+function RefBill() {
+    alert('尚未完成.');
+}
+
+
+function StartFlow() {
+    alert('尚未完成.');
+}

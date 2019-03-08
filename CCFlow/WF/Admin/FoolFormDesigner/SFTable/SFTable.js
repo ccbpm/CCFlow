3d28461b69df9e8e8c092ae3def209712bb86ffa
周朋 <106651+ccflow@users.noreply.gitee.com>
2019-03-08 18:48:50 +0800

    no commit message

--- CCFlow/WF/Admin/FoolFormDesigner/SFTable/SFTable.js
+++ CCFlow/WF/Admin/FoolFormDesigner/SFTable/SFTable.js
@@ -1,6 +1,6 @@
 ﻿
 //转拼音，并去除开头为数字的字符
-function ParseStringToPinYin(parseString, parseModel, prix ) {
+function ParseStringToPinYin(parseString, parseModel, prix) {
 
     parseString = $.trim(parseString);
     var handler = new HttpHandler("BP.WF.HttpHandler.WF_Admin_FoolFormDesigner");

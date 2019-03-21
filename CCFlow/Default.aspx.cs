387b53a551af68bfb9b5f3b3dd4ba0b55c8620d7
周朋 <106651+ccflow@users.noreply.gitee.com>
2019-03-21 11:24:53 +0800

    升级缓存。重构代码, 为适应.netcore 做准备。

--- CCFlow/Default.aspx.cs
+++ CCFlow/Default.aspx.cs
@@ -8,6 +8,8 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.IO;
 using BP.DA;
 using BP.En;
@@ -20,7 +22,7 @@ using ThoughtWorks.QRCode.Codec;
 
 namespace CCFlow
 {
-    public partial class Default : WebPage
+    public partial class Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {

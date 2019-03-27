7749d312df69420d5c86d919b1286fd108dd83a7
黄果 <772988+ccflowhg@users.noreply.gitee.com>
2019-03-27 09:50:05 +0800

    报错还原

--- CCFlow/WF/Comm/Gener.js
+++ CCFlow/WF/Comm/Gener.js
@@ -1,20 +1,21 @@
 ﻿//定义语言.
-$(".lang").each(function (index, item) {
-    var key = $(item).attr("data-key");
-    var val = "";
-    $.each(window.lang, function (i, n) {
-        if (i == key) {
-            val = n;
-            return false;
-        }
+var CurrentLang = "CN";
+//$(".lang").each(function (index, item) {
+//    var key = $(item).attr("data-key");
+//    var val = "";
+//    $.each(window.lang, function (i, n) {
+//        if (i == key) {
+//            val = n;
+//            return false;
+//        }
 
-    });
+//    });
 
-    if (val.length > 0) {
-        $(item).text(val);
-    }
+//    if (val.length > 0) {
+//        $(item).text(val);
+//    }
 
-});
+//});
 
 //检查字段,从表名,附件ID,输入是否合法.
 function CheckID(val) {

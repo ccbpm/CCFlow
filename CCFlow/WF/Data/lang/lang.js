2526fcf3acfe4ab3c6dd2b4b26afbfd0f610feb7
周朋 <106651+ccflow@users.noreply.gitee.com>
2019-03-30 18:49:36 +0800

    language

---
+++ CCFlow/WF/Data/lang/lang.js
@@ -0,0 +1,16 @@
+﻿//定义多语言.
+$(".lang").each(function (index, item) {
+    var key = $(item).attr("data-key");
+    var val = "";
+    $.each(window.lang, function (i, n) {
+        if (i == key) {
+            val = n;
+            return false;
+        }
+
+    });
+
+    if (val.length > 0) {
+        $(item).text(val);
+    }
+});

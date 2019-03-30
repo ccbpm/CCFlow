3363565ca4d46746ad4c744eca6a9c1a7e9d319b
周朋 <106651+ccflow@users.noreply.gitee.com>
2019-03-30 15:46:17 +0800

    no commit message

--- CCFlow/WF/Comm/Gener.js
+++ CCFlow/WF/Comm/Gener.js
@@ -1,20 +1,19 @@
-﻿//定义语言.
-$(".lang").each(function (index, item) {
-    var key = $(item).attr("data-key");
-    var val = "";
-    $.each(window.lang, function (i, n) {
-        if (i == key) {
-            val = n;
-            return false;
-        }
+﻿////定义语言.
+//$(".lang").each(function (index, item) {
+//    var key = $(item).attr("data-key");
+//    var val = "";
+//    $.each(window.lang, function (i, n) {
+//        if (i == key) {
+//            val = n;
+//            return false;
+//        }
 
-    });
+//    });
 
-    if (val.length > 0) {
-        $(item).text(val);
-    }
-
-});
+//    if (val.length > 0) {
+//        $(item).text(val);
+//    }
+//});
 
 //检查字段,从表名,附件ID,输入是否合法.
 function CheckID(val) {

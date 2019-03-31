64c298a9cd4da838e377cb0fc912f2a038746831
周朋 <106651+ccflow@users.noreply.gitee.com>
2019-03-31 11:39:11 +0800

    no commit message

---
+++ Components/BP.En30/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BP.Web;
+
+namespace BP.En30.Services
+{
+    public class UserService : IUserService
+    {
+        public WebUser GetUserInfo(string no) { 
+        
+        }
+
+
+        public List<WebUser> GetUserInfo() {
+        }
+    }
+}
---
+++ Components/BP.En30/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BP.Web;
+
+namespace BP.En30.Services
+{
+    interface IUserService
+    {
+        WebUser GetUserInfo(string no);
+
+
+        List<WebUser> GetUserInfo();
+    }
+}

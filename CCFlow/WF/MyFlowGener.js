cef08806dd717a1f718fb42630ced63539c52449
袁丽娜 <1629967+linahaha@users.noreply.gitee.com>
2019-04-17 16:52:26 +0800

    no commit message

--- CCFlow/WF/MyFlowGener.js
+++ CCFlow/WF/MyFlowGener.js
@@ -79,7 +79,7 @@ function closeWindow() {
     }
 }
 //从表在新建或者在打开行的时候，如果 EditModel 配置了使用卡片的模式显示一行数据的时候，就调用此方法.
-function DtlFrm(ensName, refPKVal, pkVal, frmType, InitPage,H) {
+function DtlFrm(ensName, refPKVal, pkVal, frmType, InitPage, FK_MapData, FK_Node, FID, IsSave, H) {
     // model=1 自由表单, model=2傻瓜表单.
     var pathName = document.location.pathname;
     var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);

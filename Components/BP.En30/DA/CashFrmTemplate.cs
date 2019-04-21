f2de5e27f984a00044793052eed3ee161e7b950d
周朋 <106651+ccflow@users.noreply.gitee.com>
2019-04-21 16:09:57 +0800

    no commit message

--- Components/BP.En30/DA/CashFrmTemplate.cs
+++ Components/BP.En30/DA/CashFrmTemplate.cs
@@ -16,7 +16,7 @@ namespace BP.DA
 
         #region 对实体的操作.
         /// <summary>
-        /// 放入
+        /// 放入表单
         /// </summary>
         /// <param name="frmID">表单ID</param>
         /// <param name="ds">表单模版</param>
@@ -52,11 +52,11 @@ namespace BP.DA
         }
         private static object lockObj = new object();
         /// <summary>
-        /// 获得实体类
+        /// 获得表单DataSet模式的模版数据
         /// </summary>
         /// <param name="frmID">表单ID</param>
         /// <returns>表单模版</returns>
-        public static DataSet GetFrm(string frmID)
+        public static DataSet GetFrmDataSetModel(string frmID)
         {
             lock (lockObj)
             {
@@ -68,6 +68,26 @@ namespace BP.DA
                     string json = _hts[frmID] as string;
                     DataSet ds = BP.Tools.Json.ToDataSet(json);
                     return ds;
+                }
+                return null;
+            }
+        }
+        /// <summary>
+        /// 获得表单json模式的模版数据
+        /// </summary>
+        /// <param name="frmID">表单ID</param>
+        /// <returns>json</returns>
+        public static string GetFrmJsonModel(string frmID)
+        {
+            lock (lockObj)
+            {
+                if (_hts == null)
+                    _hts = new Hashtable();
+
+                if (_hts.ContainsKey(frmID) == true)
+                {
+                    string json = _hts[frmID] as string;
+                    return json;
                 }
                 return null;
             }

8c855b2b9eeab8f6a7fc5db3d2b7112d54e0aa3c
袁丽娜 <1629967+linahaha@users.noreply.gitee.com>
2019-04-26 17:13:13 +0800

    no commit message

--- CCFlow/DataUser/JSLibData/CCFormRef.js
+++ CCFlow/DataUser/JSLibData/CCFormRef.js
@@ -12,68 +12,46 @@ function afterDtlImp(FK_MapData, frmAth, newOID, FK_Node, oldOID,oldFK_MapData) 
     //处理从表附件导入的事件
 }
 
-//阿拉伯数字转换为简写汉字
-function Number_To_Chinese(Num) {
-    for (i = Num.length - 1; i >= 0; i--) {
-        Num = Num.replace(",", "")//替换Num中的“,”
-        Num = Num.replace(" ", "")//替换Num中的空格
+function CompareData() {
+    if ($("#TB_StartTime").val() > $("#TB_EndTime").val()) {
+        alert("开始时间不能大于结束时间");
+        return false;
     }
-    if (isNaN(Num)) { //验证输入的字符是否为数字
-        //alert("请检查小写金额是否正确");
-        return;
-    }
-    //字符处理完毕后开始转换，采用前后两部分分别转换
-    part = String(Num).split(".");
-    newchar = "";
-    //小数点前进行转化
-    for (i = part[0].length - 1; i >= 0; i--) {
-        if (part[0].length > 10) {
-            //alert("位数过大，无法计算");
-            return "";
-        } //若数量超过拾亿单位，提示
-        tmpnewchar = ""
-        perchar = part[0].charAt(i);
-        switch (perchar) {
-            case "0": tmpnewchar = "零" + tmpnewchar; break;
-            case "1": tmpnewchar = "壹" + tmpnewchar; break;
-            case "2": tmpnewchar = "贰" + tmpnewchar; break;
-            case "3": tmpnewchar = "叁" + tmpnewchar; break;
-            case "4": tmpnewchar = "肆" + tmpnewchar; break;
-            case "5": tmpnewchar = "伍" + tmpnewchar; break;
-            case "6": tmpnewchar = "陆" + tmpnewchar; break;
-            case "7": tmpnewchar = "柒" + tmpnewchar; break;
-            case "8": tmpnewchar = "捌" + tmpnewchar; break;
-            case "9": tmpnewchar = "玖" + tmpnewchar; break;
-        }
-        switch (part[0].length - i - 1) {
-            case 0: tmpnewchar = tmpnewchar; break;
-            case 1: if (perchar != 0) tmpnewchar = tmpnewchar + "拾"; break;
-            case 2: if (perchar != 0) tmpnewchar = tmpnewchar + "佰"; break;
-            case 3: if (perchar != 0) tmpnewchar = tmpnewchar + "仟"; break;
-            case 4: tmpnewchar = tmpnewchar + "万"; break;
-            case 5: if (perchar != 0) tmpnewchar = tmpnewchar + "拾"; break;
-            case 6: if (perchar != 0) tmpnewchar = tmpnewchar + "佰"; break;
-            case 7: if (perchar != 0) tmpnewchar = tmpnewchar + "仟"; break;
-            case 8: tmpnewchar = tmpnewchar + "亿"; break;
-            case 9: tmpnewchar = tmpnewchar + "十"; break;
-        }
-        newchar = tmpnewchar + newchar;
-    }
-    //替换所有无用汉字，直到没有此类无用的数字为止
-    while (newchar.search("零零") != -1 || newchar.search("零亿") != -1 || newchar.search("亿万") != -1 || newchar.search("零万") != -1) {
-        newchar = newchar.replace("零亿", "亿");
-        newchar = newchar.replace("亿万", "亿");
-        newchar = newchar.replace("零万", "万");
-        newchar = newchar.replace("零零", "零");
-    }
-    //替换以“一十”开头的，为“十”
-    if (newchar.indexOf("一十") == 0) {
-        newchar = newchar.substr(1);
-    }
-    //替换以“零”结尾的，为“”
-    if (newchar.lastIndexOf("零") == newchar.length - 1) {
-        newchar = newchar.substr(0, newchar.length - 1);
-    }
-    return newchar;
+    return true;
 }
-                  
+
+function HeJi() {
+    var bmhj = $("#BMHJ").val();
+    var jthj = $("#JTHJ").val();
+    var bzhj = $("#BZHJ").val();
+    $("#HeJj").val(bmhj + jthj + bzhj);
+
+
+}
+
+function GetShiJian(keyOfEn) {
+    alert($("#TB_" + keyOfEn + "_0").val());
+}
+
+
+function IsSaveDtl() {
+    var regInput = true;
+    //获取页面的所有IFrame
+    var frames = $("#divCCForm").find("iframe");
+    var dtlFrames = $.grep(frames, function (frame,idx) {
+        if (frame.id.indexOf("Dtl_") == 0)
+            return frame;
+    });
+
+    //循环从表IFrame，如果有未填的返回false
+    $.each(dtlFrames, function (idx, dtlFrame) {
+        var mustInput = $(this).contents().find(".errorInput");
+
+        if (mustInput.length > 0) {
+            regInput = false;
+            return;
+        }
+    });
+
+return regInput;
+}    

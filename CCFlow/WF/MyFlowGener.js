d2431795899e04e59314c47f40e2c7198a08cba7
黄果 <772988+ccflowhg@users.noreply.gitee.com>
2019-05-06 14:37:00 +0800

    修正DtlFrm的调用错误

--- CCFlow/WF/MyFlowGener.js
+++ CCFlow/WF/MyFlowGener.js
@@ -115,7 +115,32 @@ function DtlFrm(ensName, refPKVal, pkVal, frmType, InitPage, H) {
         window.open(url);
     }
 }
+//从表在新建或者在打开行的时候，如果 EditModel 配置了使用卡片的模式显示一行数据的时候，就调用此方法. // IsSave 弹出页面关闭时是否要删除从表
+function DtlFrm(ensName, refPKVal, pkVal, frmType, InitPage, FK_MapData, FK_Node, FID, IsSave, H) {
+    // model=1 自由表单, model=2傻瓜表单.
+    var pathName = document.location.pathname;
+    var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
+    if (projectName.startsWith("/WF")) {
+        projectName = "";
+    }
+    if (H == undefined || H < 600)
+        H = 600;
+    if (H > 1000)
+        H = 1000;
 
+    var url = projectName + '/WF/CCForm/DtlFrm.htm?EnsName=' + ensName + '&RefPKVal=' + refPKVal + "&FrmType=" + frmType + '&OID=' + pkVal + "&FK_MapData=" + FK_MapData + "&FK_Node=" + FK_Node + "&FID=" + FID + "&IsSave=" + IsSave;
+    if (typeof ((parent && parent.OpenBootStrapModal) || OpenBootStrapModal) === "function") {
+        OpenBootStrapModal(url, "editSubGrid", '编辑', 1000, H, "icon-property", false, function () { }, null, function () {
+            if (typeof InitPage === "function") {
+                InitPage.call();
+            } else {
+                alert("请手动刷新表单");
+            }
+        }, "editSubGridDiv", null, false);
+    } else {
+        window.open(url);
+    }
+}
 //单表单加载需要执行的函数
 function CCFormLoaded() {
     if (parent != null && parent.document.getElementById('MainFrames') != undefined) {

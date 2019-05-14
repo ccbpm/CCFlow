76581476fca2c6a71b15e1dda02442ed1a46d7b9
周朋 <106651+ccflow@users.noreply.gitee.com>
2019-05-14 22:11:04 +0800

    no commit message

---
+++ CCFlow/WF/Admin/FoolFormDesigner/FrameExt/DDLBar.js
@@ -0,0 +1,88 @@
+﻿
+function InitBar(optionKey) {
+
+    var html = "<b>框架预制功能</b>:";
+
+    html += "<select id='changBar' onchange='changeOption()'>";
+
+    html += "<option value=null  disabled='disabled'>+基本设置</option>";
+    html += "<option value='Self' >自定义URL.</option>";
+    html += "<option value='Map' disabled='disabled' >地图(开发中)</option>";
+
+    html += "<option value=null  disabled='disabled'>+流程相关</option>";
+    html += "<option value='FlowTable' >流程轨迹表</option>";
+    html += "<option value='FlowChart' >流程轨迹图</option>";
+
+    html += "</select >";
+
+    html += "<input  id='Btn_Save' type=button onclick='Save()' value='保存' />";
+    html += "<input  id='Btn_Help' type=button onclick='HelpOnline()' value='在线帮助' />";
+
+    document.getElementById("bar").innerHTML = html;
+    $("#changBar option[value='" + optionKey + "']").attr("selected", "selected");
+
+}
+
+function HelpOnline() {
+    var url = "http://ccform.mydoc.io";
+    window.open(url);
+}
+
+function changeOption() {
+
+
+    var obj = document.getElementById("changBar");
+    var sele = obj.options;
+    var index = obj.selectedIndex;
+    var optionKey = optionKey = sele[index].value;
+
+    var url = GetUrl(optionKey);
+
+    window.location.href = url + "?MyPK=" + GetQueryString("MyPK");
+}
+
+function GetUrl(popModel) {
+
+    switch (popModel) {
+        case "Self":
+        case "0":
+        case 0:
+            url = "0.Self.htm";
+            break;
+        case "Map":
+        case "1":
+        case 1:
+            url = "1.Map.htm";
+            break;
+        case "FlowTable":
+        case "2":
+        case 2:
+            url = "2.FlowTable.htm";
+            break;
+        case "FlowChart":
+        case "3":
+        case 3:
+            url = "3.FlowChart.htm";
+            break;
+        default:
+            url = "0.Self.htm";
+            break;
+    }
+    return url;
+}
+
+function SaveAndClose() {
+
+    Save();
+    window.close();
+}
+
+//打开窗体.
+function OpenEasyUiDialogExt(url, title, w, h, isReload) {
+
+    OpenEasyUiDialog(url, "eudlgframe", title, w, h, "icon-property", true, null, null, null, function () {
+        if (isReload == true) {
+            window.location.href = window.location.href;
+        }
+    });
+}

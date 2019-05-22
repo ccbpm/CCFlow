d2d7f256663467110ba21906b57f9f55b7b23744
周朋 <106651+ccflow@users.noreply.gitee.com>
2019-05-22 19:19:52 +0800

    no commit message

--- CCFlow/WF/Data/UpdataCCFlowVer.sql
+++ CCFlow/WF/Data/UpdataCCFlowVer.sql
@@ -106,11 +106,11 @@ UPDATE Sys_SFTable SET  SrcType=0 WHERE SrcType IS NULL ;
 --UPDATE Sys_MapAttr SET ColSpan=4 WHERE ColSpan>=3;
 
 -- 2019.03.10 ; 
-DELETE FROM Sys_EnCfg WHERE No='BP.WF.CCBill.FrmBill';
-INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.CCBill.FrmBill','@No=基础信息,单据基础配置信息.@BtnNewLable=单据按钮权限,用于控制每个功能按钮启用规则.@BtnImpExcel=列表按钮,列表按钮控制@Designer=设计者,流程开发设计者信息');
+DELETE FROM Sys_EnCfg WHERE No='BP.Frm.FrmBill';
+INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.Frm.FrmBill','@No=基础信息,单据基础配置信息.@BtnNewLable=单据按钮权限,用于控制每个功能按钮启用规则.@BtnImpExcel=列表按钮,列表按钮控制@Designer=设计者,流程开发设计者信息');
 
 -- 2019.05.15 ; 
-DELETE FROM Sys_EnCfg WHERE No='BP.WF.CCBill.FrmDict';
-INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.CCBill.FrmDict','@No=基础信息,单据基础配置信息.@BtnNewLable=单据按钮权限,用于控制每个功能按钮启用规则.@BtnImpExcel=列表按钮,列表按钮控制@Designer=设计者,流程开发设计者信息');
+DELETE FROM Sys_EnCfg WHERE No='BP.Frm.FrmDict';
+INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.Frm.FrmDict','@No=基础信息,单据基础配置信息.@BtnNewLable=单据按钮权限,用于控制每个功能按钮启用规则.@BtnImpExcel=列表按钮,列表按钮控制@Designer=设计者,流程开发设计者信息');
  
 

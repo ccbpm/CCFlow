60dde3fcda6d8c9349a71e05e37a350a93cd8f11
袁丽娜 <1629967+linahaha@users.noreply.gitee.com>
2019-05-29 15:22:27 +0800

    no commit message

--- CCFlow/WF/CCBill/MyFrmGener.js
+++ CCFlow/WF/CCBill/MyFrmGener.js
@@ -31,8 +31,25 @@ function DraftBox() {
     window.location.href = url;
 }
 
-function RefBill() {
-    alert('尚未完成.');
+function RefBill(frmID) {
+    //关联单据
+   
+    var W = document.body.clientWidth - 40;
+    var H = document.body.clientHeight - 40;
+    var url = "Opt/RefBill.htm?PFrmID=" + frmID + "&WorkID=" + GetQueryString("WorkID") + "&FrmID=" + GetQueryString("FrmID");
+    OpenBootStrapModal(url, "eudlgframe", "关联单据", W, H, "icon-property", null, null, null, function () {
+        window.location.href = window.location.href;
+    }, null, "black");
+}
+
+//查看关联单据的信息
+function ShowRefBillInfo(frmID) {
+    var workID = frmData.MainTable[0].PWorkID;
+    var url = "MyBill.htm?WorkID=" + workID + "&FrmID=" + frmID + "&FK_MapData=" + frmID;
+    var W = document.body.clientWidth - 40;
+    var H = document.body.clientHeight - 40;
+
+    OpenBootStrapModal(url, "eudlgframe", "关联单据信息", W, H, "icon-property", null, null, null, null, null, "black"); 
 }
 
 
@@ -41,36 +58,31 @@ function StartFlow() {
 }
 
 function PrintHtml() {
-    alert('尚未完成-PrintHtml');
-    return;
-    var url = "Group.htm?FrmID=" + GetQueryString("FrmID");
-    window.location.href = url;
+    var W = document.body.clientWidth - 120;
+    var H = document.body.clientHeight -120;
+    var url = "../WorkOpt/Packup.htm?FrmID=" + GetQueryString("FrmID") + "&WorkID=" + GetQueryString("WorkID") + "&SourceType=Bill&FileType=htm";
+    OpenBootStrapModal(url, "eudlgframe", "打印PDF", W, H, "icon-property", null, null, null, null, null, "black");
 }
 
 function PrintPDF() {
-    alert('尚未完成-PrintPDF');
-    return;
-    var url = "Group.htm?FrmID=" + GetQueryString("FrmID");
-    window.location.href = url;
+    var W = document.body.clientWidth - 40;
+    var H = document.body.clientHeight - 40;
+    var url = "../WorkOpt/Packup.htm?FrmID=" + GetQueryString("FrmID") + "&WorkID=" + GetQueryString("WorkID") + "&SourceType=Bill&FileType=pdf";
+    OpenBootStrapModal(url, "eudlgframe", "打印PDF",600, 500, "icon-property", null, null, null, null, null, "black");
+
 }
 
 function PrintRTF() {
-    alert('尚未完成 - PrintRTF');
-    return;
-    var url = "Group.htm?FrmID=" + GetQueryString("FrmID");
-    window.location.href = url;
+    var url = "../WorkOpt/PrintDoc.htm?FrmID=" + GetQueryString("FrmID") + "&WorkID=" + GetQueryString("WorkID") + "&SourceType=Bill";
+    OpenBootStrapModal(url, "eudlgframe", "打印PDF", 600, 500, "icon-property", null, null, null, null, null, "black");
 }
 
 function PrintCCWord() {
-    alert('尚未完成 - PrintCCWord');
-    return;
-    var url = "Group.htm?FrmID=" + GetQueryString("FrmID");
-    window.location.href = url;
+    var url = "../WorkOpt/PrintDoc.htm?FrmID=" + GetQueryString("FrmID") + "&WorkID=" + GetQueryString("WorkID") + "&SourceType=Bill";
+    OpenBootStrapModal(url, "eudlgframe", "打印PDF", 600, 500, "icon-property", null, null, null, null, null, "black");
 }
 
 function ExpToZip() {
-    alert('尚未完成 - ExpToZip');
-    return;
-    var url = "Group.htm?FrmID=" + GetQueryString("FrmID");
-    window.location.href = url;
+    var url = "../WorkOpt/Packup.htm?FrmID=" + GetQueryString("FrmID") + "&WorkID=" + GetQueryString("WorkID") + "&SourceType=Bill&FileType=zip";
+    OpenBootStrapModal(url, "eudlgframe", "打印PDF",600, 500, "icon-property", null, null, null, null, null, "black");
 }

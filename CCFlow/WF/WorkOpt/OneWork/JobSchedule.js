c1de24179a10f91c3e4265d6e353d1f5adfc458a
周朋 <hiflow@qq.com>
2019-06-01 10:20:06 +0800

    no commit message

--- CCFlow/WF/WorkOpt/OneWork/JobSchedule.js
+++ CCFlow/WF/WorkOpt/OneWork/JobSchedule.js
@@ -1,11 +1,67 @@
 ﻿$(function () {
 
     var html = "";
-    html += "<img src='./Admin/FoolFormDesigner/Img/JobSchedule.png' />";
+
+    var handler = new HttpHandler("WF_WorkOpt_OneWork");
+    handler.AddPara("WorkID", workid);
+    var ds = handler.DoMethodReturnJSON("JobSchedule_Init");
+
+    var gwf = ds["WF_GenerWorkFlow"]; //工作记录.
+    var nodes = ds["WF_Node"]; //节点.
+    var dirs = ds["WF_Direction"]; //连接线.
+    var tracks = ds["Track"]; //历史记录.
+
+    //循环历史记录.     
+    for (var i = 0; i < tracks.length; i++) {
+        var tk = tracks[i];
+        html += "@已经完成的节点:" + tk.NodeName;
+    }
+
+    //流程未完成的状态.
+    if (gwf.WFState != 3) {
+
+        //当前停留的节点.
+        var currNode = gwf.FK_Node;
+
+        for (var i = 0; i < 100; i++) {
+
+            var nextNode = GetNextNodeID(currNode, dirs);
+            if (nextNode == 0)
+                break;
+
+            html += "@未完成的节点:" + nextNode;
+            currNode = nextNode;
+        }
+    }
+    //html += "<img src='./Admin/FoolFormDesigner/Img/JobSchedule.png' />";
 
     $("#JobSchedule").html(html);
-
     // alert('sss');
     return;
 });
+
+//根据当前节点获得下一个节点.
+function GetNextNodeID(nodeID, dirs) {
+
+    for (var i = 0; i < dirs.length; i++) {
+        var dir = dirs[i];
+        if (dir.Node == nodeID)
+            return dir.ToNode;
+    }
+    return 0;
+}
+
+function GetNextNodeIDExpSpecNode(nodeID,specToNode, dirs) {
+
+    for (var i = 0; i < dirs.length; i++) {
+        var dir = dirs[i];
+        if (dir.Node == currNode) {
+
+            if (dir.ToNode == specToNode)
+                return 0;
+        }
+    }
+    return 0;
+}
+      
       

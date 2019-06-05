2867dcda4ea5f767c7dcfe4103b5a1d839661597
周朋 <hiflow@qq.com>
2019-06-05 16:04:08 +0800

    no commit message

--- Components/BP.WF/HttpHandler/WF_WorkOpt.cs
+++ Components/BP.WF/HttpHandler/WF_WorkOpt.cs
@@ -2577,10 +2577,8 @@ namespace BP.WF.HttpHandler
             {
                 //sql = "SELECT No,Name, FK_Dept FROM Port_Emp WHERE FK_Dept='" + fk_dept + "' ";
                 sql = "SELECT distinct A.No,A.Name, '" + fk_dept + "' as FK_Dept, Idx FROM Port_Emp A LEFT JOIN Port_DeptEmp B  ON A.No=B.FK_Emp WHERE A.FK_Dept='" + fk_dept + "' OR B.FK_Dept='" + fk_dept + "' ";
-                sql += " ORDER BY Idx ";
+                sql += " ORDER BY A.Idx ";
             }
-
-
 
             DataTable dtEmps = BP.DA.DBAccess.RunSQLReturnTable(sql);
             dtEmps.TableName = "Emps";

513c6df92f14879e6c0c506f8786c684f8c61c21
周朋 <hiflow@qq.com>
2019-06-05 17:45:36 +0800

    no commit message

---
+++ Components/BP.WF/WeiXin/WeiXin.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using BP.Tools;
+
+namespace BP.WF.WeiXin
+{
+    public class WeiXin
+    {
+        private string appid = BP.Sys.SystemConfig.WX_CorpID;// "wx8eac6a18c5efec30";
+        private string appsecret = BP.Sys.SystemConfig.WX_AppSecret;// "KfFkE9AZ3Zp09zTuKvmqWLgtLj-_cHMPTvV992apOWgSKJHcbjpbu1jYVXh7gI7K";
+        public string getAccessToken()
+        {
+            string accessToken = string.Empty;
+            string url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + appid + "&corpsecret=" + appsecret + "";
+
+            try
+            {
+                AccessToken AT = new AccessToken();
+                HttpWebResponse response = new HttpWebResponseUtility().CreateGetHttpResponse(url, 10000, null, null);
+                StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                string str = reader.ReadToEnd();
+                AT = FormatToJson.ParseFromJson<AccessToken>(str);
+                reader.Dispose();
+                reader.Close();
+                if (response != null) response.Close();
+                if (AT != null)
+                {
+                    accessToken = AT.access_token;
+                }
+            }
+            catch
+            {
+            }
+            return accessToken;
+        }
+
+        #region 发送微信信息
+        public MessageErrorModel PostWeiXinMsg(StringBuilder sb)
+        {
+            string wxStr = string.Empty;
+            string url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?";
+            MessageErrorModel m = new MessageErrorModel();
+            wxStr = PostForWeiXin(sb, url);
+            m = FormatToJson.ParseFromJson<MessageErrorModel>(wxStr);
+            return m;
+        }
+        /// <summary>
+        /// POST方式请求 微信返回信息
+        /// </summary>
+        /// <param name="parameters">参数</param>
+        /// <param name="URL">请求地址</param>
+        /// <returns>返回字符</returns>
+        public string PostForWeiXin(StringBuilder parameters, string URL)
+        {
+            string access_token = getAccessToken();
+            string url = URL + "access_token=" + access_token;
+
+            HttpWebResponse response = new HttpWebResponseUtility().WXCreateGetHttpResponse(url, parameters,
+                10000, null, Encoding.UTF8, null);
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            string str = reader.ReadToEnd();
+            
+            BP.DA.Log.DebugWriteInfo(url + "----------------" + parameters + "---------------" + str);
+            return str;
+        }
+        #endregion
+
+        /// <summary>
+        /// 下载人员头像
+        /// </summary>
+        public bool DownLoadUserIcon(string savePath)
+        {
+            if (Directory.Exists(savePath) == false)
+                Directory.CreateDirectory(savePath);
+
+            DeptMent_GetList deptMentList = GetDeptMentList();
+            if (deptMentList != null && deptMentList.errcode == "0")
+            {
+                foreach (DeptMentInfo deptMent in deptMentList.department)
+                {
+                    UsersBelongDept users = GetUserListByDeptID(deptMent.id);
+                    if (users != null && users.errcode == "0")
+                    {
+                        foreach (UserInfoBelongDept userInfo in users.userlist)
+                        {
+                            if (userInfo.avatar != null)
+                            {
+                                //大图标
+                                string headimgurl = userInfo.avatar;
+                                string UserIcon = savePath + "\\" + userInfo.userid + "Biger.png";
+                                BP.DA.DataType.HttpDownloadFile(headimgurl, UserIcon);
+
+                                //小图标
+                                string iconSize = userInfo.avatar.Substring(headimgurl.LastIndexOf('/'));
+                                if (iconSize == "/")
+                                    headimgurl = userInfo.avatar + "64";
+                                else
+                                    headimgurl = userInfo.avatar.Substring(0, headimgurl.LastIndexOf('/')) + "64";
+                                UserIcon = savePath + "\\" + userInfo.userid + "Smaller.png";
+                                BP.DA.DataType.HttpDownloadFile(headimgurl, UserIcon);
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// 获取指定部门下  指定手机号的人员
+        /// </summary>
+        /// <param name="FK_Dept">部门编号</param>
+        /// <param name="Tel">手机号</param>
+        /// <returns></returns>
+        public UserInfoBelongDept GetUserListByDeptIDAndTel(string FK_Dept, string Tel = null)
+        {
+            string access_token = getAccessToken();
+            string url = "https://qyapi.weixin.qq.com/cgi-bin/user/list?access_token= " + access_token + "&department_id=" + FK_Dept + "&status=0";
+            try
+            {
+                string str = new HttpWebResponseUtility().HttpResponseGet(url);
+                UsersBelongDept users = FormatToJson.ParseFromJson<UsersBelongDept>(str);
+
+                //指定人员
+                if (Tel != null)
+                {
+                    foreach (UserInfoBelongDept user in users.userlist)
+                    {
+                        if (user.mobile == Tel)
+                            return user;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                BP.DA.Log.DefaultLogWriteLineError(ex.Message);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 获取部门集合
+        /// </summary>
+        public DeptMent_GetList GetDeptMentList()
+        {
+            string access_token = getAccessToken();
+            string url = "https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token=" + access_token;
+            try
+            {
+                string str = new HttpWebResponseUtility().HttpResponseGet(url);
+                DeptMent_GetList departMentList = FormatToJson.ParseFromJson<DeptMent_GetList>(str);
+
+                //部门集合
+                if (departMentList != null)
+                    return departMentList;
+            }
+            catch (Exception ex)
+            {
+                BP.DA.Log.DefaultLogWriteLineError(ex.Message);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 获取指定部门下的人员
+        /// </summary>
+        /// <param name="FK_Dept">部门编号</param>
+        /// <returns></returns>
+        public UsersBelongDept GetUserListByDeptID(string FK_Dept)
+        {
+            string access_token = getAccessToken();
+            string url = "https://qyapi.weixin.qq.com/cgi-bin/user/list?access_token=" + access_token + "&department_id=" + FK_Dept + "&status=0";
+            try
+            {
+                string str = new HttpWebResponseUtility().HttpResponseGet(url);
+                UsersBelongDept users = FormatToJson.ParseFromJson<UsersBelongDept>(str);
+
+                //人员集合
+                if (users != null)
+                    return users;
+            }
+            catch (Exception ex)
+            {
+                BP.DA.Log.DefaultLogWriteLineError(ex.Message);
+            }
+            return null;
+        }
+    }
+}
---
+++ Components/BP.WF/WeiXin/WeiXinMessage.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BP.Tools;
+using BP.En;
+
+namespace BP.WF.WeiXin
+{
+    /// <summary>
+    /// 微信消息处理
+    /// </summary>
+    public class WeiXinMessage
+    {
+        /// <summary>
+        /// 发送文本消息
+        /// </summary>
+        /// <param name="msgText">消息实体类</param>
+        /// <returns>发送消息结果</returns>
+        public static MessageErrorModel PostMsgOfText(WX_Msg_Text msgText)
+        {
+
+            string url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + msgText.Access_Token;
+
+            StringBuilder append_Json = new StringBuilder();
+            append_Json.Append("{");
+            append_Json.Append("\"msgtype\":\"text\"");
+            //按人员
+            if (!string.IsNullOrEmpty(msgText.touser)) 
+                append_Json.Append(",\"touser\":\"" + msgText.touser + "\"");
+
+            //按部门
+            if (!string.IsNullOrEmpty(msgText.toparty)) 
+                append_Json.Append(",\"toparty\":\"" + msgText.toparty + "\"");
+
+            //标签
+            if (!string.IsNullOrEmpty(msgText.totag)) 
+                append_Json.Append(",\"totag\":\"" + msgText.totag + "\"");
+
+            append_Json.Append(",\"agentid\":\"" + msgText.agentid + "\"");
+            append_Json.Append(",\"text\":{");
+            append_Json.Append("\"content\":\"" + msgText.content + "\"");
+            append_Json.Append("}");
+            append_Json.Append(",\"safe\":\"" + msgText.safe + "\"");
+            append_Json.Append("}");
+            string str = new HttpWebResponseUtility().HttpResponsePost_Json(url, append_Json.ToString());
+            MessageErrorModel postVal = FormatToJson.ParseFromJson<MessageErrorModel>(str);
+            return postVal;
+        }
+
+        /// <summary>
+        /// 发送新闻消息
+        /// </summary>
+        /// <param name="msgNews">消息实体类</param>
+        /// <returns>发送消息结果</returns>
+        public static MessageErrorModel PostMsgOfNews(WX_Msg_News msgNews)
+        {
+            string url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + msgNews.Access_Token;
+
+            StringBuilder append_Json = new StringBuilder();
+            append_Json.Append("{");
+            append_Json.Append("\"msgtype\":\"news\"");
+            //按人员
+            if (!string.IsNullOrEmpty(msgNews.touser)) append_Json.Append(",\"touser\":\"" + msgNews.touser + "\"");
+            //按部门
+            if (!string.IsNullOrEmpty(msgNews.toparty)) append_Json.Append(",\"toparty\":\"" + msgNews.toparty + "\"");
+            //标签
+            if (!string.IsNullOrEmpty(msgNews.totag)) append_Json.Append(",\"totag\":\"" + msgNews.totag + "\"");
+
+            append_Json.Append(",\"agentid\":\"" + msgNews.agentid + "\"");
+            append_Json.Append(",\"news\":{");
+
+            append_Json.Append("\"articles\":[");
+            foreach (News_Articles item in msgNews.articles)
+            {
+                append_Json.Append("{");
+                append_Json.Append("\"title\":\"" + item.title + "\"");
+                append_Json.Append(",\"description\":\"" + item.description + "\"");
+                if (!string.IsNullOrEmpty(item.url)) append_Json.Append(",\"url\":\"" + item.url + "\"");
+                if (!string.IsNullOrEmpty(item.picurl)) append_Json.Append(",\"picurl\":\"" + item.picurl + "\"");
+                append_Json.Append("},");
+            }
+            append_Json.Remove(append_Json.Length - 1, 1);
+
+            append_Json.Append("]}");
+            append_Json.Append("}");
+            string str = new HttpWebResponseUtility().HttpResponsePost_Json(url, append_Json.ToString());
+            MessageErrorModel postVal = FormatToJson.ParseFromJson<MessageErrorModel>(str);
+            return postVal;
+        }
+        /// <summary>
+        /// 发送待办消息
+        /// </summary>
+        /// <param name="toEmps">到达的人员多个人员用|分开比如: zhangsan|lisi </param>
+        /// <param name="title">标题</param>
+        /// <param name="msg">发送内容</param>
+        /// <param name="sender">发送人</param>
+        /// <returns></returns>
+        public static MessageErrorModel SendMsgToUsers(string toUsers, string title, string msg, string sender)
+        {
+            //企业应用必须存在
+            string agentId = BP.Sys.SystemConfig.WX_AgentID ?? null;
+            if (BP.DA.DataType.IsNullOrEmpty(agentId) == true)
+                return null;
+
+            string accessToken = new BP.WF.WeiXin.WeiXin().getAccessToken();//获取 AccessToken
+
+            News_Articles newArticle = new News_Articles();
+            newArticle.title = title;
+            newArticle.description = msg;
+
+            string New_Url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + BP.Sys.SystemConfig.WX_CorpID
+                + "&redirect_uri=" + BP.Sys.SystemConfig.WX_MessageUrl + "/CCMobile/action.aspx&response_type=code&scope=snsapi_base&state=empwork_#wechat_redirect";
+            newArticle.url = New_Url;
+
+            //http://discuz.comli.com/weixin/weather/icon/cartoon.jpg
+            //newArticle.picurl = BP.Sys.SystemConfig.WX_MessageUrl + "/DataUser/ICON/" + BP.Sys.SystemConfig.SysNo + "/LogBig.png";
+
+            WX_Msg_News wxMsg = new WX_Msg_News();
+            wxMsg.Access_Token = accessToken;
+            wxMsg.agentid = BP.Sys.SystemConfig.WX_AgentID;
+            wxMsg.touser = toUsers;
+            wxMsg.articles.Add(newArticle);
+            //执行发送
+            return WeiXinMessage.PostMsgOfNews(wxMsg);
+        }
+    }
+}

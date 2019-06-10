01e0b2b67ab5218c2d6139d7de5232a98709c834
周朋 <hiflow@qq.com>
2019-06-10 10:41:30 +0800

    no commit message

--- Components/BP.En30/GPM/Emp.cs
+++ Components/BP.En30/GPM/Emp.cs
@@ -266,7 +266,7 @@ namespace BP.GPM
 
                 #region 字段
                 /*关于字段属性的增加 */
-                map.AddTBStringPK(EmpAttr.No, null, "登陆账号", true, false, 1, 150, 90);
+                map.AddTBStringPK(EmpAttr.No, null, "登陆账号", true, false, 1, 50, 90);
                 map.AddTBString(EmpAttr.Name, null, "名称", true, false, 0, 200, 130);
                 map.AddTBString(EmpAttr.Pass, "123", "密码", false, false, 0, 100, 10);
 

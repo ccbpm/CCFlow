0f4efc6f31874ec80005bce6ecbd0dd59c091924
周朋 <hiflow@qq.com>
2019-06-17 07:55:14 +0800

    no commit message

--- CCFlow/WF/CCForm/CCFormAPI.asmx.cs
+++ CCFlow/WF/CCForm/CCFormAPI.asmx.cs
@@ -44,7 +44,7 @@ namespace CCFlow.WF.CCForm
             BP.WF.GenerWorkFlow gwf = new BP.WF.GenerWorkFlow(workID);
 
             //是否可以查看该工作.
-            bool b = BP.WF.Dev2Interface.Flow_IsCanViewTruck(gwf.FK_Flow, gwf.WorkID, gwf.FID);
+            bool b = BP.WF.Dev2Interface.Flow_IsCanViewTruck(gwf.FK_Flow, gwf.WorkID);
             if (b == false && 1 == 2)
                 throw new Exception("err@[" + userNo + "]无权查看该流程,WorkID=" + workID);
 
@@ -170,10 +170,9 @@ namespace CCFlow.WF.CCForm
             BP.WF.Dev2Interface.Port_Login(userNo);
             BP.WF.GenerWorkFlow gwf = new BP.WF.GenerWorkFlow(workID);
 
-            bool b = BP.WF.Dev2Interface.Flow_IsCanViewTruck(gwf.FK_Flow, gwf.WorkID, gwf.FID);
+            bool b = BP.WF.Dev2Interface.Flow_IsCanViewTruck(gwf.FK_Flow, gwf.WorkID);
             if (b == false && 1 == 2)
                 throw new Exception("err@[" + userNo + "]无权查看该流程,WorkID=" + workID);
-
 
             string frmID = "ND" + int.Parse(gwf.FK_Flow) + "Rpt";
 

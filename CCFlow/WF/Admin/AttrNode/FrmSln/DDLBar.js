47bbbc8efff0469c56fa06b10fc39a27fc9dedac
周朋 <hiflow@qq.com>
2019-07-28 12:12:15 +0800

    网友提出的bug.

--- CCFlow/WF/Admin/AttrNode/FrmSln/DDLBar.js
+++ CCFlow/WF/Admin/AttrNode/FrmSln/DDLBar.js
@@ -18,13 +18,13 @@ function InitBar(optionKey) {
     html += "<option value=null  disabled='disabled'>+绑定表单库里的表单</option>";
     html += "<option value=" + FormType.RefOneFrmTree + " >&nbsp;&nbsp;绑定表单库的表单</option>";
     html += "<option value=" + FormType.SheetTree + " >&nbsp;&nbsp;绑定多表单(表单树)</option>";
-   
+
     html += "</select >";
 
     html += "<input  id='Btn_Save' type=button onclick='Save()' value='保存' />";
     html += "<input  id='Btn_SaveAndClose' type=button onclick='SaveAndClose()' value='保存并关闭' />";
 
-  //  html += "<input type=button onclick='OldVer()' value='使用旧版本' />";
+    //  html += "<input type=button onclick='OldVer()' value='使用旧版本' />";
 
     html += "<input  id='Btn_Help' type=button onclick='Help()' value='视频帮助' />";
     html += "<input  id='Btn_Help' type=button onclick='HelpOnline()' value='在线帮助' />";
@@ -40,6 +40,18 @@ function OldVer() {
     var flowNo = GetQueryString("FK_Flow");
     var url = '../NodeFromWorkModel.htm?FK_Flow=' + flowNo + '&FK_Node=' + nodeID;
     window.location.href = url;
+}
+
+///设置表单类型.
+function SetNDxxRpt_FrmType(flowNo, frmType) {
+
+    var flowID = parseInt(flowNo);
+    var frmID = "ND" + flowID + "Rpt";
+
+    var mapData = new Entity("BP.Sys.MapData", frmID);
+    mapData.FrmType = frmType;
+    mapData.Update();
+
 }
 
 
@@ -137,7 +149,7 @@ function changeOption() {
     var sele = obj.options;
     var index = obj.selectedIndex;
     var optionKey = optionKey = sele[index].value;
-    
+
     var roleName = "";
     switch (parseInt(optionKey)) {
         case FormType.FoolForm:
@@ -187,10 +199,9 @@ function changeOption() {
 }
 
 
-function CheckFlow(flowNo)
-{
-	  var flow=new Entity('BP.WF.Flow', flowNo);
-	  flow.DoMethodReturnString("DoCheck"); //重置密码:不带参数的方法. 
+function CheckFlow(flowNo) {
+    var flow = new Entity('BP.WF.Flow', flowNo);
+    flow.DoMethodReturnString("DoCheck"); //重置密码:不带参数的方法. 
 }
 
 function SaveAndClose() {

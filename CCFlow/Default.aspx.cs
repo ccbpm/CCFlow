2d6676a9df2c363b87bf799cd953877128f8d9f9
周朋 <hiflow@qq.com>
2019-09-10 15:15:30 +0800

    no commit message

--- CCFlow/Default.aspx.cs
+++ CCFlow/Default.aspx.cs
@@ -18,7 +18,7 @@ using BP.Web;
 using BP.Port;
 using BP.Demo.BPFramework;
 using ThoughtWorks.QRCode.Codec;
-  
+
 
 namespace CCFlow
 {
@@ -26,6 +26,53 @@ namespace CCFlow
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+           // BP.Web.WebUser.No
+            //TodolistModel.Teamup
+            // BP.WF.HttpHandler.WF
+            //  BP.WF.Template.FrmField
+            //   BP.DA.DBAccess.IsExitsTableCol("ND154Track", "FrmDB");
+            return;
+
+            //BP.WF.Template.PowerModel pm = new BP.WF.Template.PowerModel();
+            //pm.CheckPhysicsTable();
+            // BP.WF.Data.Delays
+            // BP.Sys.Glo.GenerRealType(
+            //BP.WF.Rpt.RptDfine.
+            return;
+            BP.WF.Template.NodeExt ext = new BP.WF.Template.NodeExt();
+            ext.NodeID = 101;
+            ext.RetrieveFromDBSources();
+            return;
+
+            BP.WF.Dev2Interface.Port_Login("admin");
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+            Int64 workid = BP.WF.Dev2Interface.Node_CreateBlankWork("057", WebUser.No);
+            string msg = BP.WF.Dev2Interface.Node_SendWork("057", workid, 0, "admin").ToMsgOfHtml();
+            this.Response.Write(msg);
+
+
+            //BP.WF.Node nd = new Node(12801);
+            // nd.FormType
+            // nd.FormType = NodeFormType.SDKForm;
+            // nd.DirectUpdate();
+            return;
+
+            BP.WF.Flow fe2 = new Flow();
+            fe2.No = "128";
+            fe2.Retrieve();
+            fe2.DoCheck();
+            return;
+
+
+            BP.WF.Template.FlowExt fe = new BP.WF.Template.FlowExt();
+            fe.CheckPhysicsTable();
+            fe.No = "128";
+            fe.Retrieve();
+
 
             BP.DA.Log.DefaultLogWriteLine(LogType.Info, "----------------------------------  start ------------------ ");
             BP.DA.Log.DefaultLogWriteLine(LogType.Info, "----------------------------------  end ------------------ ");

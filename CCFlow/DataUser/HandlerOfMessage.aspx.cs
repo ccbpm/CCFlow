de0a36ce24f2206d02728996bd1d7a8c83205687
袁丽娜 <nayuanli@126.com>
2019-10-28 18:05:09 +0800

    no commit message

--- CCFlow/DataUser/HandlerOfMessage.aspx.cs
+++ CCFlow/DataUser/HandlerOfMessage.aspx.cs
@@ -12,6 +12,33 @@ namespace CCFlow.DataUser
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            string sql = "";
+
+            string doType = this.Request.QueryString["DoType"];
+            if (doType == "SendToCCMSG")
+            {
+
+                byte[] data = new byte[this.Request.InputStream.Length];
+                this.Request.InputStream.Read(data, 0, data.Length);
+                string txt = System.Text.Encoding.UTF8.GetString(data);
+                try
+                {
+                    Dictionary<string, object> dictionary = null;
+                    dictionary = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(txt);
+                    string send = dictionary["sender"].ToString();
+                    string sendTo = dictionary["sendTo"].ToString();
+                    string tel = "";
+                    if(dictionary["tel"]!=null)
+                        tel = dictionary["tel"].ToString();
+                    string title = dictionary["title"].ToString();
+                    string content = dictionary["content"].ToString();
+                    string openUrl = dictionary["openUrl"].ToString();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
         }
     }
 }

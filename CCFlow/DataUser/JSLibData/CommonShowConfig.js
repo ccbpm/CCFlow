c3554b62666f6e3659765fe227ee36da0ef87e1e
黄果 <huangzhimin@ccflow.org>
2019-11-15 16:21:35 +0800

    no commit message

---
+++ CCFlow/DataUser/JSLibData/CommonShowConfig.js
@@ -0,0 +1,10 @@
+﻿
+/*
+1. 该页面旨在解决各个项目所需功能显示问题，定义各个‘是否显示’变量.
+2. 为了不与其他字段冲突,命名加前缀'Hide_'
+3. 根据项目差异，自行定义，自行调用. 
+*/
+var Hide_IsRead = false; //轨迹中是否显示已阅读: true 显示
+var Hide_IsOpenFrm = false; //时间轴中是否显示查看表单:true 显示
+var Hide_HastenWork = false; //在途是否显示催办按钮:true 显示
+var Hide_IsTodoList = false; //待办列表中是否显示查看授权:true 显示
--- CCFlow/WF/WorkOpt/OneWork/TimeBase.js
+++ CCFlow/WF/WorkOpt/OneWork/TimeBase.js
@@ -96,7 +96,14 @@ function InitPage() {
         var isHaveCheck = false;
         if (at == ActionType.Forward || at == ActionType.FlowOver) {
             doc += "<p><span>到达节点:</span><font color=green>" + track.NDToT + "</font><span>到达人员:</span><font color=green>" + track.EmpToT + "</font> </p>";
-            doc += "<p><span><a href=\"javascript:OpenFrm('" + workid + "','" + track.NDFrom + "','" + fk_flow + "','" + fid+"','" + track.NDFrom + "')\">查看表单</a></span></p>";
+
+            //Hide_IsOpenFrm:是否隐藏该条信息,CommonShowConfig.js中定义
+            if (Hide_IsOpenFrm == undefined) {
+                Hide_IsOpenFrm == true;
+            }
+            if (Hide_IsOpenFrm == true) {
+                doc += "<p><span><a href=\"javascript:OpenFrm('" + workid + "','" + track.NDFrom + "','" + fk_flow + "','" + fid+"','" + track.NDFrom + "')\">查看表单</a></span></p>";
+            }
 
             //找到该节点，该人员的审核track, 如果没有，就输出Msg, 可能是焦点字段。
 
@@ -199,14 +206,20 @@ function InitPage() {
                 var doc = "";
                 doc += "<span>审批人</span>";
                 doc += gwl.FK_EmpText;
+                //Hide_IsRead:是否隐藏该条信息,CommonShowConfig.js中定义
+                if (Hide_IsRead == undefined) {
+                    Hide_IsRead == true;
+                }
+                if (Hide_IsRead == true) {
 
-                doc += "<br>";
-                doc += "<span>阅读状态:</span>";
+                    doc += "<br>";
+                    doc += "<span>阅读状态:</span>";
 
-                if (gwl.IsRead == "1")
-                    doc += "<span><font color=green>已阅读.</font></span>";
-                else
-                    doc += "<span><font color=green>尚未阅读.</font></span>";
+                    if (gwl.IsRead == "1")
+                        doc += "<span><font color=green>已阅读.</font></span>";
+                    else
+                        doc += "<span><font color=green>尚未阅读.</font></span>";
+                }
                 doc += "<br>";
                 doc += "<span>工作到达日期:</span>";
                 doc += gwl.RDT;

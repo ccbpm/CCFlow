66a36874e8e3a017a4e0179ed50715a787ef6869
黄果 <huangzhimin@ccflow.org>
2019-11-22 15:39:16 +0800

    no commit message

--- CCFlow/DataUser/JSLibData/CommonShowConfig.js
+++ CCFlow/DataUser/JSLibData/CommonShowConfig.js
@@ -4,7 +4,9 @@
 2. 为了不与其他字段冲突,命名加前缀'Hide_'
 3. 根据项目差异，自行定义，自行调用. 
 */
-var Hide_IsRead = false; //轨迹中是否显示已阅读: true 显示
-var Hide_IsOpenFrm = false; //时间轴中是否显示查看表单:true 显示
-var Hide_HastenWork = false; //在途是否显示催办按钮:true 显示
-var Hide_IsTodoList = false; //待办列表中是否显示查看授权:true 显示
+var Hide_IsRead = true; //轨迹中是否显示已阅读: true 显示
+var Hide_IsOpenFrm = true; //时间轴中是否显示查看表单:true 显示
+var Hide_HastenWork = true; //在途是否显示催办按钮:true 显示
+var Hide_IsTodoList = true; //待办列表中是否显示查看授权:true 显示
+
+

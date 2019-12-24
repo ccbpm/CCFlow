aba10f2f3ddb80182403a398e07f37be0ab1620d
周朋 <hiflow@qq.com>
2019-12-24 16:23:20 +0800

    no commit message

---
+++ CCFlow/WF/Admin/DevelopDesigner/DesignerTools.js
@@ -0,0 +1,19 @@
+﻿//预览.
+function PreviewForm()
+{
+    alert('在实现中..');
+}
+
+//导入表单模版.
+function ImpFrmTemplate() {
+    alert('在实现中..');
+}
+
+//导入现有字段.
+function ImpFrmFields() {
+
+    var url = "Fields.html?FrmID=" + GetQueryString("FrmID");
+    window.open(url);
+   // alert('在实现中..');
+}
+ 

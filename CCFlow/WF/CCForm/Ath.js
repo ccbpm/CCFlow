b6320b58e8965a56c8b0af47269b46dbdbda91da
袁丽娜 <nayuanli@126.com>
2020-01-02 08:55:42 +0800

    no commit message

--- CCFlow/WF/CCForm/Ath.js
+++ CCFlow/WF/CCForm/Ath.js
@@ -511,5 +511,13 @@ function checkReg() {
     return checkRegResult;
 }
 
+function setIframeHeight() {
+    if ($("body").height() < 260) {
+        $("body").height = 260;
+    }
+    $("#" + window.frameElement.getAttribute("id"), parent.document).height($("body").height() + 40);
+}
+
+
 
    

17462a580547408efc78a6323ea97234b8a34c2d
马相旭 <15802461949@163.com>
2020-01-14 09:38:17 +0800

    修改主页样式，增加element-ui

---
+++ CCFlow/WF/Scripts/PinYin1.js
@@ -0,0 +1,18 @@
+﻿
+//生成拼音.
+function ParsePinYin(str, model, textBoxId) {
+
+    var data = SpecWords(str);
+    if (data == null) {
+        var handler = new HttpHandler("BP.WF.HttpHandler.WF_Admin_FoolFormDesigner");
+        handler.AddPara("name", str);
+        handler.AddPara("flag", model);
+        data = handler.DoMethodReturnString("ParseStringToPinyin");
+    }
+
+    if (textBoxId == undefined || textBoxId == null)
+        return data;
+
+    document.getElementById(textBoxId).value = data;
+    return data;
+}

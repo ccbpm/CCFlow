6639abc45c587ddcebec6fad0b56797d06120811
李震 <5197730+YCDMW@users.noreply.gitee.com>
2020-02-04 17:24:40 +0800

    no commit message

--- CCFlow/WF/Admin/Admin.js
+++ CCFlow/WF/Admin/Admin.js
@@ -5,7 +5,7 @@
  *  3. 目前增加了两个模式的操作。 
  *     3.1 fieldset 的 legend 的ID 包含 help 就是帮助内容页面. 出现的效果是加载后，是隐藏的，用户点击文字就要显示里面的内容
  *     3.2 对于textArea 如果在 class='SQL' 系统就认为是表达式sql文本输入框.
- *     3.3 对于标记 class="HelpImg" 的图片绑定事件，在click 让其可以全屏打开.
+ *     3.3 对于标记 class="Help" 的图片绑定事件，在click 让其可以全屏打开.
  *     3.4 class=Help 的div都是帮助用的div. 比如下面，想办法，让其点击隐藏与显示. 
  *     http://localhost:2207/WF/Admin/AttrNode/Selector/3.SQL.htm?FK_Node=1702
  *       <div id="DivHelp1" class="help">
@@ -33,9 +33,10 @@ $(document).ready(function () {
     //设置自动提示.
     initToggle();
 
-    //设置  class="HelpImg" 的图片给他增加上onclick事件,点击直接可以全屏放大打开.
+    //设置  class="Help" 的图片 点击直接可以全屏放大打开.  @lz
     SetHelpImg();
-
+    //设置放大的img容器   @lz
+    SetBigImgDiv();
     //设置SQL脚本编辑器.
     CheckSQLTextArea();
    
@@ -101,14 +102,62 @@ function HelpDiv() {
 
         }
     });
-
 }
 
-
+//设置  class="HelpImg" 的图片 点击直接可以全屏放大打开.  @lz
 function SetHelpImg() {
+    $(function () {
+        $("img.help").click(function () {
+            var _this = $(this);//将当前的pimg元素作为_this传入函数  
+            imgShow("#outerdiv", "#innerdiv", "#bigimg", _this);
+        });
+    });
 
+    function imgShow(outerdiv, innerdiv, bigimg, _this) {
+        var src = _this.attr("src");//获取当前点击的pimg元素中的src属性  
+        $(bigimg).attr("src", src);//设置#bigimg元素的src属性  
+
+        /*获取当前点击图片的真实大小，并显示弹出层及大图*/
+        $("<img/>").attr("src", src).load(function () {
+            var windowW = $(window).width();//获取当前窗口宽度  
+            var windowH = $(window).height();//获取当前窗口高度  
+            var realWidth = this.width;//获取图片真实宽度  
+            var realHeight = this.height;//获取图片真实高度  
+            var imgWidth, imgHeight;
+            var scale = 0.8;//缩放尺寸，当图片真实宽度和高度大于窗口宽度和高度时进行缩放  
+
+            if (realHeight > windowH * scale) {//判断图片高度  
+                imgHeight = windowH * scale;//如大于窗口高度，图片高度进行缩放  
+                imgWidth = imgHeight / realHeight * realWidth;//等比例缩放宽度  
+                if (imgWidth > windowW * scale) {//如宽度扔大于窗口宽度  
+                    imgWidth = windowW * scale;//再对宽度进行缩放  
+                }
+            } else if (realWidth > windowW * scale) {//如图片高度合适，判断图片宽度  
+                imgWidth = windowW * scale;//如大于窗口宽度，图片宽度进行缩放  
+                imgHeight = imgWidth / realWidth * realHeight;//等比例缩放高度  
+            } else {//如果图片真实高度和宽度都符合要求，高宽不变  
+                imgWidth = realWidth;
+                imgHeight = realHeight;
+            }
+            $(bigimg).css("width", imgWidth);//以最终的宽度对图片缩放  
+
+            var w = (windowW - imgWidth) / 2;//计算图片与窗口左边距  
+            var h = (windowH - imgHeight) / 2;//计算图片与窗口上边距  
+            $(innerdiv).css({ "top": h, "left": w });//设置#innerdiv的top和left属性  
+            $(outerdiv).fadeIn("fast");//淡入显示#outerdiv及.pimg  
+        });
+
+        $(outerdiv).click(function () {//再次点击淡出消失弹出层  
+            $(this).fadeOut("fast");
+        });
+    }
+    
 }
-
+//加载放大的img容器  @lz
+function SetBigImgDiv() {
+    var divs = "<div id='outerdiv' style='position:fixed;top:0;left:0;background:rgba(0,0,0,0.7);z-index:2;width:100%;height:100%;display:none;'><div id='innerdiv' style='position:absolute;'><img id='bigimg' style='border:5px solid #fff;' src=''/></div ></div >";
+    $(".cs-content-box").append(divs);
+}
 //设置SQL脚本编辑器. 如果遇到 textarea 的className=SQL的，我们就默认为该文本框是
 //要sql的格式，就给他增加上sql的模式.
 function CheckSQLTextArea() {

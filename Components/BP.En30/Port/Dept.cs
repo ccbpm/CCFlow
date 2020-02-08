c31eec7b234ce8a312beed3224fa0ed6ddb3766c
袁丽娜 <nayuanli@126.com>
2020-02-08 15:45:53 +0800

    no commit message

--- Components/BP.En30/Port/Dept.cs
+++ Components/BP.En30/Port/Dept.cs
@@ -16,6 +16,7 @@ namespace BP.Port
         /// 父节点的编号
         /// </summary>
         public const string ParentNo = "ParentNo";
+        public const string Idx = "Idx";
     }
     /// <summary>
     /// 部门
@@ -104,7 +105,7 @@ namespace BP.Port
                 map.AddTBStringPK(DeptAttr.No, null, "编号", true, false, 1, 50, 20);
                 map.AddTBString(DeptAttr.Name, null, "名称", true, false, 0, 100, 30);
                 map.AddTBString(DeptAttr.ParentNo, null, "父节点编号", true, true, 0, 100, 30);
-
+                map.AddTBInt(DeptAttr.Idx, 0, "序号", false, true);
 
 
                 RefMethod rm = new RefMethod();

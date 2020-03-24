22554d5fba5066cfad3809ad64d52cdff452e09c
史连雨 <2409785862@qq.com>
2020-03-24 15:57:20 +0800

    f12时lang的问题报404

--- CCFlow/WF/Data/lang/load.js
+++ CCFlow/WF/Data/lang/load.js
@@ -2,5 +2,5 @@
 //zh-cn   zh-tw  zh-hk  en-us ja-jp ko-kr
 var currentLang = "zh-cn";
 //var currentLang = "en-us";
-document.write("<script language=javascript src='./Data/lang/js/" + currentLang + ".js'></script>");
-document.write("<script language=javascript src='../Data/lang/js/" + currentLang + ".js'></script>");
+//document.write("<script language=javascript src='./Data/lang/js/" + currentLang + ".js'></script>");
+//document.write("<script language=javascript src='../Data/lang/js/" + currentLang + ".js'></script>");

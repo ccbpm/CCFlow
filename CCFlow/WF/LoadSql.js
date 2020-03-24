d62dba8a29dd1b6db1c255ec461346d924138bf7
李震 <5197730+YCDMW@users.noreply.gitee.com>
2020-03-24 11:14:54 +0800

    no commit message

---
+++ CCFlow/WF/LoadSql.js
@@ -0,0 +1,22 @@
+﻿window.onload = function () {
+    var mime = 'text/x-mariadb';
+    // get mime type
+    if (window.location.href.indexOf('mime=') > -1) {
+        mime = window.location.href.substr(window.location.href.indexOf('mime=') + 5);
+    }
+    window.editor = CodeMirror.fromTextArea(document.getElementById('TB_SQL'), {
+        mode: mime,
+        indentWithTabs: true,
+        smartIndent: true,
+        lineNumbers: true,
+        matchBrackets: true,
+        autofocus: true,
+        extraKeys: { "Ctrl-Space": "autocomplete" },
+        hintOptions: {
+            tables: {
+                users: ["name", "score", "birthDate"],
+                countries: ["name", "population", "size"]
+            }
+        }
+    });
+};

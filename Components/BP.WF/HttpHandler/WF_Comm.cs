abdc7314ebe5f80d52772c8a39ebe8101161116e
ccflowlover <ccbpm@ccflow.org>
2020-03-25 19:04:21 +0800

    no commit message

--- Components/BP.WF/HttpHandler/WF_Comm.cs
+++ Components/BP.WF/HttpHandler/WF_Comm.cs
@@ -3454,8 +3454,7 @@ namespace BP.WF.HttpHandler
                 return BP.Tools.Json.ToJson(ht);
             }
 
-            ht.Add("No", WebUser.No);
-            ht.Add("OrgNo", WebUser.OrgNo);//zsy 2020.3.25 saas模式需要这个参数
+            ht.Add("No", WebUser.No);          
             ht.Add("Name", WebUser.Name);
             ht.Add("FK_Dept", WebUser.FK_Dept);
             ht.Add("FK_DeptName", WebUser.FK_DeptName);

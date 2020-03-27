d949c9c6d1cf01c28adbfcfc11d5b0abe9f1b549
袁丽娜 <nayuanli@126.com>
2020-03-27 18:09:11 +0800

    no commit message

--- Components/BP.WF/HttpHandler/CCMobile_CCForm.cs
+++ Components/BP.WF/HttpHandler/CCMobile_CCForm.cs
@@ -12,6 +12,10 @@ using BP.En;
 using BP.WF;
 using BP.WF.Template;
 using System.Net.Http;
+using System.Collections;
+using NPOI.SS.Formula.Functions;
+using LitJson;
+using System.Net;
 
 namespace BP.WF.HttpHandler
 {
@@ -487,6 +491,45 @@ namespace BP.WF.HttpHandler
             HttpResponseMessage response = client.PostAsync(authHost, new FormUrlEncodedContent(paraList)).Result;
             String result = response.Content.ReadAsStringAsync().Result;
             Console.WriteLine(result);
+
+            return result;
+        }
+
+        public static String getFileBase64(String fileName)
+        {
+            FileStream filestream = new FileStream(fileName, FileMode.Open);
+            byte[] arr = new byte[filestream.Length];
+            filestream.Read(arr, 0, (int)filestream.Length);
+            string baser64 = Convert.ToBase64String(arr);
+            filestream.Close();
+            return baser64;
+        }
+
+        public string GetIDCardInfo()
+        {
+            string token = getAccessToken();
+            JsonData jd = JsonMapper.ToObject(token);
+            string host = "https://aip.baidubce.com/rest/2.0/ocr/v1/idcard?access_token=" + jd["access_token"].ToString();
+            Encoding encoding = Encoding.Default;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(host);
+            request.Method = "post";
+            request.KeepAlive = true;
+            // 图片的base64编码
+            var files = HttpContextHelper.RequestFiles();  //context.Request.Files;
+            if (files.Count == 0)
+                return "err@请选择要上传的身份证件。";
+            Stream stream = files[0].InputStream;//new MemoryStream();
+            byte[] bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, bytes.Length);
+            string base64 = Convert.ToBase64String(bytes);
+            stream.Close();
+            String str = "id_card_side=" + "front" + "&image=" + HttpUtility.UrlEncode(base64);
+            byte[] buffer = encoding.GetBytes(str);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            string result = reader.ReadToEnd();
             return result;
         }
     }

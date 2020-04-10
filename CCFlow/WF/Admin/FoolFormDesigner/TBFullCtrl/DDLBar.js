fc363ce44019ba9fec228183bf426015233de9c3
史连雨 <2409785862@qq.com>
2020-04-10 09:20:43 +0800

    no commit message

--- CCFlow/WF/Admin/FoolFormDesigner/TBFullCtrl/DDLBar.js
+++ CCFlow/WF/Admin/FoolFormDesigner/TBFullCtrl/DDLBar.js
@@ -11,7 +11,7 @@ function InitBar(optionKey) {
     html += "</select >";
 
     html += "<input  id='Btn_Save' type=button onclick='Save()' value='保存' />";
-    //html += "<input  id='Btn_FullData' type=button onclick='FullData()' value='填充设置' />";
+    html += "<input  id='Btn_FullData' type=button onclick='FullData()' value='填充设置' />";
     //html += "<input  id='Btn_Help' type=button onclick='HelpOnline()' value='在线帮助' />";
 
     document.getElementById("bar").innerHTML = html;

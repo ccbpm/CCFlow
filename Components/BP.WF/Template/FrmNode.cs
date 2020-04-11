3ab9479e2d92a6a0604239db27579480724626bc
袁丽娜 <nayuanli@126.com>
2020-04-11 14:02:43 +0800

    no commit message

--- Components/BP.WF/Template/FrmNode.cs
+++ Components/BP.WF/Template/FrmNode.cs
@@ -289,7 +289,7 @@ namespace BP.WF.Template
             }
             set
             {
-                this.SetValByKey(FrmNodeAttr.IsEnableFWC, value);
+                this.SetValByKey(FrmNodeAttr.IsEnableFWC, (int)value);
             }
         }
         /// <summary>

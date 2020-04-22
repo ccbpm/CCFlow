cae0d52669943d0cd0f9f70c018dec816ceee26c
驰骋BPM <zhoupeng@ccflow.org>
2020-04-22 10:21:32 +0800

    no commit message

--- Components/BP.WF/HttpHandler/WF_Admin_Template.cs
+++ Components/BP.WF/HttpHandler/WF_Admin_Template.cs
@@ -32,7 +32,7 @@ namespace BP.WF.HttpHandler
             get
             {
 
-                FtpSupport.FtpConnection conn = new FtpSupport.FtpConnection(Glo.TemplateFTPHost, Glo.TemplateFTPUser, Glo.TemplateFTPPassword );
+                FtpSupport.FtpConnection conn = new FtpSupport.FtpConnection(Glo.TemplateFTPHost, Glo.TemplateFTPUser, Glo.TemplateFTPPassword);
                 return conn;
             }
         }
@@ -42,10 +42,54 @@ namespace BP.WF.HttpHandler
             if (DataType.IsNullOrEmpty(dirName) == true)
                 dirName = "/";
 
+            FtpSupport.FtpConnection conn = this.GenerFTPConn;
+            //string strs  =conn.FindFiles();
+            DataSet ds = new DataSet();
 
-            return null;
+            DataTable dt = new DataTable();
+            dt.TableName = "Dir";
+            dt.Columns.Add("Path", typeof(string));
+            dt.Columns.Add("Name", typeof(string));
+            ds.Tables.Add(dt);
+
+
+            DataTable dt2 = new DataTable();
+            dt2.TableName = "File";
+            dt2.Columns.Add("Path", typeof(string));
+            dt2.Columns.Add("Name", typeof(string));
+            ds.Tables.Add(dt2);
+
+            return BP.Tools.Json.ToJson(ds);
+        }
+        /// <summary>
+        /// 导入文件
+        /// </summary>
+        /// <returns></returns>
+        public string Flow_Imp()
+        {
+            string fls = this.GetRequestVal("Files");
+            string[] strs = fls.Split(';');
+
+            string sortNo = GetRequestVal("SortNo");
+
+            FtpConnection conn = this.GenerFTPConn();
+
+            foreach (string str in strs)
+            {
+                //定义临时文件名.
+                string tempfile = BP.Sys.SystemConfig.PathOfTemp + "\\" + BP.Web.WebUser.No + ".xml";
+
+                //下载到临时的目录下.
+                conn.GetFile(str, tempfile, true, System.IO.FileAttributes.Normal);
+
+                //执行导入.
+                Flow.DoLoadFlowTemplate(sortNo, tempfile, ImpFlowTempleteModel.AsNewFlow);
+            }
+            return "导入成功，请刷新，或者退出重新登录.";
         }
         #endregion 界面方法.
 
     }
+
+
 }

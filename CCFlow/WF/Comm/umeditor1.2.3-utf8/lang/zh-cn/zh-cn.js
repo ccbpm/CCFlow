b4694f0ba2d9aad2c9b33082f3fb4fd57fa3ccb2
史连雨 <2409785862@qq.com>
2020-05-14 17:07:36 +0800

    no commit message

--- CCFlow/WF/Comm/umeditor1.2.3-utf8/lang/zh-cn/zh-cn.js
+++ CCFlow/WF/Comm/umeditor1.2.3-utf8/lang/zh-cn/zh-cn.js
@@ -25,7 +25,8 @@ UM.I18N['zh-cn'] = {
     },
     'paragraph':{'p':'段落', 'h1':'标题 1', 'h2':'标题 2', 'h3':'标题 3', 'h4':'标题 4', 'h5':'标题 5', 'h6':'标题 6'},
     'fontfamily':{
-        'songti':'宋体',
+        'songti': '宋体',
+        'FangSong_GB2312': '仿宋_GB2312',
         'kaiti':'楷体',
         'heiti':'黑体',
         'lishu':'隶书',
--- CCFlow/WF/Comm/umeditor1.2.3-utf8/umeditor.config.js
+++ CCFlow/WF/Comm/umeditor1.2.3-utf8/umeditor.config.js
@@ -223,10 +223,20 @@ etpl.config({
 
         //fontfamily
         //字体设置
-        //        ,'fontfamily':[
-        //              { name: 'songti', val: '宋体,SimSun'},
-        //          ]
-
+        , 'fontfamily': [
+            { label: '', name: 'songti', val: '宋体,SimSun' },
+            { label: '', name: 'FangSong_GB2312', val: '仿宋_GB2312 ,FangSong_GB2312' },
+            { label: '', name: 'kaiti', val: '楷体,楷体_GB2312, SimKai' },
+            { label: '', name: 'yahei', val: '微软雅黑,Microsoft YaHei' },
+            { label: '', name: 'heiti', val: '黑体, SimHei' },
+            { label: '', name: 'lishu', val: '隶书, SimLi' },
+            { label: '', name: 'andaleMono', val: 'andale mono' },
+            { label: '', name: 'arial', val: 'arial, helvetica,sans-serif' },
+            { label: '', name: 'arialBlack', val: 'arial black,avant garde' },
+            { label: '', name: 'comicSansMs', val: 'comic sans ms' },
+            { label: '', name: 'impact', val: 'impact,chicago' },
+            { label: '', name: 'timesNewRoman', val: 'times new roman' }
+        ]
         //fontsize
         //字号
         //,'fontsize':[10, 11, 12, 14, 16, 18, 20, 24, 36]

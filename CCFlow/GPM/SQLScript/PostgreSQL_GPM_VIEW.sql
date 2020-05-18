0d98de38e9ee009049d1d93032ac1cd363b85983
刘强 <1458944162@qq.com>
2020-05-18 17:55:00 +0800

    no commit message

--- CCFlow/GPM/SQLScript/PostgreSQL_GPM_VIEW.sql
+++ CCFlow/GPM/SQLScript/PostgreSQL_GPM_VIEW.sql
@@ -3,7 +3,7 @@
 --GO-- 
 CREATE VIEW V_GPM_EmpGroup
 AS
-SELECT FK_Group,FK_Emp FROM Port_TeamEmp
+SELECT FK_Group,FK_Emp FROM GPM_GroupEmp
 UNION
 SELECT a.FK_Group,B.FK_Emp FROM GPM_GroupStation a, Port_DeptEmpStation b 
 WHERE a.FK_Station=b.FK_Station
@@ -29,29 +29,14 @@ WHERE a.FK_Station = b.FK_Station
 CREATE VIEW V_GPM_EmpMenu
 AS
 select distinct c.* from (
-SELECT a.FK_Emp+'_'+a.FK_Menu as MyPK, a.FK_Emp, b.No as FK_Menu, b.* FROM 
+SELECT a.FK_Emp||'_'||a.FK_Menu as MyPK, a.FK_Emp, b.No as FK_Menu, b.* FROM 
    GPM_EmpMenu a,GPM_Menu b WHERE a.FK_Menu=b.No AND B.IsEnable=1
 UNION
-SELECT a.FK_Emp+'_'+a.FK_Menu as MyPK, a.FK_Emp, b.No as FK_Menu, b.* FROM 
+SELECT a.FK_Emp||'_'||a.FK_Menu as MyPK, a.FK_Emp, b.No as FK_Menu, b.* FROM 
   V_GPM_EmpGroupMenu a,GPM_Menu b  WHERE a.FK_Menu=b.No AND B.IsEnable=1
 UNION
-SELECT a.FK_Emp+'_'+a.FK_Menu as MyPK, a.FK_Emp, b.No as FK_Menu, b.* 
+SELECT a.FK_Emp||'_'||a.FK_Menu as MyPK, a.FK_Emp, b.No as FK_Menu, b.* 
 FROM V_GPM_EmpStationMenu a,GPM_Menu b WHERE a.FK_Menu=b.No AND B.IsEnable=1
 ) c
 
-
---GO--
---DROP VIEW V_GPM_EmpMenu_GPM
---GO--
-CREATE VIEW V_GPM_EmpMenu_GPM
-AS
-select distinct c.* from (
-SELECT a.FK_Emp+'_'+a.FK_Menu as MyPK, a.FK_Emp,a.IsChecked, b.No as FK_Menu, b.* FROM 
-   GPM_EmpMenu a,GPM_Menu b WHERE a.FK_Menu=b.No AND B.IsEnable=1
-UNION
-SELECT a.FK_Emp+'_'+a.FK_Menu as MyPK, a.FK_Emp,a.IsChecked, b.No as FK_Menu, b.* FROM 
-  V_GPM_EmpGroupMenu a,GPM_Menu b  WHERE a.FK_Menu=b.No AND B.IsEnable=1
-UNION
-SELECT a.FK_Emp+'_'+a.FK_Menu as MyPK, a.FK_Emp,a.IsChecked, b.No as FK_Menu, b.* 
-FROM V_GPM_EmpStationMenu a,GPM_Menu b WHERE a.FK_Menu=b.No AND B.IsEnable=1
-) c
+ 

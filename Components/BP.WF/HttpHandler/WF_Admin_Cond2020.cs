0b97b028a4ff71f0e84807864d54726c2f9ad3c2
驰骋BPM <zhoupeng@ccflow.org>
2020-05-29 16:21:57 +0800

    no commit message

--- Components/BP.WF/HttpHandler/WF_Admin_Cond2020.cs
+++ Components/BP.WF/HttpHandler/WF_Admin_Cond2020.cs
@@ -31,7 +31,7 @@ namespace BP.WF.HttpHandler
         /// <returns></returns>
         public string List_Init()
         {
-           // BP.WF.Template.CondAttr.ToNodeID
+            // BP.WF.Template.CondAttr.ToNodeID
             // Conds condes = new Conds();
             // condes.RetrieveAll();
             return "";
@@ -42,55 +42,52 @@ namespace BP.WF.HttpHandler
         /// <returns></returns>
         public string List_DoCheck()
         {
+            string str = "";
+
+            string mystr = this.GetRequestVal("ToNodeID");
+            int toNodeID = this.FK_Node;
+            if (DataType.IsNullOrEmpty(mystr) == false)
+                toNodeID = int.Parse(mystr);
+
+            //集合.
+            Conds conds = new Conds();
+            conds.Retrieve(CondAttr.FK_Node, this.FK_Node, CondAttr.ToNodeID,
+                toNodeID, CondAttr.CondType, this.GetRequestValInt("CondType"), CondAttr.Idx);
+
+            //遍历方向条件.
+            foreach (Cond item in conds)
+            {
+                if (item.HisDataFrom == ConnDataFrom.CondOperator)
+                    str += " " + item.FK_Operator;
+                else
+                    str += " 1=1 "; // + item.AttrKey + item.FK_Operator + item.OperatorValue;
+            }
+
+            string sql = "";
+            switch (SystemConfig.AppCenterDBType)
+            {
+                case DBType.MSSQL:
+                    sql = " SELECT TOP 1 No FROM Port_Emp WHERE " + str;
+                    break;
+                case DBType.MySQL:
+                    sql = " SELECT No FROM Port_Emp WHERE " + str + " AND limit 1 ";
+                    break;
+                case DBType.Oracle:
+                case DBType.DM:
+                    sql = " SELECT No FROM Port_Emp WHERE " + str + " AND rownum <=1 ";
+                    break;
+                default:
+                    return "err@没有做的数据库类型判断.";
+            }
+
             try
             {
-                string mystr = this.GetRequestVal("ToNodeID");
-                int toNodeID = this.FK_Node;
-                if (DataType.IsNullOrEmpty(mystr) ==false)
-                    toNodeID = int.Parse(mystr);
-
-                //集合.
-                Conds conds = new Conds();
-                conds.Retrieve(CondAttr.FK_Node, this.FK_Node, CondAttr.ToNodeID,
-                    toNodeID, CondAttr.CondType, this.GetRequestValInt("CondType"), CondAttr.Idx);
-
-                //遍历方向条件.
-                string str = "";
-                foreach (Cond item in conds)
-                {
-                    if (item.HisDataFrom == ConnDataFrom.CondOperator)
-                        str += " " + item.FK_Operator;
-                    else
-                        str += " 1=1 "; // + item.AttrKey + item.FK_Operator + item.OperatorValue;
-                }
-
-                string sql = "";
-                switch (SystemConfig.AppCenterDBType)
-                {
-                    case DBType.MSSQL:
-                        sql = " SELECT TOP 1 No FROM Port_Emp WHERE " + str;
-                        break;
-                    case DBType.MySQL:
-                        sql = " SELECT No FROM Port_Emp WHERE " + str + " AND limit 1 ";
-                        break;
-                    case DBType.Oracle:
-                    case DBType.DM:
-                        sql = " SELECT No FROM Port_Emp WHERE " + str + " AND rownum <=1 ";
-                        break;
-                    default:
-                        return "err@没有做的数据库类型判断.";
-                }
-
                 DataTable dt = DBAccess.RunSQLReturnTable(sql);
-                if (dt.Rows.Count == 1)
-                    return "条件设置格式正确." + sql;
-
-                return "条件设置格式正确." + sql;
+                return "格式正确:<font color=blue>" + str + "</blue>";
             }
             catch (Exception ex)
             {
-                //return "err@条件设置不符合规范.:\n" + ex.Message;
-                return "err@条件设置不符合规范. <font color=blue>" + ex.Message + "</font>";
+                return "err@不符合规范. <font color=blue>" + str + "</font>";
             }
         }
 

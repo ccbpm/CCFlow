447ccb94760fb7c2f304f27cb6e7c574aeead84e
史连雨 <2409785862@qq.com>
2020-06-01 09:41:24 +0800

    按钮图标

--- CCFlow/DataUser/OverrideFiles/ToolbarImg/ToolbarIcon.js
+++ CCFlow/DataUser/OverrideFiles/ToolbarImg/ToolbarIcon.js
@@ -1,1 +1,6 @@
 // Ôö¼ÓÍ¼±ê
+function ToolbarIcon() {
+    //$("#userInfocancel").button("option", "icons", { primary: 'ui-icon-cancel', secondary: 'ui-icon-cancel' }); 
+    $("input[name='Send']").css('background','#FFF url(../DataUser/OverrideFiles/ToolbarImg/Send.png) no-repeat left 50%');
+    //$("input[name='Send']").css('margin-left','4px');
+}

a4cc7388974c7ffec942e48a9d2e99f3083ebb9b
驰骋BPM <zhoupeng@ccflow.org>
2020-06-03 10:06:21 +0800

    no commit message

--- Components/BP.En30/DA/Log.cs
+++ Components/BP.En30/DA/Log.cs
@@ -5,10 +5,10 @@ using BP.Sys;
 
 namespace BP.DA
 {
-	#region enum LogType
-	/// <summary>
-	/// 信息类型
-	/// </summary>
+    #region enum LogType
+    /// <summary>
+    /// 信息类型
+    /// </summary>
     public enum LogType
     {
         /// <summary>
@@ -24,31 +24,31 @@ namespace BP.DA
         /// </summary>
         Error = 3
     }
-	#endregion
+    #endregion
 
-	#region class Log	
-	/// <summary>
-	/// 日志
-	/// </summary>
-	public class Log
-	{
-		#region 在测试状态下
-		public static void DebugWriteInfo(string msg)
-		{
-		//	if (SystemConfig.IsDebug)
-				DefaultLogWriteLine(LogType.Info,msg);
-		}
-		public static void DebugWriteWarning(string msg)
-		{
-			//if (SystemConfig.IsDebug)
-				DefaultLogWriteLine(LogType.Warning,msg);
-		}
+    #region class Log	
+    /// <summary>
+    /// 日志
+    /// </summary>
+    public class Log
+    {
+        #region 在测试状态下
+        public static void DebugWriteInfo(string msg)
+        {
+            //	if (SystemConfig.IsDebug)
+            DefaultLogWriteLine(LogType.Info, msg);
+        }
+        public static void DebugWriteWarning(string msg)
+        {
+            //if (SystemConfig.IsDebug)
+            DefaultLogWriteLine(LogType.Warning, msg);
+        }
         public static void DebugWriteError(string msg)
         {
             //  if (SystemConfig.IsDebug)
             DefaultLogWriteLine(LogType.Error, msg);
         }
-		#endregion
+        #endregion
 
         public static void DefaultLogWriteLineError(Exception ex)
         {
@@ -58,10 +58,10 @@ namespace BP.DA
             DefaultLogWriteLine(LogType.Error, ex.StackTrace);
         }
 
-		public static void DefaultLogWriteLineError(string msg)
-		{
-			DefaultLogWriteLine(LogType.Error,msg);
-		}
+        public static void DefaultLogWriteLineError(string msg)
+        {
+            DefaultLogWriteLine(LogType.Error, msg);
+        }
 
         public static void DefaultLogWriteLineError(string msg, bool isOutDos)
         {
@@ -70,10 +70,10 @@ namespace BP.DA
                 System.Console.WriteLine(msg);
         }
 
-		public static void DefaultLogWriteLineInfo(string msg)
-		{
-			DefaultLogWriteLine(LogType.Info,msg);
-		}
+        public static void DefaultLogWriteLineInfo(string msg)
+        {
+            DefaultLogWriteLine(LogType.Info, msg);
+        }
 
         public static void DefaultLogWriteLineInfo(string msg, bool isoutDos)
         {
@@ -82,10 +82,10 @@ namespace BP.DA
                 System.Console.WriteLine(msg);
         }
 
-		public static void DefaultLogWriteLineWarning(string msg)
-		{		
-			DefaultLogWriteLine(LogType.Warning,msg);
-		}
+        public static void DefaultLogWriteLineWarning(string msg)
+        {
+            DefaultLogWriteLine(LogType.Warning, msg);
+        }
         public static void DefaultLogWriteLineWarning(string msg, bool isOutdoc)
         {
             DefaultLogWriteLine(LogType.Warning, msg);
@@ -93,28 +93,28 @@ namespace BP.DA
                 System.Console.WriteLine(msg);
         }
 
-		#region 经常使用的静态方法
-		private static Log _log = new Log( Log.GetLogFileName() );
-		public static void DefaultLogWriteLine(LogType type, string info)
-		{
-			_log.WriteLine(type, info);
-		}
+        #region 经常使用的静态方法
+        private static Log _log = new Log(Log.GetLogFileName());
+        public static void DefaultLogWriteLine(LogType type, string info)
+        {
+            _log.WriteLine(type, info);
+        }
         public static void DefaultLogWriteLineWithOutUseInfo(string info)
         {
             _log.openFile();
             _log.writelog(info);
             _log.closeFile();
         }
-		#endregion
+        #endregion
 
-		private bool isReady = false;
-		private StreamWriter swLog;
-		private string strLogFile;
-		private string userName="System";
-		/// <summary>
-		/// 构造函数
-		/// </summary>
-		/// <param name="LogFileName"></param>
+        private bool isReady = false;
+        private StreamWriter swLog;
+        private string strLogFile;
+        private string userName = "System";
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="LogFileName"></param>
         public Log(string LogFileName)
         {
             this.strLogFile = LogFileName;
@@ -128,24 +128,24 @@ namespace BP.DA
                 closeFile();
             }
         }
-		/// <summary>
-		/// 用户
-		/// </summary>
-		public string UserName
-		{
-			set{this.userName = value;}
-		}
-		private void writelog(string msg) 
-		{
-			if(isReady) 
-			{
-				swLog.WriteLine(msg);
-			} 
-			else 
-			{
-				Console.WriteLine("Error Cannot write to log file.");
-			}
-		}
+        /// <summary>
+        /// 用户
+        /// </summary>
+        public string UserName
+        {
+            set { this.userName = value; }
+        }
+        private void writelog(string msg)
+        {
+            if (isReady)
+            {
+                swLog.WriteLine(msg);
+            }
+            else
+            {
+                Console.WriteLine("Error Cannot write to log file.");
+            }
+        }
         public static void ClearLog()
         {
             try
@@ -168,72 +168,72 @@ namespace BP.DA
                 isReady = false;
             }
         }
-		private void closeFile() 
-		{			
-			if(isReady)
-			{
-				try 
-				{
-					swLog.Flush();
-					swLog.Close();
-				} 
-				catch 
-				{
-				}
-			}
-		}
-		/// <summary>
-		/// 取得Log的文件路径和文件名
-		/// </summary>
-		/// <param name="type"></param>
-		/// <returns></returns>
-		public static string GetLogFileName()
-		{
-			string filepath=SystemConfig.PathOfLog ;
-			
-			//如果目录没有建立，则建立.
-			if (Directory.Exists(filepath) == false) 
-				Directory.CreateDirectory(filepath);
+        private void closeFile()
+        {
+            if (isReady)
+            {
+                try
+                {
+                    swLog.Flush();
+                    swLog.Close();
+                }
+                catch
+                {
+                }
+            }
+        }
+        /// <summary>
+        /// 取得Log的文件路径和文件名
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static string GetLogFileName()
+        {
+            string filepath = SystemConfig.PathOfLog;
 
-			return filepath + "\\"+ DateTime.Now.ToString("yyyy_MM_dd") + ".log";
+            //如果目录没有建立，则建立.
+            if (Directory.Exists(filepath) == false)
+                Directory.CreateDirectory(filepath);
 
-			//return filepath + "\\"+ DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ".log";
-		}
-		/// <summary>
-		/// 写一行日志
-		/// </summary>
-		/// <param name="message"></param>
-		public void WriteLine(string message)
-		{
-			WriteLine(LogType.Info,message);
-		}
-		/// <summary>
-		/// 写一行日志
-		/// </summary>
-		/// <param name="logtype"></param>
-		/// <param name="message"></param>
-		public void WriteLine(LogType logtype, string message) 
-		{
-//			string stub = DateTime.Now.ToString("@HH:MM:ss") ;
-			string stub = DateTime.Now.ToString("dd日HH时mm分ss秒") ;
-			switch(logtype) 
-			{
-				case LogType.Info:
-					stub += "Info:";
-					break;
-				case LogType.Warning:
-					stub += "Warning:";
-					break;
-				case LogType.Error:
-					stub += "Fatal:";
-					break;
-			}
-			stub = stub + userName + "');\"" + message;
-			openFile();
-			writelog(stub);
-			closeFile();
-			//Console.WriteLine(stub);
-		}
+            return filepath + "\\" + DateTime.Now.ToString("yyyy_MM_dd") + ".log";
+
+            //return filepath + "\\"+ DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ".log";
+        }
+        /// <summary>
+        /// 写一行日志
+        /// </summary>
+        /// <param name="message"></param>
+        public void WriteLine(string message)
+        {
+            WriteLine(LogType.Info, message);
+        }
+        /// <summary>
+        /// 写一行日志
+        /// </summary>
+        /// <param name="logtype"></param>
+        /// <param name="message"></param>
+        public void WriteLine(LogType logtype, string message)
+        {
+            //			string stub = DateTime.Now.ToString("@HH:MM:ss") ;
+            string stub = DateTime.Now.ToString("dd日HH时mm分ss秒");
+            switch (logtype)
+            {
+                case LogType.Info:
+                    stub += "Info:";
+                    break;
+                case LogType.Warning:
+                    stub += "Warning:";
+                    break;
+                case LogType.Error:
+                    stub += "Fatal:";
+                    break;
+            }
+            stub = stub + userName + "');\"" + message;
+            openFile();
+            writelog(stub);
+            closeFile();
+            //Console.WriteLine(stub);
+        }
         /// <summary>
         /// 打开日志目录
         /// </summary>
@@ -259,12 +259,12 @@ namespace BP.DA
             {
                 System.Diagnostics.Process.Start(file);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                throw new Exception("@打开日志文件出现错误。"+ex.Message );
+                throw new Exception("@打开日志文件出现错误。" + ex.Message);
             }
         }
 
-	}
-	#endregion
+    }
+    #endregion
 }

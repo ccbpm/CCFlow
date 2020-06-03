3738473112050050eb3a4770cf1e63fa068183d6
袁丽娜 <nayuanli@126.com>
2020-06-03 17:15:46 +0800

    no commit message

--- CCFlow/WF/Toolbar.js
+++ CCFlow/WF/Toolbar.js
@@ -647,7 +647,7 @@ function execSend(toNodeID, formType) {
 //发送 退回 移交等执行成功后转到  指定页面
 var interval;
 function OptSuc(msg) {
-    if (window.parent != null && window.parent.WindowCloseReloadPage != null && WindowCloseReloadPage instanceof Function) {
+    if (window.parent != null && window.parent.WindowCloseReloadPage != null && window.parent.WindowCloseReloadPage instanceof Function) {
         window.parent.WindowCloseReloadPage(msg);
     } else {
         if (typeof WindowCloseReloadPage != 'undefined' && WindowCloseReloadPage instanceof Function)

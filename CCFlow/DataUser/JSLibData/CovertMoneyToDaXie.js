35f1d61615b476033389e7285eee65672e252844
张庆鹏 <zhangqingpeng@ccflow.org>
2020-06-04 17:11:39 +0800

    对val进行非空判断，兼容移动端

--- CCFlow/DataUser/JSLibData/CovertMoneyToDaXie.js
+++ CCFlow/DataUser/JSLibData/CovertMoneyToDaXie.js
@@ -58,6 +58,8 @@ function getArgsFromHref(sArgName) {
 }
 
 function clearNoNum(val) {
+    if (val == undefined || val == "" || val == null)
+        return "";
     val = val.replace(/[^\d.]/g, "");  //清除“数字”和“.”以外的字符
     val = val.replace(/^\./g, "");  //验证第一个字符是数字而不是.
     val = val.replace(/\.{2,}/g, "."); //只保留第一个. 清除多余的.

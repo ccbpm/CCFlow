766f049dafe81a02609049ebd37173eaa82f7f5c
袁丽娜 <nayuanli@126.com>
2020-06-08 08:53:11 +0800

    no commit message

--- CCFlow/WF/Toolbar.js
+++ CCFlow/WF/Toolbar.js
@@ -503,6 +503,13 @@ function Send(isHuiQian,formType) {
             return;
 
     /**发送前处理的信息 End**/
+    var nd = new Entity("BP.WF.Node", GetQueryString("FK_Node"));
+    if (nd.CondModel == 1) {
+        var url = ccbpmPath + "/WF/WorkOpt/ToNodes.htm?FK_Node=" + paramData.FK_Node + "&FID=" + paramData.FID + "&WorkID=" + paramData.WorkID + "&FK_Flow=" + paramData.FK_Flow + "&PWorkID=" + GetQueryString("PWorkID") + "&s=" + Math.random();
+       
+        initModal("SelectNodeUrl", null, url); $('#returnWorkModal').modal().show();
+        return;
+    }
 
     window.hasClickSend = true; //标志用来刷新待办.
 

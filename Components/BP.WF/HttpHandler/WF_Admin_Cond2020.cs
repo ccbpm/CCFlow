ad05187a962699f5923464da0f5372e454ba8700
驰骋BPM <zhoupeng@ccflow.org>
2020-06-10 10:51:31 +0800

    no commit message

---
+++ Components/BP.WF/HttpHandler/WF_Admin_Cond2020.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Web;
+using BP.DA;
+using BP.Sys;
+using BP.Web;
+using BP.Port;
+using BP.En;
+using BP.WF;
+using BP.WF.Template;
+
+namespace BP.WF.HttpHandler
+{
+    /// <summary>
+    /// 页面功能实体
+    /// </summary>
+    public class WF_Admin_Cond2020 : DirectoryPageBase
+    {
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public WF_Admin_Cond2020()
+        {
+        }
+        /// <summary>
+        /// 初始化列表
+        /// </summary>
+        /// <returns></returns>
+        public string List_Init()
+        {
+            // BP.WF.Template.CondAttr.ToNodeID
+            // Conds condes = new Conds();
+            // condes.RetrieveAll();
+            return "";
+        }
+        /// <summary>
+        /// 校验是否正确
+        /// </summary>
+        /// <returns></returns>
+        public string List_DoCheck()
+        {
+            string str = "";
+
+            string mystr = this.GetRequestVal("ToNodeID");
+            int toNodeID = this.FK_Node;
+            if (DataType.IsNullOrEmpty(mystr) == false)
+                toNodeID = int.Parse(mystr);
+
+            //集合.
+            Conds conds = new Conds();
+            conds.Retrieve(CondAttr.FK_Node, this.FK_Node, CondAttr.ToNodeID,
+                toNodeID, CondAttr.CondType, this.GetRequestValInt("CondType"), CondAttr.Idx);
+
+            if (conds.Count == 0)
+                return "";
+            if (conds.Count == 1)
+            {
+                foreach (Cond item in conds)
+                {
+                    if (item.HisDataFrom == ConnDataFrom.CondOperator)
+                        return "info@请继续增加条件";
+                    else
+                        return "条件成立.";
+                }
+            }
+
+            //遍历方向条件.
+            foreach (Cond item in conds)
+            {
+                if (item.HisDataFrom == ConnDataFrom.CondOperator)
+                    str += " " + item.OperatorValue;
+                else
+                    str += " 1=1 "; // + item.AttrKey + item.FK_Operator + item.OperatorValue;
+            }
+
+            string sql = "";
+            switch (SystemConfig.AppCenterDBType)
+            {
+                case DBType.MSSQL:
+                    sql = " SELECT TOP 1 No FROM Port_Emp WHERE " + str;
+                    break;
+                case DBType.MySQL:
+                    sql = " SELECT No FROM Port_Emp WHERE " + str + "  limit 1 ";
+                    break;
+                case DBType.Oracle:
+                case DBType.DM:
+                    sql = " SELECT No FROM Port_Emp WHERE " + str + "  rownum <=1 ";
+                    break;
+                default:
+                    return "err@没有做的数据库类型判断.";
+            }
+
+            try
+            {
+                DataTable dt = DBAccess.RunSQLReturnTable(sql);
+                return "格式正确:<font color=blue>" + str + "</blue>";
+            }
+            catch (Exception ex)
+            {
+                return "err@不符合规范. <font color=blue>" + str + "</font>";
+            }
+        }
+
+    }
+}
---
+++ Components/BP.WF/WF/WorkNodePlus.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using BP.En;
+using BP.DA;
+using System.Collections;
+using System.Data;
+using BP.Port;
+using BP.Web;
+using BP.Sys;
+using BP.WF.Template;
+using BP.WF.Data;
+namespace BP.WF
+{
+    /// <summary>
+    /// WorkNode的附加类: 2020年06月09号
+    /// 1， 因为worknode的类方法太多，为了能够更好的减轻代码逻辑.
+    /// 2.  一部分方法要移动到这里来. 
+    /// </summary>
+    public class WorkNodePlus
+    {
+        /// <summary>
+        /// 处理协作模式下的删除规则
+        /// </summary>
+        /// <param name="nd">节点</param>
+        /// <param name="gwf"></param>
+        public static void GenerWorkerListDelRole(Node nd,GenerWorkFlow gwf)
+        {
+            if (nd.GenerWorkerListDelRole == 0)
+                return;
+
+            //按照部门删除,同部门下的人员.
+            if (nd.GenerWorkerListDelRole==1)
+            {
+                //定义本部门的人员.
+                string sqlUnion = "";
+                sqlUnion += " SELECT No FROM Port_Emp WHERE FK_Dept='" + WebUser.FK_Dept + "' ";
+                sqlUnion += " UNION ";
+                sqlUnion += " SELECT FK_Dept FROM Port_DeptEmp WHERE FK_Dept='" +WebUser.FK_Dept + "''";
+
+                //获得要删除的人员.
+                string sql = " SELECT FK_Emp FROM WF_GenerWorkerlist WHERE ";
+                sql += " WHERE ";
+                sql += " WorkID="+gwf.WorkID+ " AND FK_Node="+gwf.FK_Node+" AND IsPass=0 ";
+                sql += " AND FK_Emp IN ("+ sqlUnion + ")";
+
+                //获得要删除的数据.
+                DataTable dt = DBAccess.RunSQLReturnTable(sql);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    string empNo = dr[0].ToString();
+                    sql = "UPDATE WF_GenerWorkerlist SET IsPass=1 WHERE WorkID="+gwf.WorkID+" AND FK_Node="+gwf.FK_Node+" AND FK_Emp='"+ empNo + "'";
+                    DBAccess.RunSQL(sql);
+                }
+            }
+
+            //按照岗位删除.
+            if (nd.GenerWorkerListDelRole == 2)
+            {
+                NodeStations nss = new NodeStations();
+                nss.Retrieve(NodeStationAttr.FK_Node, gwf.FK_Node);
+                if (nss.Count==0)
+
+
+                throw new Exception("err@尚未解析.");
+            }
+
+        }
+
+    }
+}

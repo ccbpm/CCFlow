9fab3b5c991f4b545b2374477d9eb3e4349363d0
驰骋BPM <zhoupeng@ccflow.org>
2020-06-15 18:07:48 +0800

    no commit message

---
+++ Components/BP.En30/Sys/EnumAth.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BP.Sys
+{
+    /// <summary>
+    /// 文件保存方式
+    /// </summary>
+    public enum AthSaveWay
+    {
+        /// <summary>
+        /// IIS服务器
+        /// </summary>
+        IISServer,
+        /// <summary>
+        /// 保存到数据库
+        /// </summary>
+        DB,
+        /// <summary>
+        /// ftp
+        /// </summary>
+        FTPServer
+    }
+    /// <summary>
+    /// 运行模式
+    /// </summary>
+    public enum AthRunModel
+    {
+        /// <summary>
+        /// 记录模式
+        /// </summary>
+        RecordModel,
+        /// <summary>
+        /// 固定模式
+        /// </summary>
+        FixModel
+    }
+    /// <summary>
+    /// 上传校验,方式.
+    /// </summary>
+    public enum UploadFileNumCheck
+    {
+        /// <summary>
+        /// 不校验
+        /// </summary>
+        None,
+        /// <summary>
+        /// 不能为空
+        /// </summary>
+        NotEmpty,
+        /// <summary>
+        /// 每个类别不能为空.
+        /// </summary>
+        EverySortNoteEmpty
+    }
+    public enum AthCtrlWay
+    {
+        /// <summary>
+        /// 表单主键
+        /// </summary>
+        PK,
+        /// <summary>
+        /// FID
+        /// </summary>
+        FID,
+        /// <summary>
+        /// 父流程ID
+        /// </summary>
+        PWorkID,
+        /// <summary>
+        /// 仅仅查看自己的
+        /// </summary>
+        MySelfOnly,
+        /// <summary>
+        /// 工作ID,对流程有效.
+        /// </summary>
+        WorkID,
+        /// <summary>
+        /// P2流程
+        /// </summary>
+        P2WorkID,
+        /// <summary>
+        /// P3流程
+        /// </summary>
+        P3WorkID
+
+    }
+    /// <summary>
+    /// 附件上传类型
+    /// </summary>
+    public enum AttachmentUploadType
+    {
+        /// <summary>
+        /// 单个的
+        /// </summary>
+        Single,
+        /// <summary>
+        /// 多个的
+        /// </summary>
+        Multi,
+        /// <summary>
+        /// 指定的
+        /// </summary>
+        Specifically
+    }
+    /// <summary>
+    /// 附件上传方式
+    /// </summary>
+    public enum AthUploadWay
+    {
+        /// <summary>
+        /// 继承模式
+        /// </summary>
+        Inherit,
+        /// <summary>
+        /// 协作模式
+        /// </summary>
+        Interwork
+    }
+    /// <summary>
+    /// 文件展现方式
+    /// </summary>
+    public enum FileShowWay
+    {
+        /// <summary>
+        /// 表格
+        /// </summary>
+        Table,
+        /// <summary>
+        /// 图片
+        /// </summary>
+        Pict,
+        /// <summary>
+        /// 自由模式
+        /// </summary>
+        Free
+    }
+
+    /// <summary>
+    /// 附件删除规则
+    /// </summary>
+    public enum AthDeleteWay
+    {
+        /// <summary>
+        /// 不删除 0
+        /// </summary>
+        None,
+        /// <summary>
+        /// 删除所有 1
+        /// </summary>
+        DelAll,
+        /// <summary>
+        /// 只删除自己上传 2
+        /// </summary>
+        DelSelf
+    }
+
+}
---
+++ Components/BP.En30/Sys/EnumLab.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP.Sys
+{
+    /// <summary>
+    /// 按日期查询方式
+    /// </summary>
+    public enum DTSearchWay
+    {
+        /// <summary>
+        /// 不设置
+        /// </summary>
+        None,
+        /// <summary>
+        /// 按日期
+        /// </summary>
+        ByDate,
+        /// <summary>
+        /// 按日期时间
+        /// </summary>
+        ByDateTime
+    }
+    /// <summary>
+    /// 表单类型
+    /// </summary>
+    public enum AppType
+    {
+        /// <summary>
+        /// 独立表单
+        /// </summary>
+        Application = 0,
+        /// <summary>
+        /// 节点表单
+        /// </summary>
+        Node = 1
+    }
+    public enum FrmFrom
+    {
+        Flow,
+        Node,
+        Dtl
+    }
+    /// <summary>
+    /// 表单类型 @0=傻瓜表单@1=自由表单@3=嵌入式表单@4=Word表单@5=Excel表单@6=VSTOForExcel@7=Entity
+    /// </summary>
+    public enum FrmType
+    {
+        /// <summary>
+        /// 傻瓜表单
+        /// </summary>
+        FoolForm = 0,
+        /// <summary>
+        /// 自由表单
+        /// </summary>
+        FreeFrm = 1,
+        /// <summary>
+        /// URL表单(自定义)
+        /// </summary>
+        Url = 3,
+        /// <summary>
+        /// Word类型表单
+        /// </summary>
+        WordFrm = 4,
+        /// <summary>
+        /// Excel表单
+        /// </summary>
+        ExcelFrm = 5,
+        /// <summary>
+        /// VSTOExccel模式.
+        /// </summary>
+        VSTOForExcel = 6,
+        /// <summary>
+        /// 实体类
+        /// </summary>
+        Entity = 7,
+        /// <summary>
+        /// 开发者表单
+        /// </summary>
+        Develop = 8
+    }
+}

c80c994b69899b5dd6d71e899d728bcfafe72438
驰骋BPM <zhoupeng@ccflow.org>
2020-07-05 17:21:28 +0800

    no commit message

---
+++ Components/BP.WF/WeiXin/DTS/DTSOrganization.cs
@@ -0,0 +1,149 @@
+﻿using BP.En;
+using BP.GPM.DTalk;
+using BP.GPM.WeiXin;
+using BP.Tools;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace BP.GPM.WeiXin
+{
+    public class DTSOrganization : Method
+    {
+        public DTSOrganization()
+        {
+            this.Title = "同步微信企业号的通讯录 ";
+            this.Help = "将微信企业号中的通讯录同步到本地的组织结构";
+            this.Help = "同步完成之后，需要配置人员的角色信息和主部门的信息。";
+
+            this.GroupName = "执行";
+        }
+        public override void Init()
+        {
+            
+        }
+        public override bool IsCanDo
+        {
+            get
+            {
+                if (BP.Web.WebUser.IsAdmin == true)
+                    return true;
+                return false;
+            }
+        }
+        public override object Do()
+        {
+            #region 读取数据.
+            //判断是否配置了企业号
+            if (string.IsNullOrWhiteSpace(BP.Sys.SystemConfig.WX_CorpID))
+                return "err@没有配置企业号相关信息";
+            //获取部门列表
+            DeptList DeptMentList = new DeptList();
+            DeptMentList.RetrieveAll();
+            if (DeptMentList.errcode == 0)
+                return "err@获得数据期间出现错误.";
+            #endregion 读取数据.
+
+            #region 清楚现有的数据.
+            //先删除所有部门
+            GPM.Depts depts = new GPM.Depts();
+            depts.ClearTable();
+
+            //删除所有人员
+            GPM.Emps emps = new GPM.Emps();
+            emps.ClearTable();
+
+            //删除部门人员表
+            GPM.DeptEmps deptEmps = new GPM.DeptEmps();
+            deptEmps.ClearTable();
+
+            //删除部门人员岗位表
+            GPM.DeptEmpStations deptEmpStations = new GPM.DeptEmpStations();
+            deptEmpStations.ClearTable();
+            #endregion 清楚现有的数据.
+
+            #region 写入数据.
+            GPM.DeptEmp deptEmp = new GPM.DeptEmp();
+            GPM.Emp emp = new GPM.Emp();
+            foreach (DeptEntity deptMent in DeptMentList.department)
+            {
+                //先插入部门表
+                GPM.Dept dept = new GPM.Dept();
+                dept.No = deptMent.id;
+                dept.Name = deptMent.name;
+                dept.ParentNo = deptMent.parentid;
+                dept.Insert();
+
+                //获取部门下的人员
+                UserList users = new UserList(deptMent.id);
+                if (users.errcode == 0)
+                    continue;
+
+                foreach (UserEntity userInfo in users.userlist)
+                {
+                    //此处不能同步admin帐号的用户
+                    if (userInfo.userid == "admin")
+                        continue;
+
+                    //如果有，放入部门人员表
+                    if (emps.Retrieve(GPM.EmpAttr.No, userInfo.userid) > 0)
+                    {
+                        //插入部门人员表
+                        deptEmp = new GPM.DeptEmp();
+                        deptEmp.MyPK = deptMent.id + "_" + userInfo.userid;
+                        deptEmp.FK_Emp = userInfo.userid;
+                        deptEmp.FK_Dept = deptMent.id;
+                        deptEmp.Insert();
+                    }
+                    //如果没有，默认主部门是当前第一个
+                    else
+                    {
+                        //插入人员表
+                        emp = new GPM.Emp();
+                        emp.No = userInfo.userid;
+                        emp.Name = userInfo.name;
+                        emp.FK_Dept = deptMent.id;
+                        emp.Email = userInfo.email;
+                        emp.Tel = userInfo.mobile;
+                        emp.Insert();
+
+                        //插入部门人员表
+                        deptEmp = new GPM.DeptEmp();
+                        deptEmp.MyPK = deptMent.id + "_" + userInfo.userid;
+                        deptEmp.FK_Emp = userInfo.userid;
+                        deptEmp.FK_Dept = deptMent.id;
+                        deptEmp.Insert();
+
+                        //没有岗位，不同步，手动分配岗位吧
+                        //GPM.DeptEmpStation deptEmpStation = new GPM.DeptEmpStation();
+                        //deptEmpStation.MyPK = deptMent.id + "_" + userInfo.userid + "";
+                    }
+                }
+            }
+            #endregion 写入数据.
+
+            #region 增加 admin.
+            //不管以上有无人员，都添加admin帐号的信息
+            //插入admin帐号
+            emp = new GPM.Emp();
+            emp.No = "admin";
+            emp.Name = "admin";
+            emp.FK_Dept = "1";//默认跟部门为1
+            emp.Email = "";
+            emp.Tel = "";
+            emp.Insert();
+
+            //部门人员表加入admin
+            deptEmp = new GPM.DeptEmp();
+            deptEmp.MyPK = "1_admin";
+            deptEmp.FK_Emp = "admin";
+            deptEmp.FK_Dept = "1";
+            deptEmp.Insert();
+            #endregion 增加admin.
+
+            return "同步完成..";
+        }
+         
+    }
+}
---
+++ Components/BP.WF/WeiXin/WeiXinEntity.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using BP.Tools;
+using BP.DA;
+using BP.WF;
+using System.Collections;
+
+namespace BP.GPM.WeiXin
+{
+    /// <summary>
+    /// 微信实体类
+    /// </summary>
+    public class WeiXinEntity
+    {
+        #region 基本配置.
+        /// <summary>
+        /// 微信应用的分配的单位ID.
+        /// 格式:wx8eac6a18c5efec30
+        /// </summary>
+        public static string appid
+        {
+            get
+            {
+                return BP.Sys.SystemConfig.WX_CorpID;// "wx8eac6a18c5efec30";
+            }
+        }
+        /// <summary>
+        /// 微信应用的分配给单位的一个加密字符串, 标识这个值对应的是这个单位的应用.
+        /// 格式:KfFkE9AZ3Zp09zTuKvmqWLgtLj
+        /// 也就是密钥.
+        /// </summary>
+        public static string appsecret
+        {
+            get
+            {
+                return BP.Sys.SystemConfig.WX_AppSecret;// "KfFkE9AZ3Zp09zTuKvmqWLgtLj-_cHMPTvV992apOWgSKJHcbjpbu1jYVXh7gI7K";
+            }
+        }
+        /// <summary>
+        /// 获得token,每间隔x分钟，就会失效.
+        /// </summary>
+        /// <returns>token</returns>
+        public static string getAccessToken()
+        {
+            string accessToken = string.Empty;
+            string url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + appid + "&corpsecret=" + appsecret;
+
+            return BP.DA.DataType.ReadURLContext(url, 9999);
+        }
+        #endregion 基本配置.
+
+        #region 应用方法.
+        /// <summary>
+        /// 调用企业号获取地理位置
+        /// </summary>
+        /// <returns></returns>
+        public static string GetWXConfigSetting(string pageUrl)
+        {
+            //必须是当前页面，如果在CCMobile/Home.htm调用，则传入Home.htm
+            string htmlPage = pageUrl;
+            Hashtable ht = new Hashtable();
+
+            //生成签名的时间戳
+            string timestamp = DateTime.Now.ToString("yyyyMMDDHHddss");
+            //生成签名的随机串
+            string nonceStr = BP.DA.DBAccess.GenerGUID();
+            //企业号jsapi_ticket
+            string jsapi_ticket = "";
+            string url1 = htmlPage;
+            //获取 AccessToken
+            string accessToken = getAccessToken();
+
+            string url = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=" + accessToken;
+            string str = BP.DA.DataType.ReadURLContext(url, 9999, null);
+
+            //权限签名算法
+            Ticket ticket = new Ticket();
+            ticket = FormatToJson.ParseFromJson<Ticket>(str);
+
+            if (ticket.errcode == "0")
+                jsapi_ticket = ticket.ticket;
+            else
+                return "err:@获取jsapi_ticket失败+accessToken=" + accessToken;
+
+            ht.Add("timestamp", timestamp);
+            ht.Add("nonceStr", nonceStr);
+            //企业微信的corpID
+            ht.Add("AppID", BP.Sys.SystemConfig.WX_CorpID);
+
+            //生成签名算法
+            string str1 = "jsapi_ticket=" + jsapi_ticket + "&noncestr=" + nonceStr + "&timestamp=" + timestamp + "&url=" + url1 + "";
+            string Signature = Sha1Signature(str1);
+            ht.Add("signature", Signature);
+
+            return BP.Tools.Json.ToJson(ht);
+        }
+        #endregion 应用方法.
+
+        #region 发送微信信息.
+        public MessageErrorModel PostWeiXinMsg(StringBuilder sb)
+        {
+            string wxStr = string.Empty;
+            string url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?";
+
+            MessageErrorModel m = new MessageErrorModel();
+            wxStr = PostForWeiXin(sb, url);
+            m = FormatToJson.ParseFromJson<MessageErrorModel>(wxStr);
+            return m;
+        }
+        /// <summary>
+        /// POST方式请求 微信返回信息
+        /// </summary>
+        /// <param name="parameters">参数</param>
+        /// <param name="URL">请求地址</param>
+        /// <returns>返回字符</returns>
+        public string PostForWeiXin(StringBuilder parameters, string URL)
+        {
+            string access_token = getAccessToken();
+            string url = URL + "access_token=" + access_token;
+
+            //todo:zqp.该方法没有完善.
+            string str = BP.DA.DataType.ReadURLContext(url, 9999, null, parameters);
+
+            //HttpWebResponse response = new HttpWebResponseUtility().WXCreateGetHttpResponse(url, parameters,
+            //    10000, null, Encoding.UTF8, null);
+            //StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            //string str = reader.ReadToEnd();
+
+            BP.DA.Log.DebugWriteInfo(url + "----------------" + parameters + "---------------" + str);
+            return str;
+        }
+        #endregion
+
+        
+        /// <summary>
+        /// 算法加密
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string Sha1Signature(string str)
+        {
+            string s = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(str, "SHA1").ToString();
+            return s.ToLower();
+        }
+    }
+}

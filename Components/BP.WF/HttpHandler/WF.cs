d86efdae30a14a2a599c35005665ca0c85e1dc1e
李震 <5197730+YCDMW@users.noreply.gitee.com>
2020-07-14 15:36:44 +0800

    no commit message

--- Components/BP.WF/HttpHandler/WF.cs
+++ Components/BP.WF/HttpHandler/WF.cs
@@ -2564,13 +2564,7 @@ namespace BP.WF.HttpHandler
                         return "url@WFRpt.htm?FK_Flow=" + ap.GetValStrByKey("FK_Flow") + "&WorkID=" + ap.GetValStrByKey("WorkID") + "&o2=1" + paras;
                 }
             }
-           //访问Bill页面
-            if (this.DoWhat.Equals(DoWhatList.Bill) == true)
-                if(this.WorkID==0)
-                    return "url@./CCBill/MyBill.htm?FrmID=" + this.FrmID;
-                else
-                    return "url@./CCBill/MyBill.htm?WorkID="+this.WorkID+"&FrmID="+this.FrmID;
-            return "err@没有约定的标记:DoWhat=" + this.DoWhat;
+           
         }
         #endregion 处理page接口.
 

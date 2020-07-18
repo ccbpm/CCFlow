26a10a2ba60faf6b75154973ea8238b44e162e72
驰骋BPM <zhoupeng@ccflow.org>
2020-07-18 17:26:31 +0800

    no commit message

--- CCFlow/WF/Scripts/DealErrInfo.js
+++ CCFlow/WF/Scripts/DealErrInfo.js
@@ -1,22 +1,15 @@
 ﻿//处理错误信息.
 function DealErrMsg(msg) {
 
-  
-
     var msgFieldly = ""; //友好的提示.
     var msgTech = "";    //技术信息错误.
 
-    if (msg.indexOf('调用类') == 0) {
+    if (msg.indexOf('调用类') == 0 || msg.indexOf('err@调用类') == 0) {
         msgFieldly = "抱歉,系统出现错误,请您把错误信息反馈给管理员.";
         msgTech = msg;
     }
 
-    if (msg.indexOf('流程设计错误') == 0) {
-        msgFieldly = "抱歉,系统出现错误,请您把错误信息反馈给管理员.";
-        msgTech = msg;
-    }
-
-    if (msg.indexOf('流程设计错误') == 0) {
+    if (msg.indexOf('err@流程设计错误') == 0 || msg.indexOf('流程设计错误') == 0) {
         msgFieldly = "抱歉,流程设计出现错误,请您把错误信息反馈给管理员.";
         msgTech = msg;
     }
@@ -26,13 +19,14 @@ function DealErrMsg(msg) {
         msgTech = msg;
     }
 
-    if (msg.indexOf('Node_SaveWork') == 0 || msg.indexOf('工作已经发送到下一个环节') > 0)
-    {
-        msgFieldly = "该工作已经运行到下一个环节,执行保存失败..";
+    if (msg.indexOf('Node_SaveWork') == 0 || msg.indexOf('工作已经发送到下一个环节') > 0) {
+        msgFieldly = "该工作已经运行到下一个环节,执行保存失败.";
         msgTech = msg;
     }
 
     var divFieldly = $("#divFieldly"); //友好提示.
-    var divTech = $("#divTech"); //技术要显示的信息.
+    $("#divTech").html(msgFieldly);
 
+    var tech = "<a href='' >技术信息</a> <div >" + msgTech + "</div>";
+    $("#divTech").html(tech); //技术要显示的信息.
 }

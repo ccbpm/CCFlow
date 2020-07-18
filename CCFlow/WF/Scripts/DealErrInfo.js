59b03033e842725947ccfa94ac3a78f1e876eb27
金红岩 <7353775+gold_red_rock@users.noreply.gitee.com>
2020-07-18 21:27:18 +0800

    no commit message

--- CCFlow/WF/Scripts/DealErrInfo.js
+++ CCFlow/WF/Scripts/DealErrInfo.js
@@ -1,32 +1,48 @@
-﻿//处理错误信息.
+﻿var msgFieldly = ""; //友好的提示.
+var msgTech = "";    //技术信息错误.
+var flag = 0;
+//处理错误信息.
 function DealErrMsg(msg) {
-
-    var msgFieldly = ""; //友好的提示.
-    var msgTech = "";    //技术信息错误.
-
     if (msg.indexOf('调用类') == 0 || msg.indexOf('err@调用类') == 0) {
-        msgFieldly = "抱歉,系统出现错误,请您把错误信息反馈给管理员.";
+        msgFieldly = "<img src='../../WF/img/info.png' height='20' width='20'/>抱歉,系统出现错误,请您把错误信息反馈给管理员.";
         msgTech = msg;
     }
 
     if (msg.indexOf('err@流程设计错误') == 0 || msg.indexOf('流程设计错误') == 0) {
-        msgFieldly = "抱歉,流程设计出现错误,请您把错误信息反馈给管理员.";
+        msgFieldly ="<img src='../../WF/img/info.png' height='20' width='20'/>抱歉,流程设计出现错误,请您把错误信息反馈给管理员.";
         msgTech = msg;
     }
 
     if (msg.indexOf('Node_SaveWork') == 0) {
-        msgFieldly = "保存错误.";
+        msgFieldly ="<img src='../../WF/img/info.png' height='20' width='20'/>保存错误.";
         msgTech = msg;
     }
 
     if (msg.indexOf('Node_SaveWork') == 0 || msg.indexOf('工作已经发送到下一个环节') > 0) {
-        msgFieldly = "该工作已经运行到下一个环节,执行保存失败.";
+        msgFieldly ="<img src='../../WF/img/info.png' height='20' width='20'/>该工作已经运行到下一个环节,执行保存失败.";
         msgTech = msg;
     }
-
-    var divFieldly = $("#divFieldly"); //友好提示.
-    $("#divTech").html(msgFieldly);
-
-    var tech = "<a href='' >技术信息</a> <div >" + msgTech + "</div>";
-    $("#divTech").html(tech); //技术要显示的信息.
+    if (msgFieldly != "") {
+        //友好提示.
+        $(".divFieldly").html(msgFieldly);
+        $(".divFieldly").css("color", "red");
+        $(".divFieldly").show();
+    }
+    if (msgTech != "") {
+        var tech = "<a href='javascript: void(0)' onclick='msgTchClick()' ><img src='../../WF/img/Message24.png' height='20' width='20'/>技术信息</a>";
+        $(".divTech").html(tech); //技术要显示的信息
+        $(".divTech").show();
+    }
+}
+function msgTchClick() {
+    msgTech = "<img src='../../WF/img/Help.png' height='20' width='20' />"+msgTech;
+    $(".techMsg").html(msgTech); //技术要显示的信息
+    if (flag == 0) {
+        $(".techMsg").show();
+        flag = 1;
+    } else {
+        $(".techMsg").hide();
+        flag = 0;
+    }
+    
 }

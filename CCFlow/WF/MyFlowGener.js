30f582499d8ba3b54436ae8a61cdecd2ed23ad6e
袁丽娜 <nayuanli@126.com>
2020-07-27 14:06:25 +0800

    no commit message

--- CCFlow/WF/MyFlowGener.js
+++ CCFlow/WF/MyFlowGener.js
@@ -1196,18 +1196,18 @@ function GenerWorkNode() {
 
     //2018.1.1 新增加的类型, 流程独立表单， 为了方便期间都按照自由表单计算了.
     if (node.FormType == 11) {
-        if (flowData.FrmNode[0] != null && flowData.FrmNode[0] != undefined)
-            if (flowData.FrmNode[0].FrmType == 0)
+        if (flowData.WF_FrmNode[0] != null && flowData.WF_FrmNode[0] != undefined)
+            if (flowData.WF_FrmNode[0].FrmType == 0)
                 GenerFoolFrm(flowData); //傻瓜表单.
-        if (flowData.FrmNode[0].FrmType == 1) {
+        if (flowData.WF_FrmNode[0].FrmType == 1) {
             Skip.addJs("./MyFlowFree2017.js");
             GenerFreeFrm(flowData);
         }
 
-        if (flowData.FrmNode[0].FrmType == 8) {
+        if (flowData.WF_FrmNode[0].FrmType == 8) {
             Skip.addJs("./CCForm/FrmDevelop.js");
             $('head').append('<link href="../DataUser/Style/MyFlowGenerDevelop.css" rel="Stylesheet" />');
-            GenerDevelopFrm(flowData, flowData.FrmNode[0].FK_Frm);
+            GenerDevelopFrm(flowData, flowData.WF_FrmNode[0].FK_Frm);
         }
 
     }
@@ -1248,7 +1248,7 @@ function GenerWorkNode() {
     //2018.1.1 新增加的类型, 流程独立表单， 为了方便期间都按照自由表单计算了.
     if (node.FormType == 11) {
         //获得配置信息.
-        var frmNode = flowData["FrmNode"];
+        var frmNode = flowData["WF_FrmNode"];
         if (frmNode) {
             frmNode = frmNode[0];
             if (frmNode.FrmSln == 1) {

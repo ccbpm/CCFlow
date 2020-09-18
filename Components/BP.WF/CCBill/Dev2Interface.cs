7ef325fdfd9929468550e33c664ad1ab2dd0ddb5
李震 <5197730+YCDMW@users.noreply.gitee.com>
2020-09-18 18:04:49 +0800

    no commit message

--- Components/BP.WF/CCBill/Dev2Interface.cs
+++ Components/BP.WF/CCBill/Dev2Interface.cs
@@ -42,15 +42,12 @@ namespace BP.CCBill
                     return gb.WorkID;
             }
 
-            BP.Port.Emp emp = new Port.Emp(userNo); 
-            
-
             FrmBill fb = new FrmBill(frmID);
 
             gb.WorkID = DBAccess.GenerOID("CCBill");
             gb.BillState = BillState.None; //初始化状态.
-            gb.Starter = emp.No;
-            gb.StarterName = emp.Name;
+            gb.Starter = WebUser.No;
+            gb.StarterName = WebUser.Name;
             gb.FrmName = fb.Name; //单据名称.
             gb.FrmID = fb.No; //单据ID
 
@@ -94,12 +91,13 @@ namespace BP.CCBill
             rpt.SetValByKey("BillState", (int)gb.BillState);
             rpt.SetValByKey("Starter", gb.Starter);
             rpt.SetValByKey("StarterName", gb.StarterName);
-            rpt.SetValByKey("FK_Dept", emp.FK_Dept);
+            rpt.SetValByKey("FK_Dept", WebUser.FK_Dept);
             rpt.SetValByKey("RDT", gb.RDT);
             rpt.SetValByKey("Title", gb.Title);
             rpt.SetValByKey("BillNo", gb.BillNo);
             rpt.OID = gb.WorkID;
             rpt.InsertAsOID(gb.WorkID);
+
 
             return gb.WorkID;
         }

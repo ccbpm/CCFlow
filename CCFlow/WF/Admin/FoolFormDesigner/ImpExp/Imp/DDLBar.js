593ca2f61a181b3f079fe87eb828bb394568373e
ccflowlover <ccbpm@ccflow.org>
2021-03-10 18:25:42 +0800

    no commit message

---
+++ CCFlow/WF/Admin/FoolFormDesigner/ImpExp/Imp/DDLBar.js
@@ -0,0 +1,108 @@
+﻿
+var frmID = null;
+var flowNo = null;
+var nodeID = null;
+
+
+
+function InitBar(optionKey) {
+
+    //补充一些变量信息.
+    frmID = GetQueryString("FK_MapData");
+    if (frmID == null || frmID == undefined)
+        frmID = GetQueryString("FrmID");
+
+    flowNo = GetQueryString("FK_Flow");
+    if (flowNo == null || flowNo == undefined)
+        flowNo = GetQueryString("FlowNo");
+
+    nodeID = GetQueryString("FK_Node");
+    if (nodeID == null || nodeID == undefined)
+        nodeID = GetQueryString("NodeID");
+
+
+    var html = "<b>请选择导入/导出模式</b>:";
+
+    html += "<select id='changBar' onchange='changeOption()'>";
+
+    html += "<option value=null  disabled='disabled'>+导入表单</option>";
+    html += "<option value=" + Imp.localhostImp + ">&nbsp;&nbsp;&nbsp;&nbsp;本地xml表单模版导入</option>";
+    html += "<option value=" + Imp.NodeFrmImp + ">&nbsp;&nbsp;&nbsp;&nbsp;本流程的节点表单导入</option>";
+    html += "<option value=" + Imp.FlowFrmImp + ">&nbsp;&nbsp;&nbsp;&nbsp;其他流程导入</option>";
+    html += "<option value=" + Imp.FrmLibraryImp + ">&nbsp;&nbsp;&nbsp;&nbsp;表单库导入</option>";
+
+    html += "<option value=null  disabled='disabled'>+从表结构导入生成表单</option>";
+    html += "<option value=" + Imp.ExternalDataSourseImp + ">&nbsp;&nbsp;&nbsp;&nbsp;外部数据源导入</option>";
+
+    html += "<option value=null  disabled='disabled'>+导出</option>";
+    html += "<option value=" + Imp.ExportFrm + ">&nbsp;&nbsp;&nbsp;&nbsp;导出表单模板</option>";
+
+    html += "</select >";
+
+    //html += "<input  id='Btn_Save' type=button onclick='Save()' value='保存' />";
+    ////html += "<input  id='Btn_Help' type=button onclick='Adv()' value='高级设置' />";
+    //html += "<input  id='Btn_Help' type=button onclick='HelpOnline()' value='在线帮助' />";
+
+
+    document.getElementById("bar").innerHTML = html;
+    $("#changBar option[value='" + optionKey + "']").attr("selected", "selected");
+}
+
+
+function HelpOnline() {
+    var url = "http://ccbpm.mydoc.io";
+    window.open(url);
+}
+
+function changeOption() {
+
+    var obj = document.getElementById("changBar");
+    var sele = obj.options;
+    var index = obj.selectedIndex;
+    var optionKey = optionKey = sele[index].value;
+    var url = GetUrl(optionKey);
+
+    window.location.href = url + '?FK_MapData=' + GetQueryString("FK_MapData") + "&DoType=FunList&FK_Flow=" + GetQueryString("FK_Flow") + "&FK_Node=" + GetQueryString("FK_Node");
+}
+
+function GetUrl(optionKey) {
+
+    switch (parseInt(optionKey)) {
+        case Imp.localhostImp:
+            url = "localhostImp.htm";
+            break;
+        case Imp.NodeFrmImp:
+            url = "NodeFrmImp.htm";
+            break;
+        case Imp.FlowFrmImp:
+            url = "FlowFrmImp.htm";
+            break;
+        case Imp.FrmLibraryImp:
+            url = "FrmLibraryImp.htm";
+            break;
+        case Imp.ExternalDataSourseImp:
+            url = "ExternalDataSourseImp.htm";
+            break;
+        case Imp.ExportFrm:
+            url = "ExportFrm.htm";
+            break;
+        default:
+            url = "localhostImp.htm";
+            break;
+    }
+
+    return url;
+}
+
+$(function () {
+
+    jQuery.getScript(basePath + "/WF/Admin/Admin.js")
+        .done(function () {
+            /* 耶，没有问题，这里可以干点什么 */
+            // alert('ok');
+        })
+        .fail(function () {
+            /* 靠，马上执行挽救操作 */
+            //alert('err');
+        });
+});
---
+++ CCFlow/WF/Admin/FoolFormDesigner/MapExt/SingleChoiceSmall/DDLBar.js
@@ -0,0 +1,84 @@
+﻿
+function InitBar(optionKey) {
+
+    var html = "小范围单选:";
+    html += "<select id='changBar' onchange='changeOption()'>";
+
+    var groups = GetDBGroup();
+    var dtls = GetDBDtl();
+    
+    for (var i = 0; i < groups.length; i++) {
+
+        var group = groups[i];
+        html += "<option value=null  disabled='disabled'>+" + group.Name + "</option>";
+
+        for (var idx = 0; idx < dtls.length; idx++) {
+            var dtl = dtls[idx];
+            if (dtl.GroupNo != group.No)
+                continue;
+            html += "<option value=" + dtl.No + ">&nbsp;&nbsp;" + dtl.Name + "</option>";
+        }
+    }
+
+    html += "</select >";
+
+    html += "<input  id='Btn_Save' type=button onclick='Save()' value='保存' />";
+
+    html += "<input id='Btn_Advanced' type=button onclick='AdvSetting()' value='更多' />";
+
+
+    document.getElementById("bar").innerHTML = html;
+    $("#changBar option[value='" + optionKey + "']").attr("selected", "selected");
+}
+
+function GetDBGroup() {
+
+    var json = [
+
+        { "No": "A", "Name": "基本设置" },
+    ];
+    return json;
+}
+
+function GetDBDtl() {
+
+    var json = [
+
+        { "No": 0, "Name": "无,不设置", "GroupNo": "A", "Url": "0.None.htm" },
+        { "No": 1, "Name": "按文本输入的值", "GroupNo": "A", "Url": "1.ByInputText.htm" },
+        { "No": 2, "Name": "按照枚举值", "GroupNo": "A", "Url": "2.ByEnum.htm" },
+        { "No": 3, "Name": "按照系统外键表计算", "GroupNo": "A", "Url": "3.BySysKey.htm" },
+        { "No": 4, "Name": "按照SQL计算", "GroupNo": "A", "Url": "4.BySQL.htm" }
+    ];
+    return json;
+}
+
+function HelpOnline() {
+    var url = "http://ccbpm.mydoc.io";
+    window.open(url);
+}
+function changeOption() {
+    var obj = document.getElementById("changBar");
+    var sele = obj.options;
+    var index = obj.selectedIndex;
+    var optionKey = optionKey = sele[index].value;
+
+    var url = GetUrl(optionKey);
+    window.location.href = url + "?FK_MapData=" + GetQueryString("FK_MapData") + "&KeyOfEn=" +GetQueryString("KeyOfEn");
+}
+//高级设置.
+function AdvSetting() {
+    var url = "Adv.htm?FK_MapData=" + GetQueryString("FK_MapData") + "&KeyOfEn=" + GetQueryString("KeyOfEn");
+    OpenEasyUiDialogExt(url, "高级设置", 400, 180, false);
+}
+function GetUrl(optionKey) {
+
+    var json = GetDBDtl();
+    for (var i = 0; i < json.length; i++) {
+        var en = json[i];
+        if (en.No == optionKey)
+            return en.Url;
+    }
+    
+    return "0.None.htm";
+}

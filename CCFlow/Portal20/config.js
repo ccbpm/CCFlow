eded6a97d397dcfeacc8463a748a352da54a1047
ccflowlover <ccbpm@ccflow.org>
2021-03-10 17:49:18 +0800

    no commit message

--- CCFlow/Portal20/config.js
+++
@@ -1,9 +0,0 @@
-﻿/* 定义项目信息的文件
-1. 该文件不能被覆盖掉.
-2. 被Home.htm所引用.
-*/
-var appNo = "ASSET";
-var appName = "资产数字化运营管理平台";
-var appNameOfSmall = "资产运营中心";
-var appNameOfSmaller = "资产运营中心";
-var ParentNo = "2000";

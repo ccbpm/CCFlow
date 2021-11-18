42f69b0edb7507dea6a7b9502c0171478716a444
驰骋BPM-敏捷开发平台 <ccbpm@ccflow.org>
2021-11-18 12:05:32 +0800

    no commit message

---
+++ CCFlow/DataUser/JSLibData/PortalConfig.js
@@ -0,0 +1,15 @@
+﻿/**
+ * portalConfig主要是定义主体框架页面展示的通用配置
+ * 
+ */
+var PortalConfig = {
+    SystemName: "驰骋BPM", //系统名称
+    SystemLogo: "./image/logo.png",                  //系统图标
+    IsShowTheme: true,             //是否显示主题图标
+    DefaultTheme:"defaultTheme",            //默认主题颜色
+    IsShowFullScreen: true,        //是否显示全屏图标
+    IsShowFast: true,              //是否显示快捷方式
+    IsShowRefresh: true,             //是否显示刷新图标
+    IsShowFlexible: true,
+    LogoutPath:"./",                //默认退出到的路径
+}
